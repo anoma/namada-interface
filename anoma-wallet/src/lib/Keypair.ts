86cc5ab58a80a7f4da26d98e6582e958f3dc34be
Justin Evans <jurevans@gmail.com>
2022-03-04 12:15:12 +0100

    Cleaning up irrelevant files - add comments

--- anoma-wallet/src/lib/Keypair.ts
+++ anoma-wallet/src/lib/Keypair.ts
@@ -1,7 +1,6 @@
-import { toHex } from "@cosmjs/encoding";
+import { fromHex, toHex } from "@cosmjs/encoding";
 import { AnomaClient } from ".";
 import { Keypair as NativeKeypair } from "lib/anoma";
-import { hexToKeypair } from "utils/helpers";
 
 class Keypair {
   private _bytes: Uint8Array = new Uint8Array();
@@ -12,42 +11,60 @@ class Keypair {
     }
   }
 
-  public async fromFile(file: File): Promise<Keypair> {
-    return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-
-      reader.onload = (e: ProgressEvent<FileReader>) => {
-        resolve(this.fromArrayBuffer(e.target?.result as ArrayBuffer));
-      };
-
-      reader.onerror = (err) => {
-        reject(err);
-      };
-
-      reader.readAsArrayBuffer(new Blob([file]));
-    });
-  }
-
+  /**
+   * Get instance of class from ArrayBuffer
+   */
   public fromArrayBuffer(buffer: ArrayBuffer): Keypair {
     const bytes = new Uint8Array(buffer);
     return this.fromBytes(bytes);
   }
 
+  /**
+   * Get instance of class from bytes
+   */
   public fromBytes(bytes: Uint8Array): Keypair {
     this._bytes = bytes;
     return this;
   }
 
+  /**
+   *
+   * Convert bytes to hex
+   */
   public toHex(): string {
     return this._bytes ? toHex(this._bytes) : "";
   }
 
+  /**
+   *
+   * Convert to native Anoma keypair
+   */
   public async toNativeKeypair(): Promise<NativeKeypair> {
     const anoma = await new AnomaClient().init();
     const hex = this.toHex();
-    return anoma?.keypair.deserialize(hexToKeypair(hex));
+    return anoma?.keypair.deserialize(this.hexToKeypair(hex));
   }
 
+  /**
+   * Convert a hex keypair to a serializable keypair object
+   */
+  public hexToKeypair(keypair: string): {
+    secret: Uint8Array;
+    public: Uint8Array;
+  } {
+    // TODO: This will be deprecated
+    // This structure is only relevant to how keys are exported
+    // from Anoma Wallet CLI, and should not be relied on.
+    const [, priv, pub] = keypair.split("20000000");
+    return {
+      secret: fromHex(priv),
+      public: fromHex(pub),
+    };
+  }
+
+  /**
+   * Get bytes
+   */
   public get bytes(): Uint8Array {
     return this._bytes;
   }
