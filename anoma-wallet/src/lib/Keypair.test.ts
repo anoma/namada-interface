703f11b86bbb7e6bef57d31db16d808c91c152d3
Justin Evans <jurevans@gmail.com>
2022-03-07 15:02:30 +0100

    Add tests for native Keypair deserialize

--- anoma-wallet/src/lib/Keypair.test.ts
+++ anoma-wallet/src/lib/Keypair.test.ts
@@ -1,4 +1,4 @@
-/* eslint-disable max-len */
+import AnomaClient from "./AnomaClient";
 import Keypair from "./Keypair";
 
 const KEYPAIR = {
@@ -6,26 +6,44 @@ const KEYPAIR = {
   public: "572512A95B190D615B1987F7072572A64951AD50F4F97EF9DBB83545C46AE600",
 };
 
+const SERIALIZABLE_KEYPAIR = {
+  public: new Uint8Array([
+    87, 37, 18, 169, 91, 25, 13, 97, 91, 25, 135, 247, 7, 37, 114, 166, 73, 81,
+    173, 80, 244, 249, 126, 249, 219, 184, 53, 69, 196, 106, 230, 0,
+  ]),
+  secret: new Uint8Array([
+    240, 3, 224, 69, 201, 148, 60, 53, 112, 79, 80, 107, 101, 127, 186, 6, 176,
+    162, 113, 224, 62, 8, 183, 187, 124, 234, 244, 251, 92, 36, 119, 243,
+  ]),
+};
+
+// Pointer object properties:
+const props = ["ptr"];
+
 describe("Keypair wasm and class methods", () => {
   test("Keypair should be able to be serialized to a native type", async () => {
     const keypair = new Keypair(KEYPAIR);
     const nativeKeypair = await keypair.toNativeKeypair();
-    expect(nativeKeypair).toEqual({ ptr: 1179720 });
+
+    expect(Object.keys(nativeKeypair)).toEqual(expect.arrayContaining(props));
+    expect(nativeKeypair.serialize()).toEqual(SERIALIZABLE_KEYPAIR);
+    expect(nativeKeypair.to_bytes().length).toBe(64);
+    // Pointer should be nullified after to_bytes() is called
+    expect(nativeKeypair).toEqual({ ptr: 0 });
   });
 
   test("Keypair should be able to return a serializable keypair type", () => {
     const keypair = new Keypair(KEYPAIR);
     const { serializable } = keypair;
-    expect(serializable).toEqual({
-      public: new Uint8Array([
-        87, 37, 18, 169, 91, 25, 13, 97, 91, 25, 135, 247, 7, 37, 114, 166, 73,
-        81, 173, 80, 244, 249, 126, 249, 219, 184, 53, 69, 196, 106, 230, 0,
-      ]),
-      secret: new Uint8Array([
-        240, 3, 224, 69, 201, 148, 60, 53, 112, 79, 80, 107, 101, 127, 186, 6,
-        176, 162, 113, 224, 62, 8, 183, 187, 124, 234, 244, 251, 92, 36, 119,
-        243,
-      ]),
-    });
+    expect(serializable).toEqual(SERIALIZABLE_KEYPAIR);
+  });
+
+  test("JS Data should be able to be deserialized to native Keypair type", async () => {
+    // First, create a native Keypair (from wasm)
+    const { keypair } = await new AnomaClient().init();
+
+    // Now, we have access to the static method "deserialize"
+    const deserialized = keypair.deserialize(SERIALIZABLE_KEYPAIR);
+    expect(Object.keys(deserialized)).toEqual(expect.arrayContaining(props));
   });
 });
