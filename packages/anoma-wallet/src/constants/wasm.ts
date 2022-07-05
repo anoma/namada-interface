c8a934562941f39b8761becd914be0828cff5a2a
Memas Deligeorgakis <deligeorgakismemas@googlemail.com>
2022-07-05 12:47:25 +0200

    removed old WASM references as suggested in PR feedback

--- packages/anoma-wallet/src/constants/wasm.ts
+++ packages/anoma-wallet/src/constants/wasm.ts
@@ -4,8 +4,6 @@ export enum TxWasm {
   InitAccount_old = "tx_init_account.836cfb2b18345650b4704baa89c03ca2017c5b593341dd9ed86fd640d5bcbece.wasm",
   InitValidator = "tx_init_validator.7ab3888a7b75b6e09e562c5895acb9ddb891ceba51d72c53fa4a4b6bb9e84b81.wasm",
   Transfer = "tx_transfer.c050b5450bea92466a628333ce6e19b14ccbb8911645bc586f4901415cae7dfd.wasm",
-  Transfer_1 = "tx_transfer.efd1e91128dc24c27cef0041be9eb44685a3bd6151034dd55947c2f6f598675c.wasm",
-  Transfer_2 = "tx_transfer.819ee0bcd2d8eeeab022e043539ff8287feefe037ad4acd4780281131c70d01a.wasm",
   Withdraw = "tx_withdraw.cff5707c4c7d40f8c59b08a6975b869453774b803ae64c40af9bafb3f3dc3e2c.wasm",
   Bond = "tx_bond.593d2558f54f20eec9155bc842322a45badf0b75aa9464fd051595f8f382b297.wasm",
   UnBond = "tx_unbond.87e6f0f778717691e33b281d522e68b8048d0012b8a34e43a329b28c8a9780a4.wasm",
