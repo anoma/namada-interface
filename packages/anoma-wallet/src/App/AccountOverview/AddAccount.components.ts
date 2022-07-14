a1b3991a1ec0bdfe218e86cddcc798d070a64bdb
Justin R. Evans <jurevans@gmail.com>
2022-07-14 08:16:28 -0400

    Add in AddAccount components

---
+++ packages/anoma-wallet/src/App/AccountOverview/AddAccount.components.ts
@@ -0,0 +1,34 @@
+import styled from "styled-components/macro";
+
+export const AddAccountContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  color: ${(props) => props.theme.colors.titleColor};
+`;
+
+export const AddAccountContent = styled.div`
+  width: 100%;
+  min-height: 500px;
+  padding: 0 40px;
+  box-sizing: border-box;
+`;
+
+export const ButtonsContainer = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  padding: 0 40px;
+  margin: 0 0 20px 0;
+
+  & > a {
+    position: absolute;
+    left: 10%;
+  }
+`;
