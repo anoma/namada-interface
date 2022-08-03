adc8bab6d4749fb5a1979d09cc2ecaaddc83b9c7
Justin R. Evans <jurevans@gmail.com>
2022-08-03 05:38:47 -0400

    Remove unused lib and associated test

--- packages/anoma-wallet/src/lib/index.ts
+++ packages/anoma-wallet/src/lib/index.ts
@@ -1,7 +1,6 @@
 export { default as Account } from "lib/tx/Account";
 export { default as IBCTransfer } from "lib/tx/IBCTransfer";
 export { default as Keplr } from "lib/Keplr";
-export { default as Keypair } from "lib/Keypair";
 export { default as RpcClient } from "lib/rpc/RpcClient";
 export { default as Session } from "lib/Session";
 export { default as SocketClient } from "lib/rpc/SocketClient";
