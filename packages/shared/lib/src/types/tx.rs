06cc184bc339dd72ab54ff0dd3d630d68a812b62
Justin R. Evans <jurevans@gmail.com>
2022-10-09 06:33:03 -0400

    Additional test coverage

--- packages/shared/lib/src/types/tx.rs
+++ packages/shared/lib/src/types/tx.rs
@@ -12,3 +12,23 @@ impl Tx {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use wasm_bindgen_test::*;
+
+    #[wasm_bindgen_test]
+    fn can_generate_a_proto_tx() {
+        let tx_code = vec![];
+        let tx_data = vec![];
+
+        // Verify that we can call methods native to namada::proto::Tx
+        let tx_proto = Tx::to_proto(tx_code, &tx_data);
+        let bytes = tx_proto.to_bytes();
+        let hash = tx_proto.hash();
+
+        assert_eq!(bytes.len(), 10);
+        assert_eq!(hash.len(), 32);
+    }
+}
--- packages/shared/lib/src/types/wrapper.rs
+++ packages/shared/lib/src/types/wrapper.rs
@@ -7,10 +7,6 @@ use std::str::FromStr;
 pub struct WrapperTx(pub(crate) transaction::WrapperTx);
 
 impl WrapperTx {
-    pub fn new(wrapper_tx: transaction::WrapperTx) -> Self {
-        Self(wrapper_tx)
-    }
-
     pub fn wrap(
         token: address::Address,
         fee_amount: u64,
@@ -56,5 +52,53 @@ impl WrapperTx {
             vec![],
             &wrapped_tx,
         )).sign(&keypair))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use namada::types::{address, key};
+    use wasm_bindgen_test::*;
+
+    #[wasm_bindgen_test]
+    fn can_generate_a_wrapper_tx() {
+        let secret = "1498b5467a63dffa2dc9d9e069caf075d16fc33fdd4c3b01bfadae6433767d93";
+        let expected_fee_payer = "atest1d9khqw36x5cnvvjpgfzyxsjpgfqnqwf5xpq5zv34gvunswp4g3znww2yxqursdpnxdz5yw2ypna253";
+        let token = address::Address::from_str("atest1v4ehgw36x3prswzxggunzv6pxqmnvdj9xvcyzvpsggeyvs3cg9qnywf589qnwvfsg5erg3fkl09rg5")
+            .expect("Should create address from string slice");
+        let epoch: u64 = 5;
+        let fee_amount: u64 = 1000;
+        let gas_limit: u64 = 1_000_000;
+        let tx_code = vec![];
+        let tx_data = vec![];
+        let secret_key = SecretKey::Ed25519(
+            key::ed25519::SecretKey::from_str(secret)
+                .expect("Should encode ed25519 from secret")
+        );
+
+        let tx = Tx::to_proto(
+            tx_code,
+            &tx_data,
+        ).sign(&secret_key);
+
+        let wrapper_tx = WrapperTx::wrap(
+            token,
+            fee_amount,
+            secret,
+            epoch,
+            gas_limit,
+            tx,
+        ).expect("Creating WrapperTx should not fail");
+
+        // Verify that we can call methods native to namada::types::transaction::WrapperTx
+        let bytes = wrapper_tx.try_to_vec().unwrap();
+        assert_eq!(bytes.len(), 370);
+
+        let is_valid_ciphertext = wrapper_tx.validate_ciphertext();
+        assert!(is_valid_ciphertext);
+
+        let fee_payer = wrapper_tx.fee_payer();
+        assert_eq!(fee_payer.to_string(), expected_fee_payer);
     }
 }
