06ef35ef278f9a2b61abb936303103089c253166
Justin R. Evans <jurevans@gmail.com>
2022-12-01 05:58:27 -0500

    Hook namada-interface up to new chains config and type

--- packages/chains/src/index.ts
+++ packages/chains/src/index.ts
@@ -1,3 +1,11 @@
-export * from "./chains/cosmos";
-export * from "./chains/namada";
-export * from "./chains/osmosis";
+import cosmos from "./chains/cosmos";
+import namada from "./chains/namada";
+import osmosis from "./chains/osmosis";
+
+export const defaultChainId = namada.chainId;
+
+export const chains = {
+  [cosmos.chainId]: cosmos,
+  [namada.chainId]: namada,
+  [osmosis.chainId]: osmosis,
+};
