2d92191ff0ec078094f99cba97fafbb6be1878a5
Justin R. Evans <jurevans@gmail.com>
2022-12-06 05:48:40 -0500

    Fix initialization of Anoma extension in context

--- packages/integrations/src/Anoma.ts
+++ packages/integrations/src/Anoma.ts
@@ -9,29 +9,36 @@ import {
 import { Integration } from "types/Integration";
 
 export default class Anoma implements Integration<Account, Signer> {
-  constructor(
-    public readonly chain: Chain,
-    private readonly _anoma = (<WindowWithAnoma>window)?.anoma
-  ) {}
+  private _anoma: WindowWithAnoma["anoma"] | undefined;
+
+  constructor(public readonly chain: Chain) {}
 
   public get instance(): IAnoma | undefined {
     return this._anoma;
   }
 
+  private _init(): void {
+    if (!this._anoma) {
+      this._anoma = (<WindowWithAnoma>window)?.anoma;
+    }
+  }
+
   public detect(): boolean {
+    this._init();
     return !!this._anoma;
   }
 
   public async connect(): Promise<void> {
-    await this._anoma.connect(this.chain.chainId);
+    this._init();
+    await this._anoma?.connect(this.chain.chainId);
   }
 
   public async accounts(): Promise<readonly Account[] | undefined> {
-    const signer = this._anoma.getSigner(this.chain.chainId);
-    return await signer.accounts();
+    const signer = this._anoma?.getSigner(this.chain.chainId);
+    return await signer?.accounts();
   }
 
   public signer(): Signer | undefined {
-    return this._anoma.getSigner(this.chain.chainId);
+    return this._anoma?.getSigner(this.chain.chainId);
   }
 }
