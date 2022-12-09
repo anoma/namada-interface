b69edfa708a0ae5ca43094fc41d0a1807d4fb229
Justin R. Evans <jurevans@gmail.com>
2022-12-09 07:41:18 -0500

    Remove logs

--- apps/namada-interface/src/App/Token/TokenSend/TokenSendForm.tsx
+++ apps/namada-interface/src/App/Token/TokenSend/TokenSendForm.tsx
@@ -164,14 +164,12 @@ const TokenSendForm = ({
   );
 
   const account: Account = derivedAccounts[address];
-  console.log({ account });
   const isShieldedSource = account.isShielded;
   const token = Tokens[tokenType] || {};
 
   const balances = balancesForChain[address] || {};
   const balance = balances[tokenType] || 0;
 
-  console.log({ tokenType, balances, balance });
   const isFormInvalid = getIsFormInvalid(
     target,
     amount,
@@ -295,11 +293,10 @@ const TokenSendForm = ({
     targetAddress: string | undefined
   ): string | undefined => {
     const balance = balancesForChain[address][token] || 0;
-    console.log({ address, token, balance, balancesForChain });
 
     const transferTypeBasedOnTarget =
       targetAddress && parseTarget(targetAddress);
-    console.log({ transferTypeBasedOnTarget });
+
     if (transferTypeBasedOnTarget === TransferType.Shielded) {
       return undefined;
     }
