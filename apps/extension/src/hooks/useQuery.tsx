eef1dc4fe165f287973a5e6c93c17f39a4990101
Eric Corson <public@giraffedev.com>
2023-05-01 17:10:22 +0900

    feat: sanitize query params in extension

--- apps/extension/src/hooks/useQuery.tsx
+++ apps/extension/src/hooks/useQuery.tsx
@@ -1,9 +1,23 @@
 import { useMemo } from "react";
 import { useLocation } from "react-router-dom";
+import { sanitize } from "dompurify";
 
-const useQuery = (): URLSearchParams => {
+interface SanitizedURLSearchParams {
+  get(name: string): string | null;
+};
+
+const toSanitized = (urlSearchParams: URLSearchParams): SanitizedURLSearchParams => ({
+  get: (name: string): string | null => {
+    const unsanitized = urlSearchParams.get(name);
+    return unsanitized === null ?
+      unsanitized :
+      sanitize(unsanitized);
+  }
+});
+
+const useQuery = (): SanitizedURLSearchParams => {
   const { search } = useLocation();
-  return useMemo(() => new URLSearchParams(search), [search]);
+  return useMemo(() => toSanitized(new URLSearchParams(search)), [search]);
 };
 
 export default useQuery;
