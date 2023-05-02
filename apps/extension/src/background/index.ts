4ac1eeb7702b02b2d751995f6e5fc0e2199149e3
Justin R. Evans <jurevans@gmail.com>
2023-05-02 06:19:26 -0400

    Simplified event broadcast and provider in interface

--- apps/extension/src/background/index.ts
+++ apps/extension/src/background/index.ts
@@ -15,7 +15,6 @@ import {
 import { Ports, KVPrefix } from "router";
 import { ChainsService, init as initChains } from "./chains";
 import { KeyRingService, init as initKeyRing, SDK_KEY } from "./keyring";
-import { ContentService, init as initContentEvents } from "./content";
 
 const messenger = new ExtensionMessenger();
 const store = new IndexedDBKVStore(KVPrefix.IndexedDB);
@@ -55,7 +54,6 @@ const { REACT_APP_NAMADA_URL = DEFAULT_URL } = process.env;
     sdk.decode(sdkData);
   }
 
-  const contentService = new ContentService(requester);
   const chainsService = new ChainsService(store, [chains[defaultChainId]]);
   const keyRingService = new KeyRingService(
     store,
@@ -70,7 +68,6 @@ const { REACT_APP_NAMADA_URL = DEFAULT_URL } = process.env;
   // Initialize messages and handlers
   initChains(router, chainsService);
   initKeyRing(router, keyRingService);
-  initContentEvents(router, contentService);
 
   router.listen(Ports.Background);
 })();
