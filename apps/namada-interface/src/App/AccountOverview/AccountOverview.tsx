8430f7d71415df5fd6dc2f73bffacfa87f720c94
Justin R. Evans <jurevans@gmail.com>
2023-05-02 08:53:32 -0400

    Clean up

--- apps/namada-interface/src/App/AccountOverview/AccountOverview.tsx
+++ apps/namada-interface/src/App/AccountOverview/AccountOverview.tsx
@@ -76,6 +76,7 @@ export const AccountOverview = (): JSX.Element => {
   const handleConnectExtension = async (): Promise<void> => {
     withConnection(
       async () => {
+        await integration.connect();
         const accounts = await integration?.accounts();
         if (accounts) {
           dispatch(addAccounts(accounts as Account[]));
