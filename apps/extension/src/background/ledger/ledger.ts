f68835317af3703a7f363cece0334facb405c7bc
Justin R. Evans <jurevans@gmail.com>
2023-05-17 08:14:45 -0400

    Complete basic functions of Ledger class

--- apps/extension/src/background/ledger/ledger.ts
+++ apps/extension/src/background/ledger/ledger.ts
@@ -28,6 +28,9 @@ export class Ledger {
 
   constructor(public readonly namadaApp: NamadaApp | undefined = undefined) {}
 
+  /**
+   * Returns an initialized Ledger class instance with initialized Transport
+   */
   static async init(): Promise<Ledger> {
     const transportHID = await initLedgerHIDTransport();
 
@@ -40,14 +43,80 @@ export class Ledger {
     return ledger;
   }
 
-  public async getPublicKey(bip44Path?: string): Promise<string> {
-    const path = bip44Path || DEFAULT_LEDGER_BIP44_PATH;
-    const pk = await this.namadaApp?.getAddressAndPubKey(path);
-
-    if (pk && pk.publicKey) {
-      return pk.publicKey.toString("hex");
+  /**
+   * Return status and version info of initialized NamadaApp
+   */
+  public status(): {
+    appName: string;
+    appVersion: string;
+    errorMessage: string;
+    returnCode: number;
+    deviceLocked: boolean;
+    major: number;
+    minor: number;
+    patch: number;
+    targetId: string;
+    testMode: boolean;
+  } {
+    if (!this.version || !this.info) {
+      throw new Error("NamadaApp is not initialized!");
     }
 
-    return Promise.reject(`No public key found for ${this.info?.appName}!`);
+    const { appName, appVersion, errorMessage, returnCode } = this.info;
+    const {
+      major,
+      minor,
+      patch,
+      targetId,
+      deviceLocked = false,
+      testMode,
+    } = this.version;
+
+    return {
+      appName,
+      appVersion,
+      errorMessage,
+      returnCode,
+      deviceLocked,
+      major,
+      minor,
+      patch,
+      targetId,
+      testMode,
+    };
+  }
+
+  /**
+   * Get public key associated with optional path, otherwise, use default path
+   */
+  public async getAddressAndPublicKey(
+    bip44Path?: string
+  ): Promise<{ address: Buffer; publicKey: Buffer }> {
+    if (!this.namadaApp) {
+      throw new Error("NamadaApp is not initialized!");
+    }
+
+    const path = bip44Path || DEFAULT_LEDGER_BIP44_PATH;
+    const { address, publicKey } = await this.namadaApp.getAddressAndPubKey(
+      path
+    );
+
+    return {
+      address,
+      publicKey,
+    };
+  }
+
+  /**
+   * Sign tx bytes with the key associated with provided (or default) path
+   */
+  public async sign(tx: ArrayBuffer, bip44Path: string): Promise<ArrayBuffer> {
+    if (!this.namadaApp) {
+      throw new Error("NamadaApp is not initialized!");
+    }
+
+    const path = bip44Path || DEFAULT_LEDGER_BIP44_PATH;
+
+    return await this.namadaApp.sign(path, Buffer.from(tx));
   }
 }
