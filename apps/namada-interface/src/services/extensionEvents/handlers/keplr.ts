7362742f141e50d81b8c17277b69954a8d1c9a11
Justin R. Evans <jurevans@gmail.com>
2023-06-30 08:04:03 -0400

    Remove unnecessary log

--- apps/namada-interface/src/services/extensionEvents/handlers/keplr.ts
+++ apps/namada-interface/src/services/extensionEvents/handlers/keplr.ts
@@ -7,7 +7,6 @@ import { addAccounts, fetchBalances } from "slices/accounts";
 export const KeplrAccountChangedHandler =
   (dispatch: Dispatch<unknown>, integration: Keplr) => async () => {
     const accounts = await integration.accounts();
-    console.log("KEPLR accounts", { accounts });
 
     if (accounts) {
       dispatch(addAccounts(accounts));
