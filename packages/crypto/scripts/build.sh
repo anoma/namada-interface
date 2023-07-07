de7c15a8fe97c3f05512f4ee1a44f53696f0765b
Mateusz Jasiuk <mateusz.jasiuk@gmail.com>
2023-07-07 11:35:20 +0200

    refactor: use wasm-pack for builds

--- packages/crypto/scripts/build.sh
+++ packages/crypto/scripts/build.sh
@@ -3,19 +3,21 @@
 SCRIPT_DIR=$(cd "$(dirname "$0")"; pwd -P)
 
 path=""
+profile=""
 
 if [ "$1" = "" ]
 then
     echo "Building \"crypto\" in dev mode."
+    profile="--dev"
     path="debug"
 elif [ "$1" = "--release" ]
 then
     echo "Building \"crypto\" in release mode."
+    profile="--release"
     path="release"
 else
     echo "Unsupported build mode \"$1\""
     exit 1
 fi
 
-cd $SCRIPT_DIR/../lib; cargo build --target wasm32-unknown-unknown ${1} $features
-wasm-bindgen --out-dir=$SCRIPT_DIR/../src/crypto --target=web --omit-default-module-path target/wasm32-unknown-unknown/$path/crypto.wasm
+wasm-pack build $SCRIPT_DIR/../lib $profile --target web --out-dir $SCRIPT_DIR/../src/crypto
--- packages/shared/scripts/build.sh
+++ packages/shared/scripts/build.sh
@@ -4,20 +4,22 @@ SCRIPT_DIR=$(cd "$(dirname "$0")"; pwd -P)
 
 features=""
 path=""
+profile=""
 
 if [ "$1" = "" ]
 then
     echo "Building \"shared\" in dev mode."
+    profile="--dev"
     features="--features dev"
     path="debug"
 elif [ "$1" = "--release" ]
 then
     echo "Building \"shared\" in release mode."
+    profile="--release"
     path="release"
 else
     echo "Unsupported build mode \"$1\""
     exit 1
 fi
 
-cd $SCRIPT_DIR/../lib; cargo build --target wasm32-unknown-unknown ${1} $features
-wasm-bindgen --out-dir=$SCRIPT_DIR/../src/shared --target=web --omit-default-module-path target/wasm32-unknown-unknown/$path/shared.wasm
+wasm-pack build $SCRIPT_DIR/../lib $profile --target web --out-dir $SCRIPT_DIR/../src/shared -- $features
