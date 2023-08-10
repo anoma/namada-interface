022081debcd5dc33308f9b047fde6824b6fabe74
Mateusz Jasiuk <mateusz.jasiuk@gmail.com>
2023-08-10 09:20:08 +0200

    tests: e2e CI

---
+++ e2e/build-wasm.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+SCRIPT_DIR=$(cd "$(dirname "$0")"; pwd -P)
+PACKAGES_PATH="../packages"
+
+suffix=""
+
+if [ "$1" != "--release" ]
+then
+    suffix=":dev"
+fi
+
+cd "${SCRIPT_DIR}/${PACKAGES_PATH}/crypto" && yarn wasm:build$suffix
+cd "${SCRIPT_DIR}/${PACKAGES_PATH}/shared" && yarn wasm:build$suffix
--- e2e/setup-frontend.sh
+++ e2e/setup-frontend.sh
@@ -1,6 +1,15 @@
 #!/bin/bash -x
 CHAIN_ID="dev-test.000000000000000000000"
 
+ext_mode=""
+interface_mode=""
+
+if [ "$1" = "--watch" ]
+then
+    ext_mode="$1"
+    interface_mode="$1 &"
+fi
+
 # Run the extension
 cd ../apps/extension && \
     yarn clean:chrome && \
@@ -8,7 +17,7 @@ cd ../apps/extension && \
     export TARGET=chrome; \
     export REACT_APP_NAMADA_CHAIN_ID=${CHAIN_ID}; \
     export REACT_APP_NAMADA_URL=http://127.0.0.1:27657 && \
-    npx webpack --watch &
+    npx webpack $ext_mode &
 
 # Run the interface
 cd ../namada-interface && \
@@ -16,13 +25,13 @@ cd ../namada-interface && \
     export REACT_APP_LOCAL=true; \
     export REACT_APP_NAMADA_CHAIN_ID=${CHAIN_ID}; \
     export REACT_APP_NAMADA_URL=http://127.0.0.1:27657 && \
-    npx webpack --watch &
+    npx webpack $interface_mode
 
 cd ../../e2e
 
 npx http-server -p 8080 -c-1 ../apps/namada-interface/build
 
-# Sleep indefinitely
+# Sleep indefinitely/ TODO: check if needed as http-server is running
 while :; do sleep 86400; done
 
 # Kill all processes on exit
