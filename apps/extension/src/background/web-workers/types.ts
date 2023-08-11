fffeb8d6bfb156d16704080f0c6a0d804a586b18
Eric Corson <13696952+emccorson@users.noreply.github.com>
2023-08-11 20:23:51 +0900

    fix: show web worker errors in extension console (#359)

--- apps/extension/src/background/web-workers/types.ts
+++ apps/extension/src/background/web-workers/types.ts
@@ -15,9 +15,11 @@ export type SubmitTransferMessageData = {
 export const INIT_MSG = "init";
 export const TRANSFER_SUCCESSFUL_MSG = "transfer-successful";
 export const TRANSFER_FAILED_MSG = "transfer-failed";
+export const WEB_WORKER_ERROR_MSG = "web-worker-error";
 export type MsgName =
   | typeof INIT_MSG
   | typeof TRANSFER_FAILED_MSG
-  | typeof TRANSFER_SUCCESSFUL_MSG;
+  | typeof TRANSFER_SUCCESSFUL_MSG
+  | typeof WEB_WORKER_ERROR_MSG;
 
 export type Msg = { msgName: MsgName; payload?: string };
