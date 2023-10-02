9b439127f171a5991434d6c45561c6977c23def5
Mateusz Jasiuk <mateusz.jasiuk@gmail.com>
2023-10-02 11:40:24 +0200

    test(e2e): transfer from transparent to shielded address

--- e2e/src/utils/values.ts
+++ e2e/src/utils/values.ts
@@ -15,4 +15,8 @@ export const mnemonic = [
 
 export const pwdOrAlias = "Asd";
 export const address0Alias = "address0";
+export const address1 =
+  "atest1d9khqw36x9zr2s6pxymrv3z9xcen2s33gvmrxsfjgccnzd2rxez5z3fex5urgsjzg4qnsw2pef6prn";
+export const shieldedAddress0 =
+  "patest1fwnveqn8urtj4j0gckqyw2lfmza5gw7ry4gwah7u05wwkay92eypkjstufj9tt8j8dnswq06yjt";
 export const shieldedAddress0Alias = "shieldedAddress0";
