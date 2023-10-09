dd217e2ed58aabf2467bd0f920995f54bf787baf
Mateusz Jasiuk <mateusz.jasiuk@gmail.com>
2023-10-09 02:23:58 +0200

    fix: rebase

--- packages/shared/lib/src/sdk/mod.rs
+++ packages/shared/lib/src/sdk/mod.rs
@@ -591,7 +591,7 @@ impl Sdk {
         let (args, faucet_signer) = tx::submit_vote_proposal_tx_args(tx_msg, password)?;
         let source = args.voter.clone();
         let default_signer = faucet_signer.clone().or(Some(source.clone()));
-        let signing_data = aux_signing_data::<_, _, DefaultIo>(
+        let signing_data = aux_signing_data::<_, _, WebIo>(
             &self.client,
             &mut self.wallet,
             &args.tx.clone(),
@@ -601,7 +601,7 @@ impl Sdk {
         .await?;
         let current_epoch = query_epoch(&self.client).await?;
 
-        let (tx, _) = build_vote_proposal::<_, _, _, DefaultIo>(
+        let (tx, _) = build_vote_proposal::<_, _, _, WebIo>(
             &mut self.client,
             &mut self.wallet,
             &mut self.shielded_ctx,
