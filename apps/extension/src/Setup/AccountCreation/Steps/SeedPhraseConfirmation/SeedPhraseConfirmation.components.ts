f217b054b22f2411a19382be79d72408660cbb5a
Pedro Rezende <xpedrorezende@gmail.com>
2023-10-28 10:50:51 -0300

    feat(extension,setup): lots of changes on Confirmation page in order to reflect the new UI flow

--- apps/extension/src/Setup/AccountCreation/Steps/SeedPhraseConfirmation/SeedPhraseConfirmation.components.ts
+++ apps/extension/src/Setup/AccountCreation/Steps/SeedPhraseConfirmation/SeedPhraseConfirmation.components.ts
@@ -1,9 +1,16 @@
+import { borderRadius, color, spacement } from "@namada/utils";
 import styled from "styled-components";
 
-export const DescriptionAndInputContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  height: 100%;
-  width: 100%;
+export const VerifyPanelContainer = styled.div`
+  background-color: ${color("utility1", "main75")};
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  justify-content: center;
+  border-radius: ${borderRadius("md")};
+  padding: ${spacement(4)} ${spacement(17)} ${spacement(7)};
+  grid-gap: ${spacement(9)};
+`;
+
+export const Footer = styled.footer`
+  margin-top: ${spacement(14)};
 `;
