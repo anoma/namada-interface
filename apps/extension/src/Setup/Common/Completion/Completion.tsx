ab0f53ab6e1c770aaacef015352896d220d01941
Pedro Rezende <xpedrorezende@gmail.com>
2023-10-29 16:56:31 -0300

    feat(extension, setup): creating new Completion page

--- apps/extension/src/Setup/Common/Completion/Completion.tsx
+++ apps/extension/src/Setup/Common/Completion/Completion.tsx
@@ -1,19 +1,24 @@
-import React, { useEffect, useState } from "react";
 import { ExtensionRequester } from "extension";
+import React, { useEffect, useState } from "react";
 import browser from "webextension-polyfill";
 
-import { Button, ButtonVariant } from "@namada/components";
+import {
+  ActionButton,
+  Heading,
+  Input,
+  InputVariants,
+  Stack,
+  Textarea,
+} from "@namada/components";
 import { SaveMnemonicMsg, ScanAccountsMsg } from "background/keyring";
 import { Ports } from "router";
-
+import { HeaderContainer, Subtitle } from "Setup/Setup.components";
+import { Loading } from "../Loading";
 import {
-  BodyText,
-  ButtonsContainer,
-  Header1,
-  SubViewContainer,
-  UpperContentContainer,
-} from "Setup/Setup.components";
-import { StatusInfo, StatusLoader } from "./Completion.components";
+  DownloadPanel,
+  WarningPanel,
+  WarningPanelTitle,
+} from "./Completion.components";
 
 type Props = {
   alias: string;
@@ -31,10 +36,19 @@ enum Status {
 
 const Completion: React.FC<Props> = (props) => {
   const { alias, mnemonic, password, requester, scanAccounts } = props;
-  const [mnemonicStatus, setMnemonicStatus] = useState<Status>(Status.Pending);
+  const [mnemonicStatus, setMnemonicStatus] = useState<Status>(
+    Status.Completed
+  );
   const [statusInfo, setStatusInfo] = useState(
     "Encrypting and storing mnemonic."
   );
+
+  const closeCurrentTab = async (): Promise<void> => {
+    const tab = await browser.tabs.getCurrent();
+    if (tab.id) {
+      browser.tabs.remove(tab.id);
+    }
+  };
 
   useEffect(() => {
     const saveMnemonic = async (): Promise<void> => {
@@ -43,7 +57,6 @@ const Completion: React.FC<Props> = (props) => {
           Ports.Background,
           new SaveMnemonicMsg(mnemonic, password, alias)
         );
-
         if (scanAccounts) {
           setStatusInfo("Scanning accounts.");
           await requester.sendMessage<ScanAccountsMsg>(
@@ -62,40 +75,61 @@ const Completion: React.FC<Props> = (props) => {
   }, [alias, mnemonic, password, scanAccounts]);
 
   return (
-    <SubViewContainer>
-      <UpperContentContainer>
-        <Header1>Creating your wallet</Header1>
-        {mnemonicStatus === Status.Completed && (
-          <BodyText>
-            Setup is complete! You may close this tab and access the extension
-            popup to view your accounts.
-          </BodyText>
-        )}
-        {mnemonicStatus !== Status.Completed && (
-          <BodyText>One moment while your wallet is being created...</BodyText>
-        )}
-        <StatusInfo>
-          <StatusLoader
-            className={mnemonicStatus === Status.Pending ? "is-loading" : ""}
-          />
-          <BodyText>{statusInfo}</BodyText>
-        </StatusInfo>
-      </UpperContentContainer>
-      <ButtonsContainer>
-        <Button
-          variant={ButtonVariant.Contained}
-          onClick={async () => {
-            const tab = await browser.tabs.getCurrent();
-            if (tab.id) {
-              browser.tabs.remove(tab.id);
-            }
-          }}
-          disabled={mnemonicStatus !== Status.Completed}
-        >
-          Close
-        </Button>
-      </ButtonsContainer>
-    </SubViewContainer>
+    <>
+      <Loading
+        status={statusInfo}
+        visible={mnemonicStatus === Status.Pending}
+      />
+      {mnemonicStatus === Status.Completed && (
+        <>
+          <HeaderContainer>
+            <Heading level="h1" size="3xl">
+              Namada Keys Created
+            </Heading>
+            <Subtitle>Here are the accounts generated from your keys</Subtitle>
+          </HeaderContainer>
+          <Stack as="section" gap={8}>
+            <Stack gap={4}>
+              <Input
+                label="Your Transparent Account"
+                variant={InputVariants.ReadOnlyCopy}
+                value="<transparent-account>"
+                theme={"primary"}
+              />
+              <Textarea
+                label="Your Shielded Account"
+                readOnly={true}
+                value="<transparent-account>"
+                theme={"secondary"}
+                sensitive={true}
+              />
+              <DownloadPanel>
+                Viewing keys of shielded account
+                <ActionButton size="base" variant="secondary">
+                  Download
+                </ActionButton>
+              </DownloadPanel>
+            </Stack>
+            <Stack as="footer" gap={4}>
+              <WarningPanel>
+                <WarningPanelTitle>
+                  BEFORE SHARING YOUR VIEWING KEYS
+                </WarningPanelTitle>
+                <p>
+                  Note that ANYONE with your viewing keys can see the assets,
+                  transaction value, memo field and receiver address of all
+                  transactions received by or sent by the corresponding shielded
+                  account.
+                </p>
+              </WarningPanel>
+              <ActionButton onClick={closeCurrentTab}>
+                Close this page
+              </ActionButton>
+            </Stack>
+          </Stack>
+        </>
+      )}
+    </>
   );
 };
 
