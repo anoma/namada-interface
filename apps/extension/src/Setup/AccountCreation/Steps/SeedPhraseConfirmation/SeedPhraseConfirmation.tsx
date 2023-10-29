a3abbd086db5ff3e2dc69aca38ed95951c7d61ea
Pedro Rezende <xpedrorezende@gmail.com>
2023-10-29 16:51:31 -0300

    feat(extension,setup): wrapping seed confirmation with a form and handling submit

--- apps/extension/src/Setup/AccountCreation/Steps/SeedPhraseConfirmation/SeedPhraseConfirmation.tsx
+++ apps/extension/src/Setup/AccountCreation/Steps/SeedPhraseConfirmation/SeedPhraseConfirmation.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 import { ActionButton, Heading, Input, Stack } from "@namada/components";
 import { Password } from "Setup/Common";
-import { HeaderContainer, Subtitle } from "Setup/Setup.components";
+import { Form, HeaderContainer, Subtitle } from "Setup/Setup.components";
 import { AccountDetails } from "Setup/types";
 import {
   Footer,
@@ -16,14 +16,13 @@ type SeedPhraseConfirmationProps = {
   seedPhrase: string[];
   accountCreationDetails?: AccountDetails;
   onConfirm: (accountCreationDetails: AccountDetails) => void;
-  onSetAccountCreationDetails: (accountCreationDetails: AccountDetails) => void;
 };
 
 const SeedPhraseConfirmation = (
   props: SeedPhraseConfirmationProps
 ): JSX.Element => {
   const navigate = useNavigate();
-  const { seedPhrase, onConfirm } = props;
+  const { seedPhrase, accountCreationDetails, onConfirm } = props;
 
   const [verificationInput1, setVerificationInput1] = useState("");
   const [index1ToConfirm, setIndex1ToConfirm] = useState(-1);
@@ -33,6 +32,7 @@ const SeedPhraseConfirmation = (
   const [index2Error, setIndex2Error] = useState("");
   const [password, setPassword] = useState<string | null>(null);
   const [keysName, setKeysName] = useState<string>("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   const seedPhraseLength = seedPhrase.length;
   const notVerified =
@@ -58,20 +58,6 @@ const SeedPhraseConfirmation = (
     setIndex2ToConfirm(index2);
   }, []);
 
-  const onSubmit = (): void => {
-    // if (isPasswordValid && alias && !isSubmitting) {
-    //   setIsSubmitting(true);
-    //   const accountCreationDetailsToSubmit: AccountDetails = {
-    //     ...accountCreationDetails,
-    //     alias,
-    //     password,
-    //   };
-    //   // Wait a moment for setStore to finish, then navigate
-    //   // so as not to block animations
-    //   onSubmitAccountCreationDetails(accountCreationDetailsToSubmit);
-    // }
-  };
-
   const verifyWord = (
     word: string,
     userInput: string,
@@ -89,6 +75,19 @@ const SeedPhraseConfirmation = (
     stateErrorFn("");
   };
 
+  const onSubmitForm = (e: React.FormEvent): void => {
+    e.preventDefault();
+    if (notVerified || isSubmitting) return;
+
+    setIsSubmitting(true);
+    const accountCreationDetailsToSubmit: AccountDetails = {
+      ...accountCreationDetails,
+      alias: keysName,
+      password,
+    };
+    onConfirm(accountCreationDetailsToSubmit);
+  };
+
   return (
     <>
       <HeaderContainer>
@@ -97,51 +96,50 @@ const SeedPhraseConfirmation = (
         </Heading>
         <Subtitle>fill out the words according to their numbers</Subtitle>
       </HeaderContainer>
-
-      <Stack as="form" gap={5}>
-        <VerifyPanelContainer>
-          <Input
-            label={`Word #${index1ToConfirm + 1}`}
-            value={verificationInput1}
-            error={index1Error}
-            onBlur={() => {
-              verifyWord(
-                seedPhrase[index1ToConfirm],
-                verificationInput1,
-                setIndex1Error
-              );
-            }}
-            onChange={(event) => {
-              setVerificationInput1(event.target.value);
-            }}
+      <Form onSubmit={onSubmitForm}>
+        <Stack gap={5}>
+          <VerifyPanelContainer>
+            <Input
+              label={`Word #${index1ToConfirm + 1}`}
+              value={verificationInput1}
+              error={index1Error}
+              onBlur={() => {
+                verifyWord(
+                  seedPhrase[index1ToConfirm],
+                  verificationInput1,
+                  setIndex1Error
+                );
+              }}
+              onChange={(event) => {
+                setVerificationInput1(event.target.value);
+              }}
+            />
+            <Input
+              label={`Word #${index2ToConfirm + 1}`}
+              value={verificationInput2}
+              error={index2Error}
+              onBlur={() => {
+                verifyWord(
+                  seedPhrase[index2ToConfirm],
+                  verificationInput2,
+                  setIndex2Error
+                );
+              }}
+              onChange={(event) => {
+                setVerificationInput2(event.target.value);
+              }}
+            />
+          </VerifyPanelContainer>
+          <Password
+            keysName={keysName}
+            onChangeKeysName={setKeysName}
+            onValidPassword={setPassword}
           />
-          <Input
-            label={`Word #${index2ToConfirm + 1}`}
-            value={verificationInput2}
-            error={index2Error}
-            onBlur={() => {
-              verifyWord(
-                seedPhrase[index2ToConfirm],
-                verificationInput2,
-                setIndex2Error
-              );
-            }}
-            onChange={(event) => {
-              setVerificationInput2(event.target.value);
-            }}
-          />
-        </VerifyPanelContainer>
-        <Password
-          keysName={keysName}
-          onChangeKeysName={setKeysName}
-          onValidPassword={setPassword}
-        />
-      </Stack>
-      <Footer>
-        <ActionButton disabled={notVerified} onClick={onSubmit}>
-          Next
-        </ActionButton>
-      </Footer>
+        </Stack>
+        <Footer>
+          <ActionButton disabled={notVerified}>Next</ActionButton>
+        </Footer>
+      </Form>
     </>
   );
 };
