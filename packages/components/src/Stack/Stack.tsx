22f1ae52f137476cfdf32c8587cac0db4090a0e4
Pedro Rezende <xpedrorezende@gmail.com>
2023-10-30 16:41:26 -0300

    feat(components): accepting elements attributes depending on 'as' property type

--- packages/components/src/Stack/Stack.tsx
+++ packages/components/src/Stack/Stack.tsx
@@ -1,21 +1,21 @@
 import { Sizing } from "@namada/utils";
 import { StackContainer } from "./Stack.components";
 
-type StackProps = {
+type StackProps<T extends keyof JSX.IntrinsicElements> = {
   children: React.ReactNode;
   gap: keyof Sizing;
   direction?: "vertical" | "horizontal";
-  as?: keyof JSX.IntrinsicElements;
+  as?: T;
 };
 
-export const Stack = ({
-  as = "ul",
+export const Stack = <T extends keyof JSX.IntrinsicElements>({
   children,
   gap = 4,
   direction = "vertical",
-}: StackProps): JSX.Element => {
+  ...props
+}: StackProps<T> & React.ComponentPropsWithoutRef<T>): JSX.Element => {
   return (
-    <StackContainer as={as} gap={gap} direction={direction}>
+    <StackContainer as={props.as} gap={gap} direction={direction} {...props}>
       {children}
     </StackContainer>
   );
