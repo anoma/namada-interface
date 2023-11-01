41b9275efe4bb7723ab339ba170ab9b30c9ad897
Pedro Rezende <xpedrorezende@gmail.com>
2023-11-01 19:40:26 -0300

    feat(extension,app): Settings page becomes ParentAccounts + ux improvements

---
+++ apps/extension/src/App/Accounts/ParentAccounts.tsx
@@ -0,0 +1,166 @@
+import React, { useEffect, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import browser from "webextension-polyfill";
+
+import {
+  ActionButton,
+  Alert,
+  Heading,
+  KeyListItem,
+  Stack,
+  Text,
+} from "@namada/components";
+import { DerivedAccount } from "@namada/types";
+
+import {
+  LockKeyRingMsg,
+  ParentAccount,
+  QueryParentAccountsMsg,
+  SetActiveAccountMsg,
+} from "background/keyring";
+
+import { formatRouterPath } from "@namada/utils";
+import { ExtensionRequester } from "extension";
+import { Ports } from "router";
+import { Status } from "../App";
+import { AccountManagementRoute, TopLevelRoute } from "../types";
+import { SettingsHeader } from "./ParentAccounts.components";
+
+/**
+ * Represents the extension's settings page.
+ */
+const ParentAccounts: React.FC<{
+  activeAccountId: string;
+  requester: ExtensionRequester;
+  onSelectAccount: (account: DerivedAccount) => void;
+}> = ({ activeAccountId, requester, onSelectAccount }) => {
+  const [status, setStatus] = useState<Status>(Status.Pending);
+  const [error, setError] = useState<string>("");
+  const [parentAccounts, setParentAccounts] = useState<DerivedAccount[]>([]);
+
+  const navigate = useNavigate();
+
+  const fetchParentAccounts = async (): Promise<void> => {
+    setStatus(Status.Pending);
+    try {
+      const accounts = await requester.sendMessage(
+        Ports.Background,
+        new QueryParentAccountsMsg()
+      );
+      setParentAccounts(accounts);
+      setStatus(Status.Completed);
+    } catch (e) {
+      console.error(e);
+      setError(`An error occurred while loading extension: ${e}`);
+      setStatus(Status.Failed);
+    }
+  };
+
+  const handleSelectAccount = async (
+    account: DerivedAccount
+  ): Promise<void> => {
+    const { id, type } = account;
+    try {
+      await requester.sendMessage(
+        Ports.Background,
+        new SetActiveAccountMsg(id, type as ParentAccount)
+      );
+
+      // Lock current wallet keyring:
+      await requester.sendMessage(Ports.Background, new LockKeyRingMsg());
+
+      // Fetch accounts for selected parent account
+      onSelectAccount(account);
+    } catch (e) {
+      console.error(e);
+      setError(`An error occurred while setting active account: ${e}`);
+      setStatus(Status.Failed);
+    }
+  };
+
+  // const onDeleteAccount = async (): Promise<void> => {
+  //   await fetchParentAccounts();
+  // };
+
+  const goToSetupPage = (): void => {
+    browser.tabs.create({
+      url: browser.runtime.getURL("setup.html"),
+    });
+  };
+
+  const goToDeletePage = (account: DerivedAccount): void => {
+    navigate(
+      formatRouterPath([
+        TopLevelRoute.Accounts,
+        AccountManagementRoute.DeleteAccount.replace(":accountId", account.id),
+      ]),
+      { state: { account } }
+    );
+  };
+
+  const goToConnectedSites = (): void => {
+    navigate(TopLevelRoute.ConnectedSites);
+  };
+
+  // we use a ref to make sure the effect does not run on the first
+  // render, which would be before parent accounts have loaded
+  const firstRender = useRef(true);
+  useEffect(() => {
+    if (firstRender.current) {
+      firstRender.current = false;
+      return;
+    }
+    (async () => {
+      if (parentAccounts.length === 0) {
+        // the last account has been deleted so navigate to setup
+        navigate(TopLevelRoute.Setup);
+      } else if (!parentAccounts.some(({ id }) => id === activeAccountId)) {
+        // the active account was deleted so make the first account active
+        await handleSelectAccount(parentAccounts[0]);
+      }
+    })();
+  }, [parentAccounts]);
+
+  useEffect(() => {
+    fetchParentAccounts();
+  }, []);
+
+  return (
+    <>
+      <Heading>Namada Keys</Heading>
+      <Stack gap={4}>
+        {error && <Alert type="error">{error}</Alert>}
+        {status === Status.Pending && "loading"}
+        <SettingsHeader>
+          <Text>Set default keys</Text>
+          <ActionButton size="sm" onClick={goToSetupPage}>
+            Add keys
+          </ActionButton>
+        </SettingsHeader>
+        <Stack as="ul" gap={4}>
+          {parentAccounts.map((account) => (
+            <KeyListItem
+              key={`key-listitem-${account.id}`}
+              as="li"
+              alias={account.alias}
+              isMainKey={activeAccountId === account.id}
+              onSelectAccount={() => handleSelectAccount(account)}
+              onRename={() => {}}
+              onDelete={() => goToDeletePage(account)}
+              onViewAccount={() => {}}
+            />
+          ))}
+        </Stack>
+        <ActionButton
+          onClick={goToConnectedSites}
+          variant="secondary"
+          size="sm"
+        >
+          View Connected Sites
+        </ActionButton>
+      </Stack>
+    </>
+  );
+};
+
+export default ParentAccounts;
