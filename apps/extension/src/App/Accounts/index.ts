e1e3fca9db91d8c52d938b1bbb4fc6344689e95b
Pedro Rezende <xpedrorezende@gmail.com>
2023-11-07 01:17:23 +0100

    refactor(extension,app): removing unused component

--- apps/extension/src/App/Accounts/index.ts
+++ apps/extension/src/App/Accounts/index.ts
@@ -1,4 +1,3 @@
-export { default as Accounts } from "./Accounts";
 export { default as AccountListing } from "./AccountListing";
 export { default as AddAccount } from "./AddAccount";
 export * from "./DeleteAccount";
