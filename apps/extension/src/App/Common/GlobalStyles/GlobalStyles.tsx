db1f0f1668c8bfbeb53a0bda1bdcf7bb21d443e8
Pedro Rezende <pedrorezende@users.noreply.github.com>
2023-11-14 19:46:57 -0300

    Styling a few elements (#442)
    * feat(extension): improving app first loading
    
    * feat(extension,app): adding scrollbar styles

--- apps/extension/src/App/Common/GlobalStyles/GlobalStyles.tsx
+++ apps/extension/src/App/Common/GlobalStyles/GlobalStyles.tsx
@@ -5,4 +5,18 @@ export const GlobalStyle = createGlobalStyle`
   body {
     background-color: ${color("utility1", "main70")};
   }
+
+  body::-webkit-scrollbar {
+    width: 0.25rem;
+  }
+
+  body::-webkit-scrollbar-track {
+    box-shadow: inset 0 0 6px black;
+  }
+
+  body::-webkit-scrollbar-thumb {
+    background-color: ${color("utility1", "main50")};
+    outline: 0;
+  }
+
 `;
--- apps/extension/src/App/index.tsx
+++ apps/extension/src/App/index.tsx
@@ -1,18 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { HashRouter } from "react-router-dom";
+import { getTheme } from "@namada/utils";
 import { AccountContextWrapper, VaultContextWrapper } from "context";
+import { HashRouter } from "react-router-dom";
 import { RequesterProvider } from "services";
+import { ThemeProvider } from "styled-components";
 import { App } from "./App";
+import { GlobalStyle } from "./Common/GlobalStyles";
 
 export default ((): void => {
+  const theme = getTheme("dark");
+
   ReactDOM.render(
     <React.StrictMode>
       <HashRouter>
         <RequesterProvider>
           <VaultContextWrapper>
             <AccountContextWrapper>
-              <App />
+              <ThemeProvider theme={theme}>
+                <GlobalStyle />
+                <App />
+              </ThemeProvider>
             </AccountContextWrapper>
           </VaultContextWrapper>
         </RequesterProvider>
