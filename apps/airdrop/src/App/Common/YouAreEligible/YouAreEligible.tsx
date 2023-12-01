d08e9f2aad8301ec159abc1d2d4db03ea67372e4
Pedro Rezende <xpedrorezende@gmail.com>
2023-11-30 21:55:24 -0300

    feat: animating you are eligible

--- apps/airdrop/src/App/Common/YouAreEligible/YouAreEligible.tsx
+++ apps/airdrop/src/App/Common/YouAreEligible/YouAreEligible.tsx
@@ -8,6 +8,8 @@ import {
   YouAreEligibleContainer,
   YouAreEligibleMessage,
 } from "./YouAreEligible.component";
+import { useLayoutEffect, useRef } from "react";
+import gsap, { Expo, Quad } from "gsap";
 
 type YouAreEligibleType = {
   title: string;
@@ -20,10 +22,42 @@ export const YouAreEligible = ({
   accountOrWallet,
   onClaim,
 }: YouAreEligibleType): JSX.Element => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useLayoutEffect(() => {
+    gsap.context(() => {
+      const tl = gsap.timeline();
+
+      tl.set(".message", { x: "+=200" });
+
+      tl.fromTo(
+        ".icon",
+        { scale: 0, rotateZ: "-20deg" },
+        { scale: 1, duration: 1, rotateZ: 0, ease: Expo.easeOut }
+      );
+
+      tl.fromTo(
+        ["h1", "p"],
+        { opacity: 0, y: "+=10" },
+        { opacity: 1, y: 0, ease: Expo.easeOut, stagger: 0.1, duration: 1.5 },
+        0.25
+      );
+
+      tl.to(".message", { x: 0, ease: Expo.easeOut }, "-=0.75");
+
+      tl.fromTo(
+        ".panel",
+        { opacity: 0 },
+        { opacity: 1, duration: 0.25, ease: Quad.easeOut },
+        "-=0.6"
+      );
+    }, [containerRef]);
+  }, []);
+
   return (
-    <YouAreEligibleContainer>
-      <YouAreEligibleMessage>
-        <IconContainer>
+    <YouAreEligibleContainer ref={containerRef}>
+      <YouAreEligibleMessage className="message">
+        <IconContainer className="icon">
           <CheckedIcon />
         </IconContainer>
         <Heading size="4xl" level="h1" themeColor="utility1">
@@ -31,7 +65,7 @@ export const YouAreEligible = ({
         </Heading>
         <p>Congrats you are eligible for the Namada RPGF Drop!</p>
       </YouAreEligibleMessage>
-      <Panel>
+      <Panel className="panel">
         <WalletOrAddressContainer>
           <Stack gap={1}>
             <span>{title}:</span>
