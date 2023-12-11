3318f7ee2359ed648278cc05cd2bbff6b1bde2c3
Mateusz Jasiuk <mateusz.jasiuk@gmail.com>
2023-12-11 12:10:30 +0100

    test: airdrop countdown

--- apps/airdrop/src/App/Main.tsx
+++ apps/airdrop/src/App/Main.tsx
@@ -62,7 +62,7 @@ export const Main: React.FC = () => {
   const githubHandler = useGithubHandler();
   const objectContainerRef = useRef<HTMLDivElement>(null);
   const mainSectionRef = useRef<HTMLDivElement>(null);
-  const endDate = new Date(Date.UTC(2023, 11, 28, 9, 0, 0));
+  const endDate = new Date(Date.UTC(2023, 11, 11, 13, 0, 0));
   const isOngoing = new Date().getTime() <= endDate.getTime();
 
   useEffect(() => {
