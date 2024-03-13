0b71b4cf0b5539b838d54de7686cdc76bc6e4a79
Mateusz Jasiuk <mateusz.jasiuk@gmail.com>
2024-03-13 16:04:06 +0100

    chore: indexWeb and indexNode cleanup

--- packages/sdk/src/indexNode.ts
+++ packages/sdk/src/indexNode.ts
@@ -1,9 +1,12 @@
+/* !!!Make sure that indexNode.ts and indexWeb.ts always have the same API !!! */
+
+import { Sdk } from "@namada/shared";
 import { webcrypto } from "node:crypto";
 export * from "./index";
 
 export { default as initSync } from "./initSync";
 
-export const initAsync = async (): Promise<void> => {
+export const initAsync = async (): Promise<Sdk> => {
   throw new Error("initAsync is not supported in Node.js");
 };
 
--- packages/sdk/src/indexWeb.ts
+++ packages/sdk/src/indexWeb.ts
@@ -1,7 +1,10 @@
+/* !!!Make sure that indexNode.ts and indexWeb.ts always have the same API !!! */
+
+import { Sdk } from "@namada/shared";
 export * from "./index";
 
 export { default as initAsync } from "./initAsync";
 
-export const initSync = async (): Promise<void> => {
+export const initSync = async (): Promise<Sdk> => {
   throw new Error("initSync is not supported in the browser");
 };
