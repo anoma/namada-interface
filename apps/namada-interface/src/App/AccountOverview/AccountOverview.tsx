71ec824fc4e96d94fcb60baaa10ea06a341a2500
Pedro Rezende <xpedrorezende@gmail.com>
2024-03-29 21:31:51 -0300

    feat(interface): starting new navigation

--- apps/namada-interface/src/App/AccountOverview/AccountOverview.tsx
+++ apps/namada-interface/src/App/AccountOverview/AccountOverview.tsx
@@ -1,20 +1,9 @@
 import { useNavigate } from "react-router-dom";
 
-import { ActionButton, Heading, Stack } from "@namada/components";
 import { useUntilIntegrationAttached } from "@namada/integrations";
-import { Chain, TokenType, Tokens } from "@namada/types";
-import { TopLevelRoute } from "App/types";
+import { Chain, TokenType } from "@namada/types";
 import { AccountsState } from "slices/accounts";
 import { useAppSelector } from "store";
-import {
-  HeadingContainer,
-  TotalAmount,
-  TotalAmountFiat,
-  TotalAmountValue,
-  TotalContainer,
-  TotalHeading,
-} from "./AccountOverview.components";
-import { DerivedAccounts } from "./DerivedAccounts";
 
 import { Intro } from "App/Common/Intro/Intro";
 import BigNumber from "bignumber.js";
@@ -51,36 +40,36 @@ export const AccountOverview = (): JSX.Element => {
     <>
       {!isEmptyObject(derived[chain.id]) && (
         <>
-          <HeadingContainer>
-            <div>
-              <TotalHeading>
-                <Heading level="h2">Wallet</Heading>
-              </TotalHeading>
-            </div>
-            <TotalContainer>
-              <TotalAmount>
-                <TotalAmountFiat>
-                  {Tokens[chain.currency.symbol as TokenType].symbol}
-                </TotalAmountFiat>
-                <TotalAmountValue>
-                  {totalNativeBalance.toString()}
-                </TotalAmountValue>
-              </TotalAmount>
-            </TotalContainer>
-          </HeadingContainer>
-          <Stack direction="horizontal" gap={8}>
-            <ActionButton onClick={() => navigate(TopLevelRoute.TokenSend)}>
-              Send
-            </ActionButton>
-            <ActionButton onClick={() => navigate(TopLevelRoute.TokenReceive)}>
-              Receive
-            </ActionButton>
-          </Stack>
-          <DerivedAccounts />
+          {/* <HeadingContainer> */}
+          {/*   <div> */}
+          {/*     <TotalHeading> */}
+          {/*       <Heading level="h2">Wallet</Heading> */}
+          {/*     </TotalHeading> */}
+          {/*   </div> */}
+          {/*   <TotalContainer> */}
+          {/*     <TotalAmount> */}
+          {/*       <TotalAmountFiat> */}
+          {/*         {Tokens[chain.currency.symbol as TokenType].symbol} */}
+          {/*       </TotalAmountFiat> */}
+          {/*       <TotalAmountValue> */}
+          {/*         {totalNativeBalance.toString()} */}
+          {/*       </TotalAmountValue> */}
+          {/*     </TotalAmount> */}
+          {/*   </TotalContainer> */}
+          {/* </HeadingContainer> */}
+          {/* <Stack direction="horizontal" gap={8}> */}
+          {/*   <ActionButton onClick={() => navigate(TopLevelRoute.TokenSend)}> */}
+          {/*     Send */}
+          {/*   </ActionButton> */}
+          {/*   <ActionButton onClick={() => navigate(TopLevelRoute.TokenReceive)}> */}
+          {/*     Receive */}
+          {/*   </ActionButton> */}
+          {/* </Stack> */}
+          {/* <DerivedAccounts /> */}
         </>
       )}
       {isEmptyObject(derived[chain.id]) && (
-        <div className="w-[380px] mx-auto flex items-center">
+        <div className="w-[420px] mx-auto flex items-center">
           <Intro chain={chain} hasExtensionInstalled={hasExtensionInstalled} />
         </div>
       )}
