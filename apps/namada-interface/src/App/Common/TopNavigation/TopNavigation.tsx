2f17373e51b7d4fdbdb3b46d4038e514b7929b3d
Pedro Rezende <xpedrorezende@gmail.com>
2024-03-29 21:31:51 -0300

    feat(interface): starting new navigation

--- apps/namada-interface/src/App/Common/TopNavigation/TopNavigation.tsx
+++ apps/namada-interface/src/App/Common/TopNavigation/TopNavigation.tsx
@@ -1,21 +1,63 @@
+import { ActionButton } from "@namada/components";
 import { Chain } from "@namada/types";
-import { ConnectExtensionButton } from "../ConnectExtensionButton/ConnectExtensionButton";
+import { ConnectExtensionButton } from "App/Common/ConnectExtensionButton";
+import clsx from "clsx";
+import { ConnectStatus, useExtensionConnect } from "hooks/useExtensionConnect";
 
 type Props = {
-  isExtensionConnected: boolean;
   chain: Chain;
 };
 
-export const TopNavigation = ({
-  isExtensionConnected,
-  chain,
-}: Props): JSX.Element => {
+export const TopNavigation = ({ chain }: Props): JSX.Element => {
+  const { connectionStatus } = useExtensionConnect(chain);
+
   return (
     <>
-      {!isExtensionConnected && (
+      {connectionStatus !== ConnectStatus.CONNECTED && (
         <span>
           <ConnectExtensionButton chain={chain} />
         </span>
+      )}
+
+      {connectionStatus === ConnectStatus.CONNECTED && (
+        <>
+          <div className="grid grid-cols-[140px_140px_140px] items-center gap-4">
+            <ActionButton
+              hoverColor="black"
+              color="white"
+              size="xs"
+              borderRadius="sm"
+            >
+              Send
+            </ActionButton>
+            <ActionButton
+              hoverColor="primary"
+              color="black"
+              size="xs"
+              borderRadius="sm"
+            >
+              Receive
+            </ActionButton>
+            <ActionButton
+              hoverColor="black"
+              color="secondary"
+              size="xs"
+              borderRadius="sm"
+            >
+              Shield
+            </ActionButton>
+          </div>
+          <div>
+            <span
+              className={clsx(
+                "px-7 py-3.5 flex items-center text-xs text-white bg-black rounded-xs"
+              )}
+            >
+              <i />
+              <span>Namada Wallet 01</span>
+            </span>
+          </div>
+        </>
       )}
     </>
   );
