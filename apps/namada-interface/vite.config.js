adfb6ba731ec866cd2600355150602ff05af0a33
Pedro Rezende <xpedrorezende@gmail.com>
2024-04-04 14:05:41 -0300

    feat(interface): loading actual .env variables and keep compatibility

--- apps/namada-interface/vite.config.js
+++ apps/namada-interface/vite.config.js
@@ -1,19 +1,33 @@
+/* eslint-disable */
+
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  define: {
-    "process.env": {},
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      // Node.js global to browser globalThis
-      define: {
-        global: "globalThis",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  const filteredEnv = Object.keys(env).reduce((acc, current) => {
+    if (current.startsWith("NAMADA_INTERFACE_")) {
+      return {
+        ...acc,
+        [current]: env[current],
+      };
+    }
+    return acc;
+  }, {});
+
+  return {
+    plugins: [react(), tsconfigPaths()],
+    define: {
+      "process.env": filteredEnv,
+    },
+    optimizeDeps: {
+      esbuildOptions: {
+        // Node.js global to browser globalThis
+        define: {
+          global: "globalThis",
+        },
       },
     },
-  },
+  };
 });
