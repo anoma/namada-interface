3587e5ff16bc6f5b6fea8cd9d43559733e8e7c18
Pedro Rezende <xpedrorezende@gmail.com>
2024-04-05 16:07:20 -0300

    feat(interface): adding disabled menu entries to sidebar

---
+++ apps/namada-interface/src/App/Icons/MASPIcon.tsx
@@ -0,0 +1,19 @@
+export const MASPIcon = (): JSX.Element => {
+  return (
+    <svg viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g opacity="0.25">
+        <circle
+          cx="8.3418"
+          cy="8.46289"
+          r="6.5"
+          stroke="white"
+          strokeWidth="2"
+        />
+        <circle cx="8.51367" cy="5.78711" r="1.39062" fill="white" />
+        <circle cx="4.74475" cy="8.46252" r="1.28479" fill="white" />
+        <circle cx="7.69434" cy="10.8086" r="1.23438" fill="white" />
+        <circle cx="11.6127" cy="8.23474" r="1.34021" fill="white" />
+      </g>
+    </svg>
+  );
+};
