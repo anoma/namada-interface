e0a3fa47c0de074e708918d44fed7611ca70ab3a
Pedro Rezende <xpedrorezende@gmail.com>
2024-04-09 19:28:32 -0300

    feat(interface): adding Validator Diversification sidebar

---
+++ apps/namada-interface/src/App/Sidebars/ValidatorDiversification.tsx
@@ -0,0 +1,26 @@
+import { ActionButton } from "@namada/components";
+import clsx from "clsx";
+import stakingInfo from "./assets/staking-info.png";
+
+// TODO: Define correct URL
+export const ValidatorDiversification = (): JSX.Element => {
+  return (
+    <div
+      className={clsx(
+        "flex flex-col items-center text-[18px] py-8 px-4 text-center text-yellow",
+        "gap-7 leading-tight"
+      )}
+    >
+      <img src={stakingInfo} className="max-w-42 mx-auto" />
+      <p>When staking consider diversifying Across multiple validators.</p>
+      <ActionButton
+        className="max-w-44"
+        href="https://namada.net"
+        borderRadius="sm"
+        size="xs"
+      >
+        Learn about CPos
+      </ActionButton>
+    </div>
+  );
+};
