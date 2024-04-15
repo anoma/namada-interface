99f6a2600f810c27b2a3b276c910d8ff14db880f
Pedro Rezende <xpedrorezende@gmail.com>
2024-04-15 19:32:16 -0300

    feat(components): refactoring and simplifying Modal component

--- packages/components/src/Modal.tsx
+++ packages/components/src/Modal.tsx
@@ -1,63 +1,18 @@
-import { default as ReactModal } from "react-modal";
-import { tv } from "tailwind-variants";
-
-type Props = {
+type ModalProps = {
   children: JSX.Element;
-  isOpen: boolean;
-  title?: string;
-  onBackdropClick?: () => void;
+  onClose: () => void;
 };
 
-const modal = tv({
-  slots: {
-    container: "flex flex-col items-center w-full bg-neutral-900",
-    header: "flex justify-center items-center text-neutral-200",
-    content: "flex items-center justify-center w-full bg-neutral-900",
-  },
-});
-
-export const Modal = (props: Props): JSX.Element => {
-  const { children, isOpen, title, onBackdropClick } = props;
-
-  const { container, header, content } = modal();
-
+export const Modal = ({ onClose, children }: ModalProps): JSX.Element => {
   return (
-    <ReactModal
-      shouldCloseOnOverlayClick={onBackdropClick !== undefined}
-      onRequestClose={() => {
-        onBackdropClick && onBackdropClick();
-      }}
-      style={{
-        overlay: {
-          backgroundColor: "rgba(0, 0, 0, 0.75)",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          zIndex: "1100",
-        },
-        content: {
-          top: "undefined",
-          left: "undefined",
-          right: "undefined",
-          bottom: "undefined",
-          zIndex: "1100",
-          display: "flex",
-          justifyContent: "center",
-          width: "80%",
-          maxWidth: "640px",
-          height: "80%",
-          padding: 0,
-        },
-      }}
-      isOpen={isOpen}
-      ariaHideApp={false}
-    >
-      <div className={container()}>
-        <header className={header()}>
-          <h3>{title}</h3>
-        </header>
-        <div className={content()}>{children}</div>
+    <>
+      <div
+        onClick={onClose}
+        className="fixed top-0 left-0 w-full h-full cursor-pointer backdrop-blur-lg z-[1000] bg-rblack/50"
+      />
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20 z-[1001]">
+        {children}
       </div>
-    </ReactModal>
+    </>
   );
 };
