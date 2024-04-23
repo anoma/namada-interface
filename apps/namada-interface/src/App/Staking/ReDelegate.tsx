433d39424a7c71cb16b67f607739a12630742875
Pedro Rezende <xpedrorezende@gmail.com>
2024-04-23 13:24:03 -0300

    feat(interface): initial re-delegate flow

---
+++ apps/namada-interface/src/App/Staking/ReDelegate.tsx
@@ -0,0 +1,106 @@
+import { ActionButton, Alert, Modal, Panel } from "@namada/components";
+import { ModalContainer } from "App/Common/ModalContainer";
+import { useStakeModule } from "hooks/useStakeModule";
+import { useAtomValue } from "jotai";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { transparentAccountsAtom } from "slices/accounts";
+import { selectedCurrencyRateAtom } from "slices/exchangeRates";
+import { selectedCurrencyAtom } from "slices/settings";
+import { fetchAllValidatorsAtom } from "slices/validators";
+import { useLoadable } from "store/hooks";
+import { BondingAmountOverview } from "./BondingAmountOverview";
+import { ReDelegateTable } from "./ReDelegateTable";
+import { ValidatorSearch } from "./ValidatorSearch";
+import StakingRoutes from "./routes";
+
+const ReDelegate = (): JSX.Element => {
+  const navigate = useNavigate();
+  const [filter, setFilter] = useState<string>("");
+  const accounts = useAtomValue(transparentAccountsAtom);
+  const validators = useLoadable(fetchAllValidatorsAtom);
+  const selectedFiatCurrency = useAtomValue(selectedCurrencyAtom);
+  const selectedCurrencyRate = useAtomValue(selectedCurrencyRateAtom);
+  const {
+    totalStakedAmount,
+    stakedAmountByAddress,
+    updatedAmountByAddress,
+    totalUpdatedAmount,
+    onChangeValidatorAmount,
+  } = useStakeModule({ accounts });
+
+  const onCloseModal = (): void => navigate(StakingRoutes.overview().url);
+
+  return (
+    <Modal onClose={onCloseModal}>
+      <ModalContainer
+        header="Select amount to re-delegate"
+        onClose={onCloseModal}
+      >
+        <div className="grid grid-cols-[2fr_1fr_1fr] gap-1.5">
+          <BondingAmountOverview
+            title="Available to re-delegate"
+            selectedFiatCurrency={selectedFiatCurrency}
+            fiatExchangeRate={selectedCurrencyRate}
+            amountInNam={0}
+            updatedAmountInNam={totalUpdatedAmount}
+            updatedValueClassList="text-pink"
+            extraContent={
+              <>
+                <Alert
+                  type="warning"
+                  className="absolute py-3 right-3 top-4 max-w-[50%] text-xs rounded-sm"
+                >
+                  To proceed all re-delegated value must be assigned
+                </Alert>
+              </>
+            }
+          />
+          <BondingAmountOverview
+            title="Current Stake"
+            selectedFiatCurrency={selectedFiatCurrency}
+            fiatExchangeRate={selectedCurrencyRate}
+            amountInNam={totalStakedAmount}
+          />
+          <Panel className="flex items-center h-full justify-center">
+            <ActionButton
+              className="w-32 mx-auto"
+              color="white"
+              size="sm"
+              borderRadius="sm"
+              onClick={() => navigate(StakingRoutes.unstake().url)}
+            >
+              Unstake
+            </ActionButton>
+          </Panel>
+        </div>
+        <Panel className="w-full rounded-md flex-1">
+          <div className="w-[70%]">
+            <ValidatorSearch onChange={(value: string) => setFilter(value)} />
+          </div>
+          {validators.state === "hasData" && (
+            <ReDelegateTable
+              filter={filter}
+              validators={validators.data}
+              updatedAmountByAddress={updatedAmountByAddress}
+              stakedAmountByAddress={stakedAmountByAddress}
+              onChangeValidatorAmount={onChangeValidatorAmount}
+              selectedFiatCurrency={selectedFiatCurrency}
+              selectedCurrencyExchangeRate={selectedCurrencyRate}
+            />
+          )}
+        </Panel>
+        <ActionButton
+          size="sm"
+          color="white"
+          borderRadius="sm"
+          className="mt-2 w-1/4 mx-auto"
+        >
+          Unstake
+        </ActionButton>
+      </ModalContainer>
+    </Modal>
+  );
+};
+
+export default ReDelegate;
