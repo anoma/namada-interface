6214e3a6841e33f5efbc61dd63ba06c92b592100
Pedro Rezende <xpedrorezende@gmail.com>
2024-05-02 17:11:59 -0300

    feat: removing deprecated content and fixing ts errors

--- apps/namada-interface/src/slices/index.ts
+++ apps/namada-interface/src/slices/index.ts
@@ -1,4 +1,3 @@
-export { stakingAndGovernanceReducers } from "./StakingAndGovernance";
 export { default as chainReducer } from "./chain";
 export { default as channelsReducer } from "./channels";
 export { default as settingsReducer } from "./settings";
