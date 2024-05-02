a34a569db7e7c8f80c6d478ef7cda5e4dad7a042
Pedro Rezende <xpedrorezende@gmail.com>
2024-05-02 17:11:59 -0300

    feat: removing deprecated content and fixing ts errors

--- apps/namada-interface/src/store/mocks.ts
+++ apps/namada-interface/src/store/mocks.ts
@@ -1,34 +1,7 @@
-import BigNumber from "bignumber.js";
-
 import { chains } from "@namada/chains";
-import { AccountType } from "@namada/types";
-import { StakingOrUnstakingState } from "slices/StakingAndGovernance";
 import { RootState } from "./store";
 
 export const mockAppState: RootState = {
-  accounts: {
-    derived: {
-      namada: {
-        tnam1q8gpzlamqksqjagt2xs3p6tnfcldy0fcd53fs4jh: {
-          details: {
-            chainId: "namada-masp-1.5.32ccad5356012a7",
-            alias: "Namada",
-            address: "tnam1q8gpzlamqksqjagt2xs3p6tnfcldy0fcd53fs4jh",
-            isShielded: false,
-            type: AccountType.PrivateKey,
-            chainKey: "namada",
-          },
-          balance: {
-            NAM: new BigNumber(1000),
-            DOT: new BigNumber(1000),
-            ETH: new BigNumber(1000),
-          },
-        },
-      },
-      cosmos: {},
-      ethereum: {},
-    },
-  },
   chain: { config: chains.namada },
   channels: {
     channelsByChain: {
@@ -43,16 +16,4 @@ export const mockAppState: RootState = {
   settings: {
     connectedChains: [],
   },
-  stakingAndGovernance: {
-    validators: [],
-    validatorAssets: {},
-    myValidators: [],
-    myStakingPositions: [],
-    stakingOrUnstakingState: StakingOrUnstakingState.Idle,
-  },
-  notifications: {
-    toasts: {},
-    pendingActions: [],
-  },
-  proposals: { proposals: [] },
 };
