7a38e1a89a74f4a0c3ca9671325b387c23dbca47
Eric Corson <public@giraffedev.com>
2024-05-13 22:28:23 +0900

    feat: improve proposal JSON display
    - Display keys matching proposal JSON format
    - Remove double quotes from number values
    - Fix incorrect type for pgf steward proposal data
    
    Closes #757.

--- apps/namada-interface/src/App/Governance/ViewJson.tsx
+++ apps/namada-interface/src/App/Governance/ViewJson.tsx
@@ -3,13 +3,87 @@ import { useAtomValue } from "jotai";
 import { useNavigate } from "react-router-dom";
 
 import { Modal } from "@namada/components";
-import { copyToClipboard } from "@namada/utils";
+import { PgfTarget, Proposal } from "@namada/types";
+import { assertNever, copyToClipboard } from "@namada/utils";
 import { ModalContainer } from "App/Common/ModalContainer";
 import clsx from "clsx";
 import { useState } from "react";
 import { GoCheck, GoCopy } from "react-icons/go";
 import { proposalFamily } from "slices/proposals";
 import GovernanceRoutes from "./routes";
+
+type DefaultData = undefined; // TODO: support displaying WASM bytes
+
+type PgfStewardData = {
+  add?: string;
+  remove: string[];
+};
+
+// TODO: add IBC target
+type PgfTargetJson = {
+  internal: {
+    target: string;
+    amount: string;
+  };
+};
+
+type PgfPaymentData = {
+  continuous: PgfTargetJson[];
+  retro: PgfTargetJson[];
+};
+
+type DataJson = DefaultData | PgfStewardData | PgfPaymentData;
+
+type ProposalJson = {
+  proposal: {
+    id: bigint;
+    content: { [key: string]: string | undefined };
+    author: string;
+    voting_start_epoch: bigint;
+    voting_end_epoch: bigint;
+    grace_epoch: bigint;
+  };
+  data: DataJson;
+};
+
+const formatPgfTarget = (value: PgfTarget): PgfTargetJson => ({
+  internal: {
+    target: value.internal.target,
+    amount: value.internal.amount.toString(),
+  },
+});
+
+const formatData = (proposal: Proposal): DataJson => {
+  switch (proposal.proposalType.type) {
+    case "default":
+      return undefined;
+
+    case "pgf_steward":
+      const addRemove = proposal.proposalType.data;
+
+      // deliberately specify keys to ensure no unwanted data is printed
+      return {
+        add: addRemove.add,
+        remove: addRemove.remove,
+      };
+
+    case "pgf_payment":
+      const pgfActions = proposal.proposalType.data;
+
+      const continuous = [
+        ...pgfActions.continuous.add,
+        ...pgfActions.continuous.remove,
+      ].map(formatPgfTarget);
+
+      const retro = pgfActions.retro.map(formatPgfTarget);
+
+      // deliberately specify keys to ensure no unwanted data is printed
+      return { continuous, retro };
+
+    default:
+      return assertNever(proposal.proposalType);
+  }
+};
 
 export const ViewJson: React.FC = () => {
   const navigate = useNavigate();
@@ -27,18 +101,25 @@ export const ViewJson: React.FC = () => {
 
   const proposal = proposalQueryResult.data;
 
-  // JSON.stringify doesn't work with BigInts
-  const proposalWithoutBigInt = Object.fromEntries(
-    Object.entries(proposal).map(([key, value]) => [
-      key,
-      typeof value === "bigint" ? value.toString() : value,
-    ])
-  );
+  const proposalJson: ProposalJson = {
+    proposal: {
+      id: proposal.id,
+      content: proposal.content,
+      author: proposal.author,
+      voting_start_epoch: proposal.startEpoch,
+      voting_end_epoch: proposal.endEpoch,
+      grace_epoch: proposal.graceEpoch,
+    },
+    data: formatData(proposal),
+  };
 
-  // TODO: this isn't quite right because bigints look like strings
-  // We maybe want the display JSON to be something that `namada client
-  // init-proposal` will accept.
-  const proposalJson = JSON.stringify(proposalWithoutBigInt, null, 2);
+  const stringified = JSON.stringify(
+    proposalJson,
+    // TODO: This is not technically safe to cast BigInt to Number, but in
+    // practice is probably fine. Still, we should consider replacing this code.
+    (_, value) => (typeof value === "bigint" ? Number(value) : value),
+    2
+  );
 
   const onCloseModal = (): void =>
     navigate(GovernanceRoutes.proposal(proposal.id).url);
@@ -46,7 +127,7 @@ export const ViewJson: React.FC = () => {
   const onCopy = (): void => {
     if (!copied) {
       setCopied(true);
-      copyToClipboard(proposalJson);
+      copyToClipboard(stringified);
       setTimeout(() => setCopied(false), 2000);
     }
   };
@@ -67,7 +148,7 @@ export const ViewJson: React.FC = () => {
           : <GoCopy />}
         </i>
         <div className="px-8 pt-4">
-          <pre className="overflow-x-auto dark-scrollbar">{proposalJson}</pre>
+          <pre className="overflow-x-auto dark-scrollbar">{stringified}</pre>
         </div>
       </ModalContainer>
     </Modal>
