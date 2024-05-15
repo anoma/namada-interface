105b3bb58aa413a38f847886786f31af07302063
Pedro Rezende <xpedrorezende@gmail.com>
2024-05-15 16:35:48 -0300

    feat(interface): removing staking amount from randomize sorting criterea

--- apps/namada-interface/src/hooks/useValidatorSorting.ts
+++ apps/namada-interface/src/hooks/useValidatorSorting.ts
@@ -5,28 +5,20 @@ import { Validator } from "slices/validators";
 type useValidatorSortingProps = {
   validators: Validator[];
   seed: number;
-  stakedAmountByAddress: Record<string, BigNumber>;
   updatedAmountByAddress: Record<string, BigNumber>;
 };
 
 export const useValidatorSorting = ({
   validators,
   seed,
-  stakedAmountByAddress,
   updatedAmountByAddress,
 }: useValidatorSortingProps): Validator[] => {
   return useMemo(() => {
     return validators
       .map((v) => ({ value: v, sort: Math.random() }))
       .sort((validator1, validator2) => {
-        const v1HasStake = !!stakedAmountByAddress[validator1.value.address];
-        const v2HasStake = !!stakedAmountByAddress[validator2.value.address];
         const v1IsUpdated = !!updatedAmountByAddress[validator1.value.address];
         const v2IsUpdated = !!updatedAmountByAddress[validator2.value.address];
-
-        // Prioritizing validators with stake
-        if (v1HasStake && !v2HasStake) return -1;
-        if (!v1HasStake && v2HasStake) return 1;
 
         // Then addresses that were updated (increased / decreased staking)
         if (v1IsUpdated && !v2IsUpdated) return -1;
@@ -35,5 +27,5 @@ export const useValidatorSorting = ({
         return validator1.sort - validator2.sort;
       })
       .map(({ value }) => value);
-  }, [validators, stakedAmountByAddress, seed]);
+  }, [validators, seed]);
 };
