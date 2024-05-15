bd9a6abc5be14f2b0a78464c55329b771b1d9493
Pedro Rezende <xpedrorezende@gmail.com>
2024-05-15 20:56:01 -0300

    feat(interface): responsive tweaks

--- apps/namada-interface/src/App/Common/PageWithSidebar.tsx
+++ apps/namada-interface/src/App/Common/PageWithSidebar.tsx
@@ -4,6 +4,8 @@ type PageWithSidebar = {
 
 export const PageWithSidebar = ({ children }: PageWithSidebar): JSX.Element => {
   return (
-    <div className="w-full grid grid-cols-[auto_240px] gap-2">{children}</div>
+    <div className="w-full lg:grid lg:grid-cols-[auto_240px] gap-2">
+      {children}
+    </div>
   );
 };
