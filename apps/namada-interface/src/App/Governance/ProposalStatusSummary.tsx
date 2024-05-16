3a93c9c87ef866b0376e6bc79a99b160528ecd2c
Eric Corson <public@giraffedev.com>
2024-05-16 13:11:44 +0900

    feat: add loading state for proposal summary

--- apps/namada-interface/src/App/Governance/ProposalStatusSummary.tsx
+++ apps/namada-interface/src/App/Governance/ProposalStatusSummary.tsx
@@ -53,7 +53,101 @@ const StatusListItem: React.FC<{
   );
 };
 
-export const ProposalStatusSummary: React.FC<{
+const Layout: React.FC<{
+  pieChartData?: PieChartData[];
+  percentages: Record<VoteType, string>;
+  amounts: Record<VoteType, string>;
+  turnout: string;
+  quorum: string;
+  pieChartMiddle?: React.ReactNode;
+  hoveredVoteType?: VoteType;
+  onMouseEnter?: (data: PieChartData, index: number) => void;
+  onMouseLeave?: () => void;
+}> = ({
+  pieChartData,
+  percentages,
+  amounts,
+  turnout,
+  quorum,
+  pieChartMiddle,
+  hoveredVoteType,
+  onMouseEnter,
+  onMouseLeave,
+}) => (
+  <Stack className="@sm:flex-row" gap={4}>
+    <PieChart
+      id="proposal-status-pie-chart"
+      data={pieChartData || [{ value: 1, color: "#1B1B1B" }]}
+      segmentMargin={0}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
+      {pieChartMiddle}
+    </PieChart>
+
+    <div className="contents w-full @sm:block">
+      <Stack as="ul" gap={1}>
+        {voteTypes.map((voteType, i) => (
+          <StatusListItem
+            key={i}
+            color={colors[voteType]}
+            leftContent={voteType}
+            rightContent={percentages[voteType]}
+            rightSubContent={amounts[voteType]}
+            selected={hoveredVoteType === voteType}
+          />
+        ))}
+      </Stack>
+
+      <div className="@sm:mt-6">
+        <h3 className="text-[#A3A3A3] text-xs">Turnout / Quorum</h3>
+        <p className="text-xl">
+          {turnout} / {quorum}
+        </p>
+      </div>
+    </div>
+  </Stack>
+);
+
+type ProposalStatusSummaryProps =
+  | {
+      loading: false;
+      proposal: Proposal;
+      status: ProposalStatus;
+    }
+  | { loading: true };
+
+export const ProposalStatusSummary: React.FC<ProposalStatusSummaryProps> = (
+  props
+) => {
+  if (props.loading) {
+    return (
+      <Layout
+        percentages={{
+          yay: "%",
+          nay: "%",
+          abstain: "%",
+        }}
+        amounts={{
+          yay: "NAM",
+          nay: "NAM",
+          abstain: "NAM",
+        }}
+        turnout="%"
+        quorum="%"
+        pieChartMiddle={
+          <div className="text-sm text-[#B0B0B0] animate-pulse">
+            Syncing Data
+          </div>
+        }
+      />
+    );
+  } else {
+    return <Loaded proposal={props.proposal} status={props.status} />;
+  }
+};
+
+const Loaded: React.FC<{
   proposal: Proposal;
   status: ProposalStatus;
 }> = ({ proposal, status }) => {
@@ -83,13 +177,13 @@ export const ProposalStatusSummary: React.FC<{
     return formatPercentage(voteProportion, 2);
   };
 
-  const data: PieChartData[] =
-    zeroVotes ?
-      [{ value: 1, color: "#2F2F2F" }]
-    : voteTypes.map((voteType) => ({
+  const data: PieChartData[] | undefined =
+    zeroVotes ? undefined : (
+      voteTypes.map((voteType) => ({
         value: status[voteType],
         color: colors[voteType],
-      }));
+      }))
+    );
 
   const handleMouseEnter = (_data: PieChartData, index: number): void => {
     if (!zeroVotes) {
@@ -99,56 +193,52 @@ export const ProposalStatusSummary: React.FC<{
 
   const handleMouseLeave = (): void => setHoveredVoteType(undefined);
 
+  const pieChartMiddle = (
+    <AnimatePresence>
+      {typeof hoveredVoteType !== "undefined" && (
+        <motion.article
+          className="leading-tight"
+          style={{ color: colors[hoveredVoteType] }}
+          transition={{ duration: 0.1 }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+        >
+          <div className="uppercase text-sm">{hoveredVoteType}</div>
+          <div className="text-3xl">
+            {percentageString(status[hoveredVoteType])}
+          </div>
+        </motion.article>
+      )}
+    </AnimatePresence>
+  );
+
+  const percentages = {
+    yay: percentageString(status.yay),
+    nay: percentageString(status.nay),
+    abstain: percentageString(status.abstain),
+  };
+
+  const amountString = (voteType: VoteType): string =>
+    status[voteType].toString() + " NAM";
+
+  const amounts = {
+    yay: amountString("yay"),
+    nay: amountString("nay"),
+    abstain: amountString("abstain"),
+  };
+
   return (
-    <Stack className="@sm:flex-row" gap={4}>
-      <PieChart
-        id="proposal-status-pie-chart"
-        data={data}
-        segmentMargin={0}
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-      >
-        <AnimatePresence>
-          {typeof hoveredVoteType !== "undefined" && (
-            <motion.article
-              className="leading-tight"
-              style={{ color: colors[hoveredVoteType] }}
-              transition={{ duration: 0.1 }}
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-            >
-              <div className="uppercase text-sm">{hoveredVoteType}</div>
-              <div className="text-3xl">
-                {percentageString(status[hoveredVoteType])}
-              </div>
-            </motion.article>
-          )}
-        </AnimatePresence>
-      </PieChart>
-
-      <div className="contents w-full @sm:block">
-        <Stack as="ul" gap={1}>
-          {voteTypes.map((voteType, i) => (
-            <StatusListItem
-              key={i}
-              color={colors[voteType]}
-              leftContent={voteType}
-              rightContent={percentageString(status[voteType])}
-              rightSubContent={status[voteType].toString() + " NAM"}
-              selected={hoveredVoteType === voteType}
-            />
-          ))}
-        </Stack>
-
-        <div className="@sm:mt-6">
-          <h3 className="text-[#A3A3A3] text-xs">Turnout / Quorum</h3>
-          <p className="text-xl">
-            {formatPercentage(votedProportion, 2)} /{" "}
-            {formatPercentage(quorum, 2)}
-          </p>
-        </div>
-      </div>
-    </Stack>
+    <Layout
+      pieChartData={data}
+      percentages={percentages}
+      amounts={amounts}
+      turnout={formatPercentage(votedProportion, 2)}
+      quorum={formatPercentage(quorum, 2)}
+      pieChartMiddle={pieChartMiddle}
+      hoveredVoteType={hoveredVoteType}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    />
   );
 };
