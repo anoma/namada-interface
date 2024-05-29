d357d5b383fb5ddccab135817734b6cc8fbd4649
Pedro Rezende <xpedrorezende@gmail.com>
2024-05-29 12:40:27 +0200

    feat(namadillo): removing ts and eslint check from lintstaged + config vite to check for types

--- apps/namadillo/vite.config.js
+++ apps/namadillo/vite.config.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 import react from "@vitejs/plugin-react";
 import { defineConfig, loadEnv } from "vite";
+import checker from "vite-plugin-checker";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -22,6 +23,10 @@ export default defineConfig(({ mode }) => {
       tsconfigPaths(),
       nodePolyfills({
         protocolImports: true,
+      }),
+      checker({
+        typescript: true,
+        eslint: { lintCommand: 'eslint "./src/**/*.{ts,tsx}"' },
       }),
     ],
     define: {
