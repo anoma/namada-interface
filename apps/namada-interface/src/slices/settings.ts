e9384b897c032cfcfc61cd2c133958b6771482c4
Pedro Rezende <xpedrorezende@gmail.com>
2024-05-29 15:25:12 +0200

    feat: preparing settings slice for new settings page

--- apps/namada-interface/src/slices/settings.ts
+++ apps/namada-interface/src/slices/settings.ts
@@ -1,40 +1,61 @@
 import { ChainKey } from "@namada/types";
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { CurrencyType } from "@namada/utils";
+import { Getter, Setter, atom } from "jotai";
+import { atomWithStorage } from "jotai/utils";
 
-import { atom } from "jotai";
-
-const SETTINGS_ACTIONS_BASE = "settings";
-
-export type SettingsState = {
-  connectedChains: string[];
+type SettingsStorage = {
+  fiat: CurrencyType;
+  hideBalances: boolean;
+  rpcUrl: string;
+  chainId: string;
 };
 
-const initialState: SettingsState = {
-  connectedChains: [],
-};
+export const namadaExtensionConnectedAtom = atom(false);
 
-const settingsSlice = createSlice({
-  name: SETTINGS_ACTIONS_BASE,
-  initialState,
-  reducers: {
-    setIsConnected: (state, action: PayloadAction<ChainKey>) => {
-      state.connectedChains = state.connectedChains.includes(action.payload)
-        ? state.connectedChains
-        : [...state.connectedChains, action.payload];
-    },
-  },
+export const namadilloSettingsAtom = atomWithStorage<SettingsStorage>(
+  "namadillo:settings",
+  {
+    fiat: "usd",
+    hideBalances: false,
+    rpcUrl: process.env.NAMADA_INTERFACE_NAMADA_URL || "",
+    chainId: process.env.NAMADA_INTERFACE_NAMADA_CHAIN_ID || "",
+  }
+);
+
+const changeSettings =
+  <T>(key: keyof SettingsStorage) =>
+  (get: Getter, set: Setter, value: T) => {
+    const settings = get(namadilloSettingsAtom);
+    set(namadilloSettingsAtom, { ...settings, [key]: value });
+  };
+
+export const selectedCurrencyAtom = atom(
+  (get) => get(namadilloSettingsAtom).fiat,
+  changeSettings<CurrencyType>("fiat")
+);
+
+export const hideBalancesAtom = atom(
+  (get) => get(namadilloSettingsAtom).hideBalances,
+  changeSettings<boolean>("hideBalances")
+);
+
+export const rpcUrlAtom = atom(
+  (get) => get(namadilloSettingsAtom).rpcUrl,
+  changeSettings<string>("rpcUrl")
+);
+
+export const chainIdAtom = atom(
+  (get) => get(namadilloSettingsAtom).chainId,
+  changeSettings<string>("chainId")
+);
+
+export const connectedChainsAtom = atom<ChainKey[]>([]);
+export const addConnectedChainAtom = atom(null, (get, set, chain: ChainKey) => {
+  const connectedChains = get(connectedChainsAtom);
+  set(
+    connectedChainsAtom,
+    connectedChains.includes(chain) ? connectedChains : (
+      [...connectedChains, chain]
+    )
+  );
 });
-
-const { actions, reducer } = settingsSlice;
-
-export const { setIsConnected } = actions;
-
-export default reducer;
-
-////////////////////////////////////////////////////////////////////////////////
-// JOTAI
-////////////////////////////////////////////////////////////////////////////////
-
-const namadaExtensionConnectedAtom = atom(false);
-
-export { namadaExtensionConnectedAtom };
