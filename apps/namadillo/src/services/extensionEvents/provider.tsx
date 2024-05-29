26ed6d240563a645688a82f04f2c52b72d88543d
Pedro Rezende <xpedrorezende@gmail.com>
2024-05-29 14:32:54 +0200

    feat(namadillo): removing unused events

--- apps/namadillo/src/services/extensionEvents/provider.tsx
+++ apps/namadillo/src/services/extensionEvents/provider.tsx
@@ -1,13 +1,9 @@
 import { useEventListenerOnce } from "@namada/hooks";
 import { Namada, useIntegration } from "@namada/integrations";
-import { Events, KeplrEvents, MetamaskEvents } from "@namada/types";
+import { Events } from "@namada/types";
 import { useAtomValue, useSetAtom } from "jotai";
 import { createContext } from "react";
-import {
-  addAccountsAtom,
-  balancesAtom,
-  fetchDefaultAccountAtom,
-} from "slices/accounts";
+import { balancesAtom, fetchDefaultAccountAtom } from "slices/accounts";
 import { namadaExtensionConnectedAtom } from "slices/settings";
 import { NamadaConnectionRevokedHandler } from "./handlers";
 
@@ -15,11 +11,8 @@ export const ExtensionEventsContext = createContext({});
 
 export const ExtensionEventsProvider: React.FC = (props): JSX.Element => {
   const namadaIntegration = useIntegration("namada");
-  const keplrIntegration = useIntegration("cosmos");
-  const metamaskIntegration = useIntegration("ethereum");
   const fetchDefaultAccount = useSetAtom(fetchDefaultAccountAtom);
   const balances = useAtomValue(balancesAtom);
-  const addAccounts = useSetAtom(addAccountsAtom);
   const setNamadaExtensionConnected = useSetAtom(namadaExtensionConnectedAtom);
 
   // Instantiate handlers:
@@ -38,41 +31,6 @@ export const ExtensionEventsProvider: React.FC = (props): JSX.Element => {
     Events.ConnectionRevoked,
     namadaConnectionRevokedHandler
   );
-
-  useEventListenerOnce(KeplrEvents.AccountChanged, async () => {
-    const accounts = await keplrIntegration.accounts();
-    if (accounts) {
-      addAccounts(accounts);
-      balances.refetch();
-    }
-  });
-
-  useEventListenerOnce(
-    MetamaskEvents.AccountChanged,
-    async () => {
-      const accounts = await metamaskIntegration.accounts();
-      if (accounts) {
-        addAccounts(accounts);
-        balances.refetch();
-      }
-    },
-    false,
-    (event, handler) => {
-      if (window.ethereum) {
-        //TODO: fix as
-        window.ethereum.on(event, handler as () => unknown);
-      }
-    },
-    (event, handler) => {
-      if (window.ethereum) {
-        window.ethereum.removeListener(event, handler);
-      }
-    }
-  );
-
-  useEventListenerOnce(MetamaskEvents.BridgeTransferCompleted, async () => {
-    balances.refetch();
-  });
 
   return (
     <ExtensionEventsContext.Provider value={{}}>
