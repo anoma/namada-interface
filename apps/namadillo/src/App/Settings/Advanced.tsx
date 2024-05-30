b75d8f3d48a7c2b365110786d6388246a3e11547
Pedro Rezende <xpedrorezende@gmail.com>
2024-05-30 10:57:32 +0200

    feat(namadillo_settings): avoid to leave application

--- apps/namadillo/src/App/Settings/Advanced.tsx
+++ apps/namadillo/src/App/Settings/Advanced.tsx
@@ -1,16 +1,20 @@
 import { ActionButton, Input, Stack } from "@namada/components";
+import SettingsRoute from "App/Settings/routes";
 import { useAtom } from "jotai";
 import { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import { rpcUrlAtom } from "slices/settings";
 
 export const Advanced = (): JSX.Element => {
+  const navigate = useNavigate();
+  const location = useLocation();
   const [currentRpc, setCurrentRpc] = useAtom(rpcUrlAtom);
   const [rpc, setRpc] = useState(currentRpc);
 
   const onSubmit = (e: React.FormEvent): void => {
     e.preventDefault();
     setCurrentRpc(rpc);
-    window.history.back();
+    navigate(SettingsRoute.index(), { replace: true, state: location.state });
   };
 
   return (
