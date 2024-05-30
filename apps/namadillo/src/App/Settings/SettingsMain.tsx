9fd6898f8617e3b0960e5c890cc528ceecd1af3d
Pedro Rezende <xpedrorezende@gmail.com>
2024-05-30 02:00:46 +0200

    feat(namadillo_settings): warning to review the arbitrary message signing

--- apps/namadillo/src/App/Settings/SettingsMain.tsx
+++ apps/namadillo/src/App/Settings/SettingsMain.tsx
@@ -1,5 +1,6 @@
-import { Stack, ToggleButton } from "@namada/components";
+import { Alert, Stack, ToggleButton } from "@namada/components";
 import { useAtom } from "jotai";
+import { IoWarning } from "react-icons/io5";
 import { signArbitraryEnabledAtom } from "slices/settings";
 import { SettingsPanelMenuItem } from "./SettingsPanelMenuItem";
 import SettingsRoutes from "./routes";
@@ -10,7 +11,7 @@ export const SettingsMain = (): JSX.Element => {
   );
 
   return (
-    <div className="flex flex-1 justify-between flex-col">
+    <div className="flex flex-1 justify-between flex-col pb-4">
       <ul className="flex flex-col gap-2">
         <SettingsPanelMenuItem
           url={`${SettingsRoutes.currencySelection().url}`}
@@ -22,15 +23,28 @@ export const SettingsMain = (): JSX.Element => {
         />
       </ul>
 
-      <Stack as="footer" gap={2.5}>
+      <Stack as="footer" className="px-5" gap={3}>
         <h2 className="text-base">Sign Arbitrary Message</h2>
         <p className="text-sm">
           Enabling this setting puts you at risk of phishing attacks. Please
           check what you are signing very carefully when using this feature. We
           recommend keeping this setting turned off unless absolutely necessary
         </p>
+        {signArbitraryEnabled && (
+          <Alert type="warning" className="text-xs rounded-sm py-3">
+            <div className="flex items-center gap-3">
+              <i className="text-4xl">
+                <IoWarning />
+              </i>
+              You are at risk of phishing attacks. Please review carefully what
+              you sign
+            </div>
+          </Alert>
+        )}
         <ToggleButton
-          label="Off (Recommended)"
+          label={
+            signArbitraryEnabled ? "On (Not Recommended)" : "Off (Recommended)"
+          }
           color="white"
           activeColor="yellow"
           checked={signArbitraryEnabled}
