467f3257d330c441adbb72189072c0deaf5c441a
Pedro Rezende <xpedrorezende@gmail.com>
2024-05-30 10:05:13 +0200

    feat(components): creating tooltip component

---
+++ packages/components/src/Tooltip.tsx
@@ -0,0 +1,39 @@
+import clsx from "clsx";
+import { twMerge } from "tailwind-merge";
+
+type TooltipProps = {
+  children: React.ReactNode;
+  position?: "top" | "bottom";
+} & React.ComponentPropsWithoutRef<"span">;
+
+// Use group/tooltip class in the parent element that will trigger the Tooltip
+export const Tooltip = ({
+  children,
+  className,
+  position = "top",
+  ...props
+}: TooltipProps): JSX.Element => {
+  return (
+    <span
+      className={twMerge(
+        clsx(
+          "flex bg-rblack text-xs absolute opacity-0 pointer-events-none",
+          "text-white rounded-sm px-4 py-1 left-1/2 -translate-x-1/2",
+          "transition-all duration-500 ease-out-expo",
+          "group-hover/tooltip:visible group-hover/tooltip:opacity-100",
+          "group-hover/tooltip:pointer-events-auto",
+          {
+            "-top-1/2 group-hover/tooltip:top-[calc(-100%-4px)]":
+              position === "top",
+            "-bottom-1/2 group-hover/tooltip:bottom-[calc(-100%-4px)]":
+              position === "bottom",
+          },
+          className
+        )
+      )}
+      {...props}
+    >
+      {children}
+    </span>
+  );
+};
