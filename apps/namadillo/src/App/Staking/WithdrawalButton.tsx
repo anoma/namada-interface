3c24c960e8213bae1d3f7cbb0cf96583492fef51
Pedro Rezende <xpedrorezende@gmail.com>
2024-06-04 18:13:42 +0200

    feat(namadillo_staking): adding withdraw transaction build

--- apps/namadillo/src/App/Staking/WithdrawalButton.tsx
+++ apps/namadillo/src/App/Staking/WithdrawalButton.tsx
@@ -1,6 +1,10 @@
 import { ActionButton } from "@namada/components";
+import { useGasEstimate } from "hooks/useGasEstimate";
+import invariant from "invariant";
 import { useAtomValue } from "jotai";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
+import { defaultAccountAtom } from "slices/accounts";
+import { GAS_LIMIT } from "slices/fees";
 import { createWithdrawTxAtom } from "slices/staking";
 import { MyValidator } from "slices/validators";
 
@@ -11,9 +15,46 @@ type WithdrawalButtonProps = {
 export const WithdrawalButton = ({
   myValidator,
 }: WithdrawalButtonProps): JSX.Element => {
-  const { isPending, isSuccess, isIdle } = useAtomValue(createWithdrawTxAtom);
+  const { gasPrice } = useGasEstimate();
+  const account = useAtomValue(defaultAccountAtom);
+  const {
+    mutate: createWithdrawTx,
+    data: withdrawalTx,
+    isPending,
+    isSuccess,
+    isIdle,
+  } = useAtomValue(createWithdrawTxAtom);
 
-  const onWithdraw = useCallback((_myValidator: MyValidator) => {}, []);
+  const onWithdraw = useCallback(async (myValidator: MyValidator) => {
+    invariant(
+      account,
+      "Extension is not connected or you don't have an account"
+    );
+    invariant(gasPrice, "Gas price loading is still pending");
+    invariant(
+      myValidator.withdrawableAmount,
+      "Validator doesn't have amounts available for withdrawal"
+    );
+    createWithdrawTx({
+      changes: [
+        {
+          validatorId: myValidator.validator.address,
+          amount: myValidator.withdrawableAmount!,
+        },
+      ],
+      gasConfig: {
+        gasPrice: gasPrice!,
+        gasLimit: GAS_LIMIT,
+      },
+      account: account!,
+    });
+  }, []);
+
+  useEffect(() => {
+    if (withdrawalTx) {
+      // TODO: run the transaction
+    }
+  }, [isSuccess]);
 
   return (
     <ActionButton
@@ -22,7 +63,10 @@ export const WithdrawalButton = ({
       outlined
       borderRadius="sm"
       disabled={
-        !myValidator.withdrawableAmount?.eq(0) || isPending || isSuccess
+        !myValidator.withdrawableAmount?.eq(0) ||
+        isPending ||
+        isSuccess ||
+        !gasPrice
       }
       onClick={() => onWithdraw(myValidator)}
     >
