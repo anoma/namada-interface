a21e6f19a93b39920438c4e660dc8274672c6c5b
Pedro Rezende <pedrorezende@users.noreply.github.com>
2024-06-07 18:01:54 +0200

    Namadillo: preferring user selected RPC url and improving queries (#859)
    * feat(namadillo): using user selected rpc intead of .env constant
    
    * feat(namadillo): refactoring and avoiding duplicate queries
    
    * refactor(namadillo): adding a version property instead of using localStorage key
    
    * feat(namadillo): updating reference for native token in accounts

--- apps/namadillo/src/slices/settings.ts
+++ apps/namadillo/src/slices/settings.ts
@@ -3,10 +3,12 @@ import { Getter, Setter, atom } from "jotai";
 import { atomWithStorage } from "jotai/utils";
 
 type SettingsStorage = {
+  version: string;
   fiat: CurrencyType;
   hideBalances: boolean;
   rpcUrl: string;
   chainId: string;
+  nativeToken: string;
   signArbitraryEnabled: boolean;
 };
 
@@ -20,12 +22,16 @@ export const namadaExtensionConnectedAtom = atom<boolean>(
 export const namadilloSettingsAtom = atomWithStorage<SettingsStorage>(
   "namadillo:settings",
   {
+    version: "0.1",
     fiat: "usd",
     hideBalances: false,
     rpcUrl: process.env.NAMADA_INTERFACE_NAMADA_URL || "",
     chainId: process.env.NAMADA_INTERFACE_NAMADA_CHAIN_ID || "",
+    nativeToken: process.env.NAMADA_INTERFACE_NAMADA_TOKEN || "",
     signArbitraryEnabled: false,
-  }
+  },
+  undefined,
+  { getOnInit: true }
 );
 
 const changeSettings =
@@ -53,6 +59,11 @@ export const rpcUrlAtom = atom(
 export const chainIdAtom = atom(
   (get) => get(namadilloSettingsAtom).chainId,
   changeSettings<string>("chainId")
+);
+
+export const nativeTokenAtom = atom(
+  (get) => get(namadilloSettingsAtom).nativeToken,
+  changeSettings<string>("nativeToken")
 );
 
 export const signArbitraryEnabledAtom = atom(
