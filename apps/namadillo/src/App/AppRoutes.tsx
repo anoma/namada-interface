670580c87f9cfffdec2f664aad4ab601343381b2
Pedro Rezende <xpedrorezende@gmail.com>
2024-06-11 18:05:27 -0300

    feat(namadillo): removing x-fade transitions between pages

--- apps/namadillo/src/App/AppRoutes.tsx
+++ apps/namadillo/src/App/AppRoutes.tsx
@@ -9,7 +9,6 @@ import {
 } from "react-router-dom";
 import { AccountOverview } from "./AccountOverview";
 import { App } from "./App";
-import { AnimatedTransition } from "./Common/AnimatedTransition";
 import { Governance } from "./Governance";
 import { SettingsPanel } from "./Settings/SettingsPanel";
 import { Staking } from "./Staking";
@@ -26,29 +25,11 @@ export const MainRoutes = (): JSX.Element => {
     <>
       <Routes location={state?.backgroundLocation || location}>
         <Route path="/" element={<App />}>
-          <Route
-            index
-            element={
-              <AnimatedTransition elementKey="/">
-                <AccountOverview />
-              </AnimatedTransition>
-            }
-          />
-          <Route
-            path={`${StakingRoutes.index()}/*`}
-            element={
-              <AnimatedTransition elementKey={StakingRoutes.index()}>
-                <Staking />
-              </AnimatedTransition>
-            }
-          />
+          <Route index element={<AccountOverview />} />
+          <Route path={`${StakingRoutes.index()}/*`} element={<Staking />} />
           <Route
             path={`${GovernanceRoutes.index()}/*`}
-            element={
-              <AnimatedTransition elementKey={GovernanceRoutes.index()}>
-                <Governance />
-              </AnimatedTransition>
-            }
+            element={<Governance />}
           />
         </Route>
       </Routes>
