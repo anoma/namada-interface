b92dbdb069ea5190e2f4b68028b7f352d77abcd8
Justin R. Evans <jurevans@gmail.com>
2024-07-26 11:02:37 -0400

    feat: use proper denomination for amounts in Tx details

--- packages/shared/lib/src/sdk/tx.rs
+++ packages/shared/lib/src/sdk/tx.rs
@@ -5,10 +5,11 @@ use gloo_utils::format::JsValueSerdeExt;
 use namada::core::borsh::{self, BorshDeserialize, BorshSerialize};
 use namada::sdk::signing::SigningTxData;
 use namada::sdk::tx::{
-    TX_BOND_WASM, TX_REDELEGATE_WASM, TX_REVEAL_PK, TX_TRANSFER_WASM, TX_UNBOND_WASM,
-    TX_VOTE_PROPOSAL, TX_WITHDRAW_WASM, TX_CLAIM_REWARDS_WASM
+    TX_BOND_WASM, TX_CLAIM_REWARDS_WASM, TX_REDELEGATE_WASM, TX_REVEAL_PK, TX_TRANSFER_WASM,
+    TX_UNBOND_WASM, TX_VOTE_PROPOSAL, TX_WITHDRAW_WASM,
 };
 use namada::sdk::uint::Uint;
+use namada::token::Amount;
 use namada::tx;
 use namada::{address::Address, key::common::PublicKey};
 use wasm_bindgen::{prelude::wasm_bindgen, JsError, JsValue};
@@ -152,7 +153,9 @@ impl TxDetails {
         let tx_details = match tx.header().tx_type {
             tx::data::TxType::Wrapper(wrapper) => {
                 let fee_amount = wrapper.get_tx_fee()?.to_string();
-                let gas_limit = Uint::from(wrapper.gas_limit).to_string();
+                let gas_limit = Amount::from_uint(Uint::from(wrapper.gas_limit), 0)?
+                    .native_denominated()
+                    .to_string();
                 let token = wrapper.fee.token.to_string();
 
                 let wrapper_tx =
