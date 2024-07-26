5ba9d319392f8584ef2e90f0aed6c8d9a40ace2b
Harrison Mendonça <harrisonmendonca@gmail.com>
2024-07-26 10:53:10 -0300

    feat: update sign tx layout

---
+++ apps/extension/src/Approvals/StatusBox.tsx
@@ -0,0 +1,33 @@
+import { Alert } from "@namada/components";
+import { Status } from "Approvals/Approvals";
+
+type Props = {
+  status?: Status;
+  idleText?: string;
+  pendingText?: string;
+  errorText?: string;
+};
+
+export const StatusBox: React.FC<Props> = ({
+  status,
+  idleText,
+  pendingText,
+  errorText,
+}) => {
+  if (!status) {
+    return <>{idleText && <Alert type="warning">{idleText}</Alert>}</>;
+  }
+  if (status === Status.Pending) {
+    return <Alert type="info">{pendingText}</Alert>;
+  }
+  if (status === Status.Failed) {
+    return (
+      <Alert type="error">
+        {errorText}
+        <br />
+        Try again
+      </Alert>
+    );
+  }
+  return null;
+};
