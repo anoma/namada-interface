e92d7713c51d7973d974f024f116145a48117298
Harrison Mendonça <harrisonmendonca@gmail.com>
2024-08-12 15:40:12 -0300

    feat: add colored dot

--- apps/namadillo/src/App/Common/SyncIndicator.tsx
+++ apps/namadillo/src/App/Common/SyncIndicator.tsx
@@ -1,3 +1,25 @@
+import { useState } from "react";
+import { twMerge } from "tailwind-merge";
+
+enum Status {
+  IDLE = "idle",
+  SYNCING = "syncing",
+  ERROR = "error",
+}
+
 export const SyncIndicator = (): JSX.Element => {
-  return <div className="text-white">SyncIndicator</div>;
+  const [status] = useState<Status>(Status.IDLE);
+
+  return (
+    <div className="flex items-center gap-2">
+      <div
+        className={twMerge(
+          "w-2 h-2 rounded-full",
+          status === Status.IDLE ? "bg-green-500" : undefined,
+          status === Status.SYNCING ? "bg-yellow-500 animate-pulse" : undefined,
+          status === Status.ERROR ? "bg-red-500" : undefined
+        )}
+      />
+    </div>
+  );
 };
