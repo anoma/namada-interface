f67437c66c78941072092ec0f5eda49361c70479
Pedro Rezende <pedrorezende@users.noreply.github.com>
2024-08-15 19:33:05 -0300

    feat(namadillo): avoid retrying for 4xx errors (#993)

---
+++ apps/namadillo/src/App/Common/QueryProvider.tsx
@@ -0,0 +1,50 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { queryClientAtom } from "jotai-tanstack-query";
+import { Provider as JotaiProvider } from "jotai/react";
+import { useHydrateAtoms } from "jotai/utils";
+
+type ErrorWithResponse = Error & {
+  response?: {
+    status?: number;
+  };
+};
+
+const MAX_RETRIES = 3;
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: (failureCount, error: ErrorWithResponse) => {
+        if (failureCount > MAX_RETRIES) {
+          return false;
+        }
+
+        // Handles all 4xx errors
+        if (String(error?.response?.status).startsWith("4")) {
+          return false;
+        }
+
+        return true;
+      },
+    },
+  },
+});
+
+const HydrateAtoms = (props: { children: JSX.Element }): JSX.Element => {
+  useHydrateAtoms([[queryClientAtom, queryClient]]);
+  return props.children;
+};
+
+export const QueryProvider = ({
+  children,
+}: {
+  children: JSX.Element;
+}): JSX.Element => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <JotaiProvider>
+        <HydrateAtoms>{children}</HydrateAtoms>
+      </JotaiProvider>
+    </QueryClientProvider>
+  );
+};
