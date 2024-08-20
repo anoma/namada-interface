5ca5edeb8069fa70d566d0bce8b651ed16d633a2
Harrison Mendonça <harrisonmendonca@gmail.com>
2024-08-20 11:51:35 -0300

    feat: render chain id (#1017)

--- apps/namadillo/src/App/Settings/SettingsMain.tsx
+++ apps/namadillo/src/App/Settings/SettingsMain.tsx
@@ -1,9 +1,14 @@
+import { chainParametersAtom } from "atoms/chain/atoms";
+import { useAtomValue } from "jotai";
+import { version } from "../../../package.json";
 import { SettingsPanelMenuItem } from "./SettingsPanelMenuItem";
 import SettingsRoutes from "./routes";
 
 export const SettingsMain = (): JSX.Element => {
+  const { data: chainParameters } = useAtomValue(chainParametersAtom);
+
   return (
-    <div className="flex flex-1 justify-between flex-col pb-4">
+    <div className="flex flex-1 justify-between flex-col">
       <ul className="flex flex-col gap-2">
         <SettingsPanelMenuItem
           url={`${SettingsRoutes.advanced().url}`}
@@ -14,6 +19,10 @@ export const SettingsMain = (): JSX.Element => {
           text="Sign Arbitrary"
         />
       </ul>
+      <div className="text-xs">
+        <div>Version: {version}</div>
+        <div>Chain ID: {chainParameters?.chainId}</div>
+      </div>
     </div>
   );
 };
