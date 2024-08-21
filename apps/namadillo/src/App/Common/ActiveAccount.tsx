295ddc49a49730152e81104ef22fc2a51ab255f8
Harrison Mendonça <harrisonmendonca@gmail.com>
2024-08-21 14:14:11 -0300

    feat: add account tooltip

--- apps/namadillo/src/App/Common/ActiveAccount.tsx
+++ apps/namadillo/src/App/Common/ActiveAccount.tsx
@@ -1,4 +1,4 @@
-import { CopyToClipboardControl } from "@namada/components";
+import { CopyToClipboardControl, Tooltip } from "@namada/components";
 import { defaultAccountAtom } from "atoms/accounts";
 import clsx from "clsx";
 import { useAtomValue } from "jotai";
@@ -14,17 +14,18 @@ export const ActiveAccount = (): JSX.Element => {
     <div>
       <span
         className={clsx(
-          "px-5 py-3.5 flex items-center text-xs rounded-[2px]",
+          "px-4 py-2.5 flex items-center text-xs rounded-[2px]",
           "text-white bg-black rounded-xs"
         )}
       >
-        <span className="flex items-center gap-2 ">
+        <span className="flex items-center gap-2 relative group/tooltip">
           <CopyToClipboardControl
-            className="opacity-80 transition-opacity duration-150 hover:opacity-100"
+            className="p-1 opacity-80 transition-opacity duration-150 hover:opacity-100"
             value={account.address || ""}
           >
             {account.alias}
           </CopyToClipboardControl>
+          <Tooltip position="left">{account.address}</Tooltip>
         </span>
       </span>
     </div>
