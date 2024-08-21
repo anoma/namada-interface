a871fd9a1f51cc19e19e4a155d973c966d32fc7e
Harrison Mendonça <harrisonmendonca@gmail.com>
2024-08-21 14:14:11 -0300

    feat: add icon

--- apps/namadillo/src/App/Common/ActiveAccount.tsx
+++ apps/namadillo/src/App/Common/ActiveAccount.tsx
@@ -2,6 +2,7 @@ import { CopyToClipboardControl } from "@namada/components";
 import { defaultAccountAtom } from "atoms/accounts";
 import clsx from "clsx";
 import { useAtomValue } from "jotai";
+import { SwitchAccountIcon } from "./SwitchAccountIcon";
 
 export const ActiveAccount = (): JSX.Element => {
   const { data: account, isFetching } = useAtomValue(defaultAccountAtom);
@@ -10,22 +11,28 @@ export const ActiveAccount = (): JSX.Element => {
     return <></>;
   }
 
+  const buttonClassName =
+    "opacity-80 transition-opacity duration-150 hover:opacity-100";
+
   return (
     <div>
       <span
         className={clsx(
-          "px-5 py-3.5 flex items-center text-xs rounded-[2px]",
+          "px-5 py-3.5 gap-2 flex items-center text-xs rounded-[2px]",
           "text-white bg-black rounded-xs"
         )}
       >
         <span className="flex items-center gap-2 ">
           <CopyToClipboardControl
-            className="opacity-80 transition-opacity duration-150 hover:opacity-100"
+            className={buttonClassName}
             value={account.address || ""}
           >
             {account.alias}
           </CopyToClipboardControl>
         </span>
+        <button className={buttonClassName} onClick={() => {}}>
+          <SwitchAccountIcon />
+        </button>
       </span>
     </div>
   );
