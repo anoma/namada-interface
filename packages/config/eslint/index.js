6b43b587521d30420929f1e8771e92f8a4b432ee
Pedro Rezende <xpedrorezende@gmail.com>
2024-09-07 11:39:18 +0200

    chore: adding @typescript-eslint/no-require-imports rule

--- packages/config/eslint/index.js
+++ packages/config/eslint/index.js
@@ -1,52 +1,53 @@
 module.exports = {
-  "env": {
-    "es2021": true
+  env: {
+    es2021: true,
   },
-  "extends": [
+  extends: [
     "plugin:@typescript-eslint/recommended",
     "prettier",
-    "prettier/prettier"
+    "prettier/prettier",
   ],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaVersion": 12,
-    "sourceType": "module"
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
   },
-  "plugins": ["@typescript-eslint", "import"],
-  "rules": {
+  plugins: ["@typescript-eslint", "import"],
+  rules: {
     "no-use-before-define": "off",
     "import/extensions": [
       "error",
       "ignorePackages",
       {
-        "ts": "never"
-      }
+        ts: "never",
+      },
     ],
     "no-shadow": "off",
     "@typescript-eslint/no-shadow": ["off"],
+    "@typescript-eslint/no-require-imports": "off",
     "@typescript-eslint/explicit-function-return-type": [
       "error",
       {
-        "allowExpressions": true
-      }
+        allowExpressions: true,
+      },
     ],
     "import/prefer-default-export": "off",
     "no-restricted-globals": [
       "error",
       {
-        "name": "browser",
-        "message": "You probably need to import webextension-polyfill."
-      }
-    ]
+        name: "browser",
+        message: "You probably need to import webextension-polyfill.",
+      },
+    ],
   },
-  "settings": {
+  settings: {
     "import/parsers": {
-      "@typescript-eslint/parser": [".ts"]
+      "@typescript-eslint/parser": [".ts"],
     },
     "import/resolver": {
-      "typescript": {
-        "paths": ["./src"]
-      }
-    }
-  }
+      typescript: {
+        paths: ["./src"],
+      },
+    },
+  },
 };
