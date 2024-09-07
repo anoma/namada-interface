d29eaa62433cfb6cf05b0aa92b0328a2cee479ed
Pedro Rezende <xpedrorezende@gmail.com>
2024-09-07 14:26:55 +0200

    fix: fixing eslintrc project config

--- apps/extension/.eslintrc.js
+++ apps/extension/.eslintrc.js
@@ -1,5 +1,11 @@
 module.exports = {
   extends: require.resolve("@namada/config/eslint/react.js"),
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+    tsconfigRootDir: __dirname,
+    project: [`./tsconfig.json`],
+  },
   overrides: [
     {
       files: ["*.ts", "*.tsx"],
