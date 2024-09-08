b47cbc6cc785b0bc1c1a762cfdd88b57b43a9237
Harrison Mendonça <harrisonmendonca@gmail.com>
2024-09-08 10:10:56 +0200

    feat: add dropdown

--- apps/namadillo/src/App/Common/ActiveAccount.tsx
+++ apps/namadillo/src/App/Common/ActiveAccount.tsx
@@ -3,19 +3,16 @@ import { defaultAccountAtom } from "atoms/accounts";
 import { useAtomValue } from "jotai";
 
 export const ActiveAccount = (): JSX.Element => {
-  const { data: account, isFetching } = useAtomValue(defaultAccountAtom);
+  const { data: account } = useAtomValue(defaultAccountAtom);
 
-  if (!account || isFetching) {
+  if (!account) {
     return <></>;
   }
-
-  const buttonClassName =
-    "p-1 opacity-80 transition-opacity duration-150 hover:opacity-100";
 
   return (
     <span className="flex items-center gap-2 relative group/tooltip">
       <CopyToClipboardControl
-        className={buttonClassName}
+        className="p-1 opacity-80 transition-opacity hover:opacity-100"
         value={account.address || ""}
       >
         {account.alias}
--- apps/namadillo/src/App/Common/SwitchAccount.tsx
+++ apps/namadillo/src/App/Common/SwitchAccount.tsx
@@ -1,29 +1,51 @@
-import { getIntegration } from "@namada/integrations/hooks/useIntegration";
-import { updateDefaultAccountAtom } from "atoms/accounts";
+import { accountsAtom, updateDefaultAccountAtom } from "atoms/accounts";
 import { useAtomValue } from "jotai";
+import { useState } from "react";
+import { twMerge } from "tailwind-merge";
 import { SwitchAccountIcon } from "./SwitchAccountIcon";
 
 export const SwitchAccount = (): JSX.Element => {
+  const { data } = useAtomValue(accountsAtom);
   const { mutateAsync: updateAccount } = useAtomValue(updateDefaultAccountAtom);
 
-  const buttonClassName =
-    "p-1 opacity-80 transition-opacity duration-150 hover:opacity-100";
+  const [open, setOpen] = useState(false);
 
   return (
-    <button
-      className={buttonClassName}
-      onClick={async () => {
-        const integration = getIntegration("namada");
-        const accounts = (await integration.accounts())?.filter(
-          (a) => !a.isShielded
-        );
-        if (!accounts) return;
-        const { address } =
-          accounts[Math.floor(Math.random() * accounts?.length)];
-        updateAccount(address);
-      }}
-    >
-      <SwitchAccountIcon />
-    </button>
+    <div className="relative">
+      <button
+        className="p-1 opacity-80 transition-opacity hover:opacity-100"
+        onClick={() => setOpen(true)}
+      >
+        <SwitchAccountIcon />
+      </button>
+      {open && (
+        <div
+          className={twMerge(
+            "absolute right-0 z-10",
+            "bg-black py-1",
+            "border border-yellow rounded-sm"
+          )}
+        >
+          {data
+            ?.filter((i) => !i.isShielded)
+            .map(({ alias, address }) => (
+              <button
+                key={alias}
+                className={twMerge(
+                  "block w-full py-1 px-2",
+                  "whitespace-nowrap text-left",
+                  "cursor-pointer hover:text-yellow transition-colors"
+                )}
+                onClick={async () => {
+                  setOpen(false);
+                  updateAccount(address);
+                }}
+              >
+                {alias}
+              </button>
+            ))}
+        </div>
+      )}
+    </div>
   );
 };
