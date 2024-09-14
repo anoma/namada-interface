e9616a919b3f1263cae44a10ac3589eb02b3f12c
Harrison Mendonça <harrisonmendonca@gmail.com>
2024-09-14 18:13:28 +0200

    feat: create new routes

---
+++ apps/namadillo/src/App/Transfer/ShieldAsset.tsx
@@ -0,0 +1,14 @@
+import { PageWithSidebar } from "App/Common/PageWithSidebar";
+
+export const ShieldTransfer: React.FC = () => {
+  return (
+    <PageWithSidebar>
+      <div className="flex flex-col gap-2 text-yellow">
+        <div>Shield Transfer (WIP)</div>
+      </div>
+      <aside className="flex flex-col gap-2 text-yellow">
+        <div>Sidebar (WIP)</div>
+      </aside>
+    </PageWithSidebar>
+  );
+};
--- apps/namadillo/src/App/Transfer/routes.ts
+++ apps/namadillo/src/App/Transfer/routes.ts
@@ -4,12 +4,18 @@ export const index = (): string => `/transfer`;
 
 const routeOutput = createRouteOutput(index);
 
-export const overview = (): RouteOutput => routeOutput(`/`);
+export const overview = (): RouteOutput => routeOutput("/");
 
-export const shieldAll = (): RouteOutput => routeOutput(`/shield-all`);
+export const transfer = (): RouteOutput => routeOutput("/transfer");
+
+export const masp = (): RouteOutput => routeOutput("/masp");
+
+export const shieldAll = (): RouteOutput => routeOutput("/shield-all");
 
 export default {
   index,
   overview,
+  transfer,
+  shield: masp,
   shieldAll,
 };
