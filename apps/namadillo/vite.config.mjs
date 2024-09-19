c1b221735a7e52f26b6b82938ec59205a635ef93
Pedro Rezende <pedrorezende@users.noreply.github.com>
2024-09-19 10:18:14 -0300

    fix(namadillo): removing unused .env injection on Vite config (#1127)

--- apps/namadillo/vite.config.mjs
+++ apps/namadillo/vite.config.mjs
@@ -1,22 +1,11 @@
 /* eslint-disable */
 import react from "@vitejs/plugin-react";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import checker from "vite-plugin-checker";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-  const filteredEnv = Object.keys(env).reduce((acc, current) => {
-    if (current.startsWith("NAMADA_INTERFACE_")) {
-      return {
-        ...acc,
-        [current]: env[current],
-      };
-    }
-    return acc;
-  }, {});
-
+export default defineConfig(() => {
   return {
     plugins: [
       react(),
@@ -30,9 +19,6 @@ export default defineConfig(({ mode }) => {
         overlay: { initialIsOpen: false },
       }),
     ],
-    define: {
-      "process.env": filteredEnv,
-    },
     optimizeDeps: {
       esbuildOptions: {
         // Node.js global to browser globalThis
