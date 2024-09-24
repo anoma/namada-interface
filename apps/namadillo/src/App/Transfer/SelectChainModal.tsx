0fd778841add1f4841bc8e3bd7a323b5e770444d
Pedro Rezende <xpedrorezende@gmail.com>
2024-09-24 16:42:15 -0300

    feat(namadillo): writing tests for SelectChainModal

--- apps/namadillo/src/App/Transfer/SelectChainModal.tsx
+++ apps/namadillo/src/App/Transfer/SelectChainModal.tsx
@@ -1,30 +1,36 @@
+import { Chain, Chains } from "@chain-registry/types";
 import { Stack } from "@namada/components";
 import { SelectModal } from "App/Common/SelectModal";
 import clsx from "clsx";
-import { Chain } from "types";
 
 type SelectChainModalProps = {
   onClose: () => void;
-  chains: Chain[];
+  onSelect: (chain: Chain) => void;
+  chains: Chains;
 };
 
 export const SelectChainModal = ({
   onClose,
+  onSelect,
   chains,
 }: SelectChainModalProps): JSX.Element => {
   return (
     <SelectModal title="Select Source Chain" onClose={onClose}>
       <Stack as="ul">
         {chains.map((chain) => (
-          <li key={chain.chainId}>
+          <li key={chain.chain_id}>
             <button
+              onClick={() => onSelect(chain)}
               className={clsx(
                 "grid grid-cols-[30px_auto] w-full px-6 py-2.5 rounded-sm border",
                 "hover:border-neutral-400"
               )}
             >
-              <img src={chain.iconUrl} />
-              <span>{chain.name}</span>
+              <img
+                src={chain.logo_URIs?.svg}
+                alt={chain.pretty_name + " logo"}
+              />
+              <span>{chain.pretty_name}</span>
             </button>
           </li>
         ))}
