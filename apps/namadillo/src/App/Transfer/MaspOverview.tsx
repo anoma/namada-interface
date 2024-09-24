3310d6ee96716b772b33fd782cd2af3abf4c256f
Harrison Mendonça <harrisonmendonca@gmail.com>
2024-09-24 16:05:50 -0300

    feat: add wip page

---
+++ apps/namadillo/src/App/Transfer/MaspOverview.tsx
@@ -0,0 +1,14 @@
+import { PageWithSidebar } from "App/Common/PageWithSidebar";
+
+export const MaspOverview: React.FC = () => {
+  return (
+    <PageWithSidebar>
+      <div className="flex flex-col gap-2 text-yellow">
+        <div>MaspOverview (WIP)</div>
+      </div>
+      <aside className="flex flex-col gap-2 text-yellow">
+        <div>Sidebar (WIP)</div>
+      </aside>
+    </PageWithSidebar>
+  );
+};
--- apps/namadillo/src/App/Transfer/routes.ts
+++ apps/namadillo/src/App/Transfer/routes.ts
@@ -6,6 +6,8 @@ const routeOutput = createRouteOutput(index);
 
 export const overview = (): RouteOutput => routeOutput("/");
 
+export const masp = (): RouteOutput => routeOutput("/masp");
+
 export const namTransfer = (): RouteOutput => routeOutput("/nam");
 
 export const shield = (): RouteOutput => routeOutput("/shield");
@@ -17,6 +19,7 @@ export const ibcTransfer = (): RouteOutput => routeOutput(`/ibc`);
 export default {
   index,
   overview,
+  masp,
   namTransfer,
   shield,
   shieldAll,
