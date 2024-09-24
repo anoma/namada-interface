519dd3ceed426e2c950471542fb99101e74ca00b
Pedro Rezende <xpedrorezende@gmail.com>
2024-09-24 16:42:15 -0300

    feat(namadillo): writing tests for SelectChainModal

--- apps/namadillo/src/App/Transfer/__mocks__/chains.ts
+++ apps/namadillo/src/App/Transfer/__mocks__/chains.ts
@@ -1,14 +1,14 @@
 import { Chain } from "@chain-registry/types";
 
 export const namadaChainMock: Partial<Chain> = {
-  chain_id: "test",
+  chain_id: "namada-mock-chain-id",
   chain_name: "namada",
   pretty_name: "Namada",
   logo_URIs: { svg: "namada-icon" },
 };
 
 export const randomChainMock: Partial<Chain> = {
-  chain_id: "test",
+  chain_id: "testchain-mock-chain-id",
   chain_name: "testchain",
   pretty_name: "TestChain",
   logo_URIs: { svg: "testchain-icon" },
