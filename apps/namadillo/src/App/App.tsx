cad474ed31742536a39e4d0621bbf9c0be53b349
Justin R. Evans <330911+jurevans@users.noreply.github.com>
2024-11-14 19:58:00 +0000

    feat/1251: Fetch applications feature flags from registry (#1255)
    * feat: fetch applications settings from registry
    
    * feat: update chain lookup, add shielded rewards and failsafe config
    
    * fix: lint - temporarily disable FeaturesCheckbox
    
    * feat: fix tests, revert unneeded changes, add hook

--- apps/namadillo/src/App/App.tsx
+++ apps/namadillo/src/App/App.tsx
@@ -2,6 +2,7 @@ import { Toasts } from "App/Common/Toast";
 import { AppLayout } from "App/Layout/AppLayout";
 import { createBrowserHistory } from "history";
 import { useExtensionEvents } from "hooks/useExtensionEvents";
+import { useRegistryFeatures } from "hooks/useRegistryFeatures";
 import { useTransactionCallback } from "hooks/useTransactionCallbacks";
 import { useTransactionNotifications } from "hooks/useTransactionNotifications";
 import { Outlet } from "react-router-dom";
@@ -14,6 +15,7 @@ export function App(): JSX.Element {
   useExtensionEvents();
   useTransactionNotifications();
   useTransactionCallback();
+  useRegistryFeatures();
 
   return (
     <>
