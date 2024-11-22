57e9e71ba04020fe3027161275f4db668f8ec202
Harrison Mendonça <harrisonmendonca@gmail.com>
2024-11-22 17:14:40 -0300

    feat: update active account

--- apps/namadillo/src/App/Layout/DisconnectAccountIcon.tsx
+++ apps/namadillo/src/App/Layout/DisconnectAccountIcon.tsx
@@ -1,8 +1,8 @@
 export const DisconnectAccountIcon = (): JSX.Element => {
   return (
     <svg
-      width="12"
-      height="12"
+      width="18"
+      height="18"
       viewBox="0 0 12 12"
       fill="currentColor"
       xmlns="http://www.w3.org/2000/svg"
