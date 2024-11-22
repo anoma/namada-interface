208ace521720485608f224ef24637f9eeebd5255
Justin R. Evans <jurevans@gmail.com>
2024-11-22 06:51:52 -0500

    feat: add commit hash, link to revision

--- apps/namadillo/src/App/Settings/SettingsMain.tsx
+++ apps/namadillo/src/App/Settings/SettingsMain.tsx
@@ -3,6 +3,8 @@ import { version as sdkVersion } from "../../../../../packages/sdk/package.json"
 import { version } from "../../../package.json";
 import { SettingsPanelMenuItem } from "./SettingsPanelMenuItem";
 
+const { VITE_GIT_SHORT, VITE_GIT_LONG } = import.meta.env;
+
 export const SettingsMain = (): JSX.Element => {
   return (
     <div className="flex flex-1 justify-between flex-col">
@@ -15,6 +17,16 @@ export const SettingsMain = (): JSX.Element => {
       </ul>
       <div className="text-xs">
         <div>Namadillo Version: {version}</div>
+        <div>
+          Revision:{" "}
+          <a
+            href={`https://github.com/anoma/namada-interface/commit/${VITE_GIT_LONG}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            {VITE_GIT_SHORT}
+          </a>
+        </div>
         <div>SDK Version: {sdkVersion}</div>
       </div>
     </div>
