f87f9db444b6b76576e03d4f81a1bbe786b004f5
Mateusz Jasiuk <mateusz.jasiuk@gmail.com>
2024-11-25 10:14:42 +0100

    fix: unit tests

--- apps/namadillo/src/registry/namadaAsset.ts
+++ apps/namadillo/src/registry/namadaAsset.ts
@@ -1,19 +1,21 @@
 import { Asset } from "@chain-registry/types";
-import { localnetConfigAtom } from "atoms/integrations";
+import { localnetConfigAtom } from "atoms/integrations/atoms";
 import { getDefaultStore } from "jotai";
 import namadaShieldedSvg from "./assets/namada-shielded.svg";
+
+const ADDRESS = "tnam1qxp7u2vmlgcrpn9j0ml7hrtr79g2w2fdesteh7tu";
 
 // We can't return "satisfies Asset" from fn
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export const namadaAsset = () => {
   const store = getDefaultStore();
   const config = store.get(localnetConfigAtom);
-  const localNamAddress = config.data?.tokenAddress;
+  const address = config.data?.tokenAddress || ADDRESS;
 
   return {
     name: "Namada",
     base: "unam",
-    address: localNamAddress || "tnam1qxp7u2vmlgcrpn9j0ml7hrtr79g2w2fdesteh7tu",
+    address,
     display: "nam",
     symbol: "NAM",
     denom_units: [
--- apps/namadillo/src/setupTests.ts
+++ apps/namadillo/src/setupTests.ts
@@ -1,6 +1,11 @@
 import "@testing-library/jest-dom";
+import { atom } from "jotai";
 
 jest.mock("atoms/integrations", () => ({
   getRestApiAddressByIndex: jest.fn(),
   getRpcByIndex: jest.fn(),
+}));
+
+jest.mock("atoms/integrations/atoms", () => ({
+  localnetConfigAtom: atom({ data: undefined }),
 }));
