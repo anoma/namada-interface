9153529c67e818d4bfc5acb66f5ad61c449b2ca9
Mateusz Jasiuk <mateusz.jasiuk@gmail.com>
2024-12-11 13:04:41 +0100

    fix: undefined TextDecoder

--- apps/namadillo/src/setupTests.ts
+++ apps/namadillo/src/setupTests.ts
@@ -1,5 +1,8 @@
 import "@testing-library/jest-dom";
 import { atom } from "jotai";
+import { TextDecoder, TextEncoder } from "util";
+
+Object.assign(global, { TextDecoder, TextEncoder });
 
 jest.mock("atoms/integrations", () => ({
   getRestApiAddressByIndex: jest.fn(),
