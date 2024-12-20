40efe695602751529f03f9d895179a83934a076c
Harrison Mendonça <harrisonmendonca@gmail.com>
2024-12-20 12:10:46 -0300

    feat: add remaining time

--- apps/namadillo/src/utils/dates.ts
+++ apps/namadillo/src/utils/dates.ts
@@ -1,4 +1,3 @@
-import * as fns from "date-fns";
 import { DateTime } from "luxon";
 
 export const epochToString = (epoch: bigint): string =>
@@ -22,33 +21,7 @@ export const secondsToFullDateTimeString = (seconds: bigint): string =>
 export const secondsToTimeRemainingString = (
   startTimeInSeconds: bigint,
   endTimeInSeconds: bigint
-): string | undefined => {
-  // TODO is this useful?
-  return;
-  if (endTimeInSeconds < startTimeInSeconds) {
-    return undefined;
-  }
-
-  const toMilliNumber = (n: bigint): number =>
-    fns.secondsToMilliseconds(Number(n));
-  const interval = {
-    start: toMilliNumber(startTimeInSeconds),
-    end: toMilliNumber(endTimeInSeconds),
-  };
-  const format: fns.DurationUnit[] = ["days", "hours", "minutes"];
-  const timeLeft = fns.intervalToDuration(interval);
-  const formatted = fns.formatDuration(timeLeft, {
-    format,
-    zero: true,
-    delimiter: ": ",
-  });
-
-  if (formatted === "") {
-    return "< 1 Min";
-  }
-
-  return formatted
-    .replace("day", "Day")
-    .replace("hour", "Hr")
-    .replace("minute", "Min");
-};
+): string =>
+  secondsToDateTime(endTimeInSeconds)
+    .diff(secondsToDateTime(startTimeInSeconds), ["days", "hours", "minutes"])
+    .toHuman();
