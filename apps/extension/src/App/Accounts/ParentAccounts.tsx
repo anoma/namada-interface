753529ffa015bf843d573549d5a2f98bf5650470
Mateusz Jasiuk <mateusz.jasiuk@gmail.com>
2024-12-30 11:49:17 +0100

    feat: notify about needed account re import

--- apps/extension/src/App/Accounts/ParentAccounts.tsx
+++ apps/extension/src/App/Accounts/ParentAccounts.tsx
@@ -1,5 +1,5 @@
-import { useContext } from "react";
-import { useNavigate } from "react-router-dom";
+import { useContext, useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 
 import {
   ActionButton,
@@ -7,7 +7,7 @@ import {
   KeyListItem,
   Stack,
 } from "@namada/components";
-import { DerivedAccount } from "@namada/types";
+import { AccountType, DerivedAccount } from "@namada/types";
 import { ParentAccountsFooter } from "App/Accounts/ParentAccountsFooter";
 import { PageHeader } from "App/Common";
 import routes from "App/routes";
@@ -20,8 +20,37 @@ import { openSetupTab } from "utils";
  */
 export const ParentAccounts = (): JSX.Element => {
   const navigate = useNavigate();
-  const { activeAccountId, parentAccounts, changeActiveAccountId } =
-    useContext(AccountContext);
+  const {
+    activeAccountId,
+    parentAccounts,
+    accounts: allAccounts,
+    changeActiveAccountId,
+  } = useContext(AccountContext);
+
+  // We check which accounts need to be re-imported
+  const accounts = allAccounts
+    .filter(
+      (account) => account.parentId || account.type === AccountType.Ledger
+    )
+    .map((account) => {
+      const outdated =
+        account.type !== AccountType.Ledger &&
+        typeof account.pseudoExtendedKey === "undefined";
+
+      // The only account without a parent is the ledger account
+      const parent =
+        parentAccounts.find((pa) => pa.id === account.parentId) || account;
+
+      return { ...parent, outdated };
+    });
+
+  useEffect(() => {
+    const hasOutdatedAccounts = accounts.some((account) => account.outdated);
+    // If there are outdated accounts, we redirect to the update required page
+    if (hasOutdatedAccounts) {
+      navigate(routes.accountsUpdateRequired());
+    }
+  }, []);
 
   const goToSetupPage = async (): Promise<void> => {
     await openSetupTab();
@@ -44,47 +73,52 @@ export const ParentAccounts = (): JSX.Element => {
   };
 
   return (
-    <Stack
-      gap={GapPatterns.TitleContent}
-      full
-      className="max-h-[calc(100vh-40px)]"
-    >
-      <PageHeader title="Select Account" />
-      <Stack gap={4} className="flex-1 overflow-auto">
-        <nav className="grid items-end grid-cols-[auto_min-content]">
-          <p className="text-white font-medium text-xs">Set default keys</p>
-          <div className="w-26">
-            <ActionButton size="xs" onClick={goToSetupPage}>
-              Add Keys
-            </ActionButton>
-          </div>
-        </nav>
-        <Stack as="ul" gap={3} className="flex-1 overflow-auto">
-          {[...parentAccounts].reverse().map((account, idx) => (
-            <KeyListItem
-              key={`key-listitem-${account.id}`}
-              as="li"
-              alias={account.alias}
-              type={account.type}
-              dropdownPosition={
-                idx > 2 && idx > parentAccounts.length - 4 ? "bottom" : "top"
-              }
-              isMainKey={activeAccountId === account.id}
-              onRename={() => goToRenameAccount(account)}
-              onDelete={() => goToDeletePage(account)}
-              onViewAccount={() => goToViewAccount(account)}
-              onViewRecoveryPhrase={() => goToViewRecoveryPhrase(account)}
-              onSelectAccount={() => {
-                changeActiveAccountId(
-                  account.id,
-                  account.type as ParentAccount
-                );
-              }}
-            />
-          ))}
+    <>
+      <Stack
+        gap={GapPatterns.TitleContent}
+        full
+        className="max-h-[calc(100vh-40px)]"
+      >
+        <PageHeader title="Select Account" />
+        <Stack gap={4} className="flex-1 overflow-auto">
+          <nav className="grid items-end grid-cols-[auto_min-content]">
+            <p className="text-white font-medium text-xs">Set default keys</p>
+            <div className="w-26">
+              <ActionButton size="xs" onClick={goToSetupPage}>
+                Add Keys
+              </ActionButton>
+            </div>
+          </nav>
+          <Stack as="ul" gap={3} className="flex-1 overflow-auto">
+            {[...accounts].reverse().map((account, idx) => (
+              <KeyListItem
+                key={`key-listitem-${account.id}`}
+                as="li"
+                alias={account.alias}
+                type={account.type}
+                outdated={account.outdated}
+                dropdownPosition={
+                  idx > 2 && idx > accounts.length - 4 ? "bottom" : "top"
+                }
+                isMainKey={activeAccountId === account.id}
+                onRename={() => goToRenameAccount(account)}
+                onDelete={() => goToDeletePage(account)}
+                onViewAccount={() => goToViewAccount(account)}
+                onViewRecoveryPhrase={() => goToViewRecoveryPhrase(account)}
+                onSelectAccount={() => {
+                  changeActiveAccountId(
+                    account.id,
+                    account.type as ParentAccount
+                  );
+                }}
+              />
+            ))}
+          </Stack>
+          <ParentAccountsFooter />
         </Stack>
-        <ParentAccountsFooter />
       </Stack>
-    </Stack>
+
+      <Outlet />
+    </>
   );
 };
