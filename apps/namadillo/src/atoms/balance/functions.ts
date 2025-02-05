8b4b9231efc1f2fb15f7e8a2828c634c0da88357
Harrison Mendonça <harrisonmendonca@gmail.com>
2025-02-05 12:41:09 -0300

    feat: remove the n/a message

--- apps/namadillo/src/atoms/balance/functions.ts
+++ apps/namadillo/src/atoms/balance/functions.ts
@@ -1,42 +1,16 @@
 import { IbcToken, NativeToken } from "@namada/indexer-client";
-import { nativeTokenAddressAtom } from "atoms/chain";
 import { mapCoinsToAssets } from "atoms/integrations";
 import BigNumber from "bignumber.js";
 import { DenomTrace } from "cosmjs-types/ibc/applications/transfer/v1/transfer";
-import { getDefaultStore } from "jotai";
 import { AddressWithAssetAndAmountMap } from "types";
 import { isNamadaAsset } from "utils";
 import { TokenBalance } from "./atoms";
 
-const getNativeTokenAddress = (): string | undefined => {
-  const { get } = getDefaultStore();
-  return get(nativeTokenAddressAtom).data;
-};
-
-/**
- * Sum the dollar amount of a list of tokens
- * @param tokens
- * @returns The total of dollars, or `undefined` if at least one token has `dollar: undefined`
- */
-export const sumDollars = (tokens: TokenBalance[]): BigNumber | undefined => {
-  let sum = new BigNumber(0);
-  for (let i = 0; i < tokens.length; i++) {
-    const { dollar, originalAddress } = tokens[i];
-    if (!dollar) {
-      // create an exception for native token while we don't have a market price for it
-      // we can safely delete this condition when osmosis api returns a native token price
-      if (originalAddress === getNativeTokenAddress()) {
-        continue;
-      }
-      return undefined;
-    }
-    sum = sum.plus(dollar);
-  }
-  return sum;
-};
-
-export const getTotalDollar = (list?: TokenBalance[]): BigNumber | undefined =>
-  sumDollars(list ?? []);
+export const getTotalDollar = (list?: TokenBalance[]): BigNumber =>
+  (list ?? []).reduce(
+    (sum, { dollar }) => (dollar ? sum.plus(dollar) : sum),
+    new BigNumber(0)
+  );
 
 export const getTotalNam = (list?: TokenBalance[]): BigNumber =>
   list?.find((i) => isNamadaAsset(i.asset))?.amount ?? new BigNumber(0);
