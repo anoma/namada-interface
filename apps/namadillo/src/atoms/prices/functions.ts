d245b7bfd442d59c0e980d4064503ef728391217
Harrison Mendonça <harrisonmendonca@gmail.com>
2025-02-05 13:03:13 -0300

    feat: map dollar to all tokens

--- apps/namadillo/src/atoms/prices/functions.ts
+++ apps/namadillo/src/atoms/prices/functions.ts
@@ -1,22 +1,26 @@
 import { IbcToken, NativeToken } from "@namada/indexer-client";
 import BigNumber from "bignumber.js";
 import * as osmosis from "chain-registry/mainnet/osmosis";
-import { Address } from "types";
+import { Address, BaseDenom } from "types";
 import { findAssetByToken } from "utils/assets";
 import { fetchCoinPrices } from "./services";
 
 export const fetchTokenPrices = async (
   tokenAddressToFetch: Address[],
-  apiTokens: (NativeToken | IbcToken)[]
+  chainTokens: (NativeToken | IbcToken)[]
 ): Promise<Record<Address, BigNumber>> => {
-  const baseMap: Record<string, string> = {};
+  const baseMap: Record<BaseDenom, Address[]> = {};
   tokenAddressToFetch.forEach((address) => {
-    const token = apiTokens?.find((t) => t.address === address);
+    const token = chainTokens?.find((t) => t.address === address);
     if (token) {
       // searching only on osmosis because these are the assets supported by fetchCoinPrices
       const asset = findAssetByToken(token, osmosis.assets.assets);
       if (asset) {
-        baseMap[asset.base] = address;
+        if (baseMap[asset.base]) {
+          baseMap[asset.base].push(address);
+        } else {
+          baseMap[asset.base] = [address];
+        }
       }
     }
   });
@@ -25,10 +29,11 @@ export const fetchTokenPrices = async (
 
   const tokenPrices: Record<string, BigNumber> = {};
   Object.entries(apiResponse).forEach(([base, value]) => {
-    const address = baseMap[base];
     const dollar = Object.values(value)[0];
     if (dollar) {
-      tokenPrices[address] = new BigNumber(dollar);
+      baseMap[base].forEach((address) => {
+        tokenPrices[address] = new BigNumber(dollar);
+      });
     }
   });
   return tokenPrices;
