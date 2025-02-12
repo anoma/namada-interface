88d8939b35b1426baf8b117401c3cc8a61e3dd98
Harrison Mendonça <harrisonmendonca@gmail.com>
2025-02-12 16:26:14 -0300

    feat: add shielded overview label

--- apps/namadillo/src/App/AccountOverview/MaspBanner.tsx
+++ apps/namadillo/src/App/AccountOverview/MaspBanner.tsx
@@ -13,38 +13,37 @@ export const MaspBanner = (): JSX.Element => {
   const total = getTotalDollar(shieldedTokensQuery.data);
 
   return (
-    <Panel
-      className={twMerge(
-        "relative p-10 border border-yellow",
-        "flex items-center flex-wrap gap-10",
-        "text-yellow"
-      )}
-    >
-      <div className="relative h-[170px] w-[170px] flex items-center justify-center">
-        <img src={maspBg} className="absolute" />
-        <div className="col-start-1 row-start-1 text-4xl">MASP</div>
+    <Panel className="border border-yellow" title="Shielded Overview">
+      <div className="p-10 pt-5 flex items-center flex-wrap gap-10 text-yellow">
+        <div className="relative h-[170px] w-[170px] flex items-center justify-center">
+          <img src={maspBg} className="absolute" />
+          <div className="col-start-1 row-start-1 text-4xl">MASP</div>
+        </div>
+        <div className="flex-1">
+          <div className="text-sm">Total shielded balance</div>
+          {total ?
+            <FiatCurrency
+              className={twMerge(
+                "text-4xl",
+                isShieldSyncing && "animate-pulse"
+              )}
+              amount={total}
+            />
+          : <SkeletonLoading height="54px" width="120px" />}
+        </div>
+        <ActionButton
+          size="md"
+          href={routes.masp}
+          className="self-end justify-end"
+          outlineColor="yellow"
+          backgroundColor="yellow"
+          backgroundHoverColor="transparent"
+          textColor="black"
+          textHoverColor="yellow"
+        >
+          Manage your shielded assets
+        </ActionButton>
       </div>
-      <div className="flex-1">
-        <div className="text-sm">Total shielded balance</div>
-        {total ?
-          <FiatCurrency
-            className={twMerge("text-4xl", isShieldSyncing && "animate-pulse")}
-            amount={total}
-          />
-        : <SkeletonLoading height="54px" width="120px" />}
-      </div>
-      <ActionButton
-        size="md"
-        href={routes.masp}
-        className="self-end justify-end"
-        outlineColor="yellow"
-        backgroundColor="yellow"
-        backgroundHoverColor="transparent"
-        textColor="black"
-        textHoverColor="yellow"
-      >
-        Manage your shielded assets
-      </ActionButton>
     </Panel>
   );
 };
