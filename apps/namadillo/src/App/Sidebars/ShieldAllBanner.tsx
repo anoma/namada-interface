975157321a59816fea6fc84f4a639aa849354924
Harrison Mendonça <harrisonmendonca@gmail.com>
2025-02-24 13:17:54 -0300

    feat: add shield assets modal on sidebar banner (#1724)

--- apps/namadillo/src/App/Sidebars/ShieldAllBanner.tsx
+++ apps/namadillo/src/App/Sidebars/ShieldAllBanner.tsx
@@ -1,40 +1,36 @@
 import { ActionButton } from "@namada/components";
 import svgImg from "App/Assets/ShieldedParty.svg";
-import { routes } from "App/routes";
+import { ShieldAssetsModal } from "App/Common/ShieldAssetsModal";
 import { applicationFeaturesAtom } from "atoms/settings";
 import { useAtomValue } from "jotai";
 import { useState } from "react";
-import { Link } from "react-router-dom";
 import { twMerge } from "tailwind-merge";
 
 export const ShieldAllBanner = (): JSX.Element => {
   const { maspEnabled } = useAtomValue(applicationFeaturesAtom);
   const [isAnimating, setIsAnimating] = useState(false);
+  const [shieldingModalOpen, setShieldingModalOpen] = useState(false);
 
   if (!maspEnabled) {
     return <></>;
   }
 
   return (
-    <div
-      className={twMerge(
-        "bg-yellow rounded-sm h-fit",
-        "flex flex-col items-center gap-2",
-        "p-3"
-      )}
-    >
-      <img
-        className="h-[145px] w-full"
-        // hover effect with :target for external loaded svg with <img>
-        // https://developer.mozilla.org/en-US/docs/Web/CSS/:target
-        // https://gist.github.com/LeaVerou/5198257
-        src={`${svgImg}${isAnimating ? "#hover " : ""}`}
-      />
-      <Link
-        to={routes.maspShield}
-        onMouseEnter={() => setIsAnimating(true)}
-        onMouseLeave={() => setIsAnimating(false)}
+    <>
+      <div
+        className={twMerge(
+          "bg-yellow rounded-sm h-fit",
+          "flex flex-col items-center gap-2",
+          "p-3"
+        )}
       >
+        <img
+          className="h-[145px] w-full"
+          // hover effect with :target for external loaded svg with <img>
+          // https://developer.mozilla.org/en-US/docs/Web/CSS/:target
+          // https://gist.github.com/LeaVerou/5198257
+          src={`${svgImg}${isAnimating ? "#hover " : ""}`}
+        />
         <ActionButton
           as="div"
           className="max-w-[160px]"
@@ -44,10 +40,16 @@ export const ShieldAllBanner = (): JSX.Element => {
           backgroundHoverColor="transparent"
           textColor="yellow"
           textHoverColor="black"
+          onClick={() => setShieldingModalOpen(true)}
+          onMouseEnter={() => setIsAnimating(true)}
+          onMouseLeave={() => setIsAnimating(false)}
         >
           Shield Assets
         </ActionButton>
-      </Link>
-    </div>
+      </div>
+      {shieldingModalOpen && (
+        <ShieldAssetsModal onClose={() => setShieldingModalOpen(false)} />
+      )}
+    </>
   );
 };
