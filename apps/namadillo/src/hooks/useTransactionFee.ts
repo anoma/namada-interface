d9bf71625e816d0cda5a69838f05bc94dff674db
Pedro Rezende <xpedrorezende@gmail.com>
2025-03-07 17:20:06 +0100

    feat: falling back to other fee tokens if user has no nams

--- apps/namadillo/src/hooks/useTransactionFee.ts
+++ apps/namadillo/src/hooks/useTransactionFee.ts
@@ -1,4 +1,6 @@
 import { GasEstimate } from "@namada/indexer-client";
+import { transparentBalanceAtom } from "atoms/accounts";
+import { shieldedBalanceAtom } from "atoms/balance";
 import { nativeTokenAddressAtom } from "atoms/chain";
 import {
   gasEstimateFamily,
@@ -8,7 +10,7 @@ import {
 } from "atoms/fees";
 import BigNumber from "bignumber.js";
 import { useAtomValue } from "jotai";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { GasConfig } from "types";
 import { TxKind } from "types/txKind";
 
@@ -21,9 +23,14 @@ export type TransactionFeeProps = {
   onChangeGasToken: (value: string) => void;
 };
 
-export const useTransactionFee = (txKinds: TxKind[]): TransactionFeeProps => {
+export const useTransactionFee = (
+  txKinds: TxKind[],
+  isShielded = false
+): TransactionFeeProps => {
   const [gasLimitValue, setGasLimitValue] = useState<BigNumber | undefined>();
   const [gasTokenValue, setGasTokenValue] = useState<string | undefined>();
+  const userTransparentBalances = useAtomValue(transparentBalanceAtom);
+  const userShieldedBalances = useAtomValue(shieldedBalanceAtom);
   const isPublicKeyRevealed = useAtomValue(isPublicKeyRevealedAtom);
 
   const { data: nativeToken, isLoading: isLoadingNativeToken } = useAtomValue(
@@ -37,9 +44,52 @@ export const useTransactionFee = (txKinds: TxKind[]): TransactionFeeProps => {
   const { data: gasPriceTable, isLoading: isLoadingGasPriceTable } =
     useAtomValue(gasPriceTableAtom);
 
+  const availableGasTokenAddress = useMemo(() => {
+    if (!gasPriceTable) return nativeToken;
+
+    if (isShielded && !userShieldedBalances.data) {
+      return nativeToken;
+    }
+
+    if (!isShielded && !userTransparentBalances.data) {
+      return nativeToken;
+    }
+
+    // Separate shielded amount from transparent
+    const balances =
+      (isShielded ?
+        // TODO: we need to refactor userShieldedBalances to return Balance[] type instead
+        userShieldedBalances.data?.map((balance) => ({
+          minDenomAmount: balance.minDenomAmount,
+          tokenAddress: balance.address,
+        }))
+      : userTransparentBalances.data) || [];
+
+    // Check if user has enough NAM to pay fees
+    const nativeAddressBalance = balances.find(
+      (balance) => balance.tokenAddress === nativeToken
+    );
+
+    if (new BigNumber(nativeAddressBalance?.minDenomAmount || "0").gt(0)) {
+      return nativeAddressBalance?.tokenAddress;
+    }
+
+    // Fallback to another token containing balance
+    const gas = gasPriceTable.filter((gas) => {
+      return !!balances.find(
+        (balances) =>
+          balances.tokenAddress === gas.token &&
+          new BigNumber(balances.minDenomAmount).gt(0)
+      );
+    });
+
+    // If no token found, fallback to NAM
+    return gas.length > 0 ? gas[0].token : nativeToken;
+  }, [userTransparentBalances, gasPriceTable, isShielded]);
+
   const averageGasLimit = gasEstimate && BigNumber(gasEstimate.avg);
   const gasLimit = gasLimitValue ?? averageGasLimit ?? BigNumber(0);
-  const gasToken = gasTokenValue ?? nativeToken ?? "";
+  const gasToken = gasTokenValue ?? availableGasTokenAddress ?? "";
   const gasPrice =
     gasPriceTable?.find((i) => i.token === gasToken)?.gasPrice ?? BigNumber(0);
 
@@ -50,7 +100,10 @@ export const useTransactionFee = (txKinds: TxKind[]): TransactionFeeProps => {
   };
 
   const isLoading =
-    isLoadingNativeToken || isLoadingGasEstimate || isLoadingGasPriceTable;
+    userTransparentBalances.isLoading ||
+    isLoadingNativeToken ||
+    isLoadingGasEstimate ||
+    isLoadingGasPriceTable;
 
   return {
     gasConfig,
