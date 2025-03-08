37bd3f7d988bdc89667594afc74194fbc086c5f0
Harrison Mendonça <harrisonmendonca@gmail.com>
2025-03-08 15:12:37 +0100

    feat: add masp indexer version (#1861)

--- apps/namadillo/src/atoms/settings/services.ts
+++ apps/namadillo/src/atoms/settings/services.ts
@@ -32,15 +32,27 @@ export const getIndexerCrawlerInfo = async (
   }
 };
 
-export const isMaspIndexerAlive = async (url: string): Promise<boolean> => {
-  if (!isUrlValid(url)) {
-    return false;
-  }
+export const getMaspIndexerHealth = async (
+  url: string
+  // TODO replace the `HealthGet200Response` by the type returned from
+  // the package "@namada/masp-indexer-client" when it is published
+): Promise<HealthGet200Response | undefined> => {
   try {
     const response = await fetch(`${url}/health`);
-    return response.ok && response.status === 200;
+    // Former masp indexers returns the commit hash string instead of json,
+    // then we manually check if the returned string is a json-like:
+    // - If yes, we convert to json with `JSON.parse()` and return it.
+    // - If not, it's the commit hash string, then we manually create a json
+    // This is to avoid compatibility issues. We can remove this condition
+    // when all hosts update the masp indexer
+    const text = await response.text();
+    if (text.startsWith("{")) {
+      return JSON.parse(text);
+    } else {
+      return { commit: text, version: "0.0.0" };
+    }
   } catch {
-    return false;
+    return;
   }
 };
 
