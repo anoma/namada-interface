b373076e78266404c17b0e3eecb7846eb5c093bd
Harrison Mendonça <harrisonmendonca@gmail.com>
2025-03-11 16:40:50 +0100

    feat: check namadillo version to invalidate shielded context (#1887)

--- apps/namadillo/src/hooks/useShouldInvalidateShieldedContext.ts
+++ apps/namadillo/src/hooks/useShouldInvalidateShieldedContext.ts
@@ -3,16 +3,21 @@ import {
   lastInvalidateShieldedContextAtom,
   maspIndexerHeartbeatAtom,
 } from "atoms/settings/atoms";
-import { useAtomValue } from "jotai";
+import { useAtom, useAtomValue } from "jotai";
+import { RESET } from "jotai/utils";
 import { useEffect } from "react";
 import semverSatisfies from "semver/functions/satisfies";
+import { version as namadilloVersion } from "../../package.json";
 import { useInvalidateShieldedContext } from "./useInvalidateShieldedContext";
 
-// change this value to the minimun masp indexer version that demands to clear the context
-const minMaspIndexerVersion = ">=1.1.0";
+// change these values to the minimun versions that demands to clear the context
+const minNamadilloVersion = ">=1.18.0";
+const minMaspIndexerVersion = ">=1.1.3";
 
 export const useShouldInvalidateShieldedContext = (): void => {
-  const lastInvalidate = useAtomValue(lastInvalidateShieldedContextAtom);
+  const [lastInvalidate, setLastInvalidate] = useAtom(
+    lastInvalidateShieldedContextAtom
+  );
   const chainParameters = useAtomValue(chainParametersAtom);
   const maspIndexerInfo = useAtomValue(maspIndexerHeartbeatAtom);
 
@@ -25,13 +30,23 @@ export const useShouldInvalidateShieldedContext = (): void => {
     if (!chainId || !maspIndexerVersion) {
       return;
     }
+
+    const last = lastInvalidate[chainId];
+    // old model saved as string, reset the storage value
+    if (typeof last === "string") {
+      setLastInvalidate(RESET);
+      return;
+    }
+
     if (
+      // namadillo should be updated and satifies the minimum version
+      semverSatisfies(namadilloVersion, minNamadilloVersion) &&
       // masp indexer should be updated and satifies the minimum version
       semverSatisfies(maspIndexerVersion, minMaspIndexerVersion) &&
       // and the last clear context should be done with an old masp indexer version
-      !semverSatisfies(lastInvalidate[chainId] ?? "", minMaspIndexerVersion)
+      !semverSatisfies(last?.maspIndexerVersion ?? "", minMaspIndexerVersion)
     ) {
       invalidateShieldedContext();
     }
-  }, [chainId, maspIndexerVersion]);
+  }, [chainId, maspIndexerVersion, lastInvalidate]);
 };
