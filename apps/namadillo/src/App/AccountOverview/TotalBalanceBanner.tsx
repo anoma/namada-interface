d772f0ab9301fe7b3996eac1241a576aa08b2fa1
Pedro Rezende <xpedrorezende@gmail.com>
2025-03-26 15:51:03 -0300

    feat: desktop version for top balance banner

--- apps/namadillo/src/App/AccountOverview/TotalBalanceBanner.tsx
+++ apps/namadillo/src/App/AccountOverview/TotalBalanceBanner.tsx
@@ -3,6 +3,7 @@ import {
   Panel,
   SkeletonLoading,
   Stack,
+  Tooltip,
 } from "@namada/components";
 import { FiatCurrency } from "App/Common/FiatCurrency";
 import { PulsingRing } from "App/Common/PulsingRing";
@@ -14,8 +15,10 @@ import {
 } from "atoms/balance";
 import { getTotalDollar } from "atoms/balance/functions";
 import { applicationFeaturesAtom } from "atoms/settings";
+import clsx from "clsx";
 import { useRequiresNewShieldedSync } from "hooks/useRequiresNewShieldedSync";
 import { useAtomValue } from "jotai";
+import { GoInfo } from "react-icons/go";
 
 export const TotalBalanceBanner = (): JSX.Element => {
   const { namTransfersEnabled } = useAtomValue(applicationFeaturesAtom);
@@ -29,11 +32,13 @@ export const TotalBalanceBanner = (): JSX.Element => {
   const transparentDollars = getTotalDollar(transparentTokensQuery.data);
   const totalAmountInDollars = shieldedDollars.plus(transparentDollars);
 
-  const balanceIsLoading =
-    shieldedTokensQuery.isLoading || transparentTokensQuery.isLoading;
-
   const balancesHaveLoaded =
     shieldedTokensQuery.isSuccess && transparentTokensQuery.isSuccess;
+
+  const hasErrors =
+    shieldedTokensQuery.isError && transparentTokensQuery.isError;
+
+  const balanceIsLoading = !balancesHaveLoaded && !hasErrors;
 
   return (
     <Panel className="py-4">
@@ -42,19 +47,39 @@ export const TotalBalanceBanner = (): JSX.Element => {
         className="items-center justify-between px-4"
       >
         <div className="text-white">
-          <header className="text-sm mb-3">Total Balance in Namada</header>
+          <header className="text-sm mb-3">
+            <div className="flex items-center">
+              Total Balance in Namada
+              {shouldWaitForShieldedSync && (
+                <span className="relative px-1.5 text-yellow group/tooltip">
+                  <GoInfo />
+                  <Tooltip className="z-40 w-74" position="right">
+                    The total amount of your shielded assets will be available
+                    once Shielded Sync is complete.
+                  </Tooltip>
+                </span>
+              )}
+            </div>
+            {!namTransfersEnabled && (
+              <small className="block text-xxs text-neutral-400">
+                Balance excludes NAM until phase 5
+              </small>
+            )}
+          </header>
           {balanceIsLoading && (
             <SkeletonLoading height="1em" width="200px" className="text-6xl" />
           )}
           {balancesHaveLoaded && (
-            <div className="text-6xl leading-none animate-fade-in">
+            <div className={clsx("flex items-center text-7xl leading-none")}>
               <FiatCurrency amount={totalAmountInDollars} />
-              {!namTransfersEnabled && (
-                <small className="text-xl align-super">&nbsp;*</small>
+              {shouldWaitForShieldedSync && (
+                <span
+                  className="relative text-xs ml-9"
+                  title="Shielded sync in progress..."
+                >
+                  <PulsingRing />
+                </span>
               )}
-              <span className="relative">
-                <PulsingRing />
-              </span>
             </div>
           )}
         </div>
