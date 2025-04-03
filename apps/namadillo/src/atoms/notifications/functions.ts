ce70cae79a7aba2e6b22fd5a43f59aa648aea01c
neocybereth <sean@arrowburn.co.nz>
2025-04-03 18:04:46 +1300

    fix: types

--- apps/namadillo/src/atoms/notifications/functions.ts
+++ apps/namadillo/src/atoms/notifications/functions.ts
@@ -2,10 +2,10 @@ export const notificationIdSeparator = ";";
 import { TxProps } from "@namada/types";
 
 export const createNotificationId = (
-  data?: TxProps["hash"] | TxProps | TxProps["hash"][]
+  data?: TxProps["hash"] | TxProps["hash"][]
 ): string => {
   if (!data) return Date.now().toString();
   if (typeof data === "string") return data;
   if (Array.isArray(data)) return data.join(notificationIdSeparator);
-  return data.hash;
+  return data;
 };
