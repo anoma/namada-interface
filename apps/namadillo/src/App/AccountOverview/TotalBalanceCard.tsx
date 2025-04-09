17daf201fdfec2af5d9f063c94e938c34e461c11
neocybereth <sean@arrowburn.co.nz>
2025-04-09 20:03:07 +1200

    fix: update nam exclusion tezxt

--- apps/namadillo/src/App/AccountOverview/TotalBalanceCard.tsx
+++ apps/namadillo/src/App/AccountOverview/TotalBalanceCard.tsx
@@ -1,7 +1,9 @@
 import { Stack } from "@namada/components";
 import { FiatCurrency } from "App/Common/FiatCurrency";
+import { applicationFeaturesAtom } from "atoms/settings";
 import BigNumber from "bignumber.js";
 import clsx from "clsx";
+import { useAtomValue } from "jotai";
 
 type TotalBalanceCardProps = {
   balanceInFiat: BigNumber;
@@ -14,12 +16,19 @@ export const TotalBalanceCard = ({
   isShielded = false,
   footerButtons,
 }: TotalBalanceCardProps): JSX.Element => {
+  const { namTransfersEnabled } = useAtomValue(applicationFeaturesAtom);
+
   return (
     <div className="px-7">
       <div className={clsx(isShielded ? "text-yellow" : "text-white")}>
         <header className="text-xs">
           Total {isShielded ? "Shielded" : "Unshielded"} Balance
         </header>
+        {!namTransfersEnabled && (
+          <small className="block text-xxs text-neutral-400">
+            Balance excludes NAM until phase 5
+          </small>
+        )}
         <FiatCurrency className="text-5xl" amount={balanceInFiat} />
       </div>
       <Stack
