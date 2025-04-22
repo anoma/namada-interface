fd67dcdd10ef4f23c9c96d7a88d361b6309106ac
Mateusz Jasiuk <mateusz.jasiuk@gmail.com>
2025-04-22 13:25:39 +0200

    feat: hardcoded values for transparent osmosis swap

--- apps/namadillo/src/App/Ibc/OsmosisSwap.tsx
+++ apps/namadillo/src/App/Ibc/OsmosisSwap.tsx
@@ -3,6 +3,7 @@ import { createOsmosisSwapTxAtom } from "atoms/transfer/atoms";
 import BigNumber from "bignumber.js";
 import invariant from "invariant";
 import { useAtomValue } from "jotai";
+import { broadcastTransaction, signTx } from "lib/query";
 import { useEffect } from "react";
 
 export const OsmosisSwap: React.FC = () => {
@@ -17,44 +18,28 @@ export const OsmosisSwap: React.FC = () => {
       invariant(account.data, "No transparent account is found");
 
       const transfer = {
-        amountInBaseDenom: BigNumber(10),
+        amountInBaseDenom: BigNumber(100),
         // osmosis channel
-        channelId: "channel-7",
+        channelId: "channel-13",
         portId: "transfer",
-        token: "tnam1p4zuqqd94csj6zv8n0jxylz9kex4vdsgvg3uglw9",
-        source:
-          "03d5935721000000803fe3d7a9c42c483e5c3840c13eb7dadec2e420f850a769342a2786b58c86215d1fd1ca29f99d94bf033533a30b3461a2dacc4d0f968a080a2a335a085525d18b29f5e59e281a297c35d62299ff82a1525ae327862aca92d01faceebe375af12530bf9eff49e6f90c2eb554db591b1fc30694c716635f0bd2050682d6eeb6a2c5438dd7725495fb866d76db12de4e44ad9be424af57d12c8c19a6dc8664825d8701000000000000000000000000000000000000000000000000000000000000000001f3d7b291d734e35aefd38601bc947778d3adefc9ee2defd8d745fe124e850d0b",
+        token: "tnam1pkwymnfpkjprr59u9tpard62ser5dvm7ggfs7gxh",
+        source: "tnam1qz4u7j7dkxj5wv9xuwy2qemaeeqd450ysgl7pq0r",
         receiver:
-          "osmo1ewll8h7up3g0ca2z9ur9e6dv6an64snxg5k8tmzylg6uprkyhgzszjgdzr",
-        gasSpendingKey:
-          "03d5935721000000803fe3d7a9c42c483e5c3840c13eb7dadec2e420f850a769342a2786b58c86215d1fd1ca29f99d94bf033533a30b3461a2dacc4d0f968a080a2a335a085525d18b29f5e59e281a297c35d62299ff82a1525ae327862aca92d01faceebe375af12530bf9eff49e6f90c2eb554db591b1fc30694c716635f0bd2050682d6eeb6a2c5438dd7725495fb866d76db12de4e44ad9be424af57d12c8c19a6dc8664825d8701000000000000000000000000000000000000000000000000000000000000000001f3d7b291d734e35aefd38601bc947778d3adefc9ee2defd8d745fe124e850d0b",
-        refundTarget: "tnam1qz24lx0mz3y9uahfkxvnurnynqe898actvzyy22y",
+          "osmo1lrlqeq38ephw8mz0c3uzfdpt4fh3fr0s2atur5n33md90m4wx3mqmz7fq6",
       };
       const params = {
         transfer,
         // We want to receive TIA
-        outputDenom: "transfer/channel-10/utia",
-        recipient: "tnam1qqshvryx9pngpk7mmzpzkjkm6klelgusuvmkc0uz",
-        overflow: "tnam1qqshvryx9pngpk7mmzpzkjkm6klelgusuvmkc0uz",
-        slippage: { 0: "1" },
-        localRecoveryAddr: "osmo1sy99khct7t7wth2wus5xpwkcf6n8p0lvrnwelh",
-        route: [
-          {
-            poolId: "1464",
-            tokenOutDenom:
-              "ibc/498A0751C798A0D9A389AA3691123DADA57DAA4FE165D5C75894505B876BA6E4",
-          },
-          {
-            poolId: "1247",
-            tokenOutDenom:
-              "ibc/D79E7D83AB399BFFF93433E54FAA480C191248FC556924A2A8351AE2638B3877",
-          },
-        ],
+        outputDenom: "transfer/channel-14/uatom",
+        recipient: "tnam1qz4u7j7dkxj5wv9xuwy2qemaeeqd450ysgl7pq0r",
+        overflow: "tnam1qz4u7j7dkxj5wv9xuwy2qemaeeqd450ysgl7pq0r",
+        slippage: { slippagePercentage: "20", windowSeconds: BigInt(60) },
+        localRecoveryAddr: "osmo18st0wqx84av8y6xdlss9d6m2nepyqwj6n3q7js",
         osmosisRestRpc: "https://osmosis-rest.publicnode.com",
       };
 
       try {
-        await performOsmosisSwap({
+        const encodedTxData = await performOsmosisSwap({
           signer: {
             publicKey: account.data.publicKey!,
             address: account.data.address!,
@@ -64,9 +49,12 @@ export const OsmosisSwap: React.FC = () => {
           gasConfig: {
             gasLimit: BigNumber(75000),
             gasPriceInMinDenom: BigNumber(0.000001),
-            gasToken: "tnam1p4zuqqd94csj6zv8n0jxylz9kex4vdsgvg3uglw9",
+            gasToken: "tnam1qy440ynh9fwrx8aewjvvmu38zxqgukgc259fzp6h",
           },
         });
+
+        const signedTxs = await signTx(encodedTxData, account.data.address!);
+        await broadcastTransaction(encodedTxData, signedTxs);
       } catch (error) {
         console.error("Error performing Osmosis swap:", error);
       }
