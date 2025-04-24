7a99ce9b7fa0a2e052a0f0e43456ba8f66e5cb2b
neocybereth <sean@arrowburn.co.nz>
2025-04-24 13:31:13 +1200

    fix: combine padding

--- apps/namadillo/src/App/Ibc/IbcTransfersLayout.tsx
+++ apps/namadillo/src/App/Ibc/IbcTransfersLayout.tsx
@@ -19,7 +19,7 @@ export const IbcTransfersLayout = (): JSX.Element => {
   }
 
   const renderOutletContent = (): JSX.Element => (
-    <Panel className="pt-20 pb-20 rounded-t-none h-full">
+    <Panel className="py-20 rounded-t-none h-full">
       <Outlet />
     </Panel>
   );
