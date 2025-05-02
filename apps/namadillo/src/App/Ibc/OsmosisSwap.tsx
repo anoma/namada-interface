0d3a50b0c7d1b1a367f8c0dfe62204111c14bc65
Mateusz Jasiuk <mateusz.jasiuk@gmail.com>
2025-05-02 17:00:24 +0200

    feat: wire in swaps

--- apps/namadillo/src/App/Ibc/OsmosisSwap.tsx
+++ apps/namadillo/src/App/Ibc/OsmosisSwap.tsx
@@ -1,60 +1,81 @@
+import { Asset } from "@chain-registry/types";
+import { Stack } from "@namada/components";
+import { IbcToken } from "@namada/indexer-client";
 import { AccountType, BparamsMsgValue } from "@namada/types";
-import { defaultAccountAtom } from "atoms/accounts";
-import {
-  namadaShieldedAssetsAtom,
-  namadaTransparentAssetsAtom,
-} from "atoms/balance";
-import {
-  osmosisBaseAssetMapAtom,
-  osmosisSymbolAssetMapAtom,
-} from "atoms/chain";
+import { allDefaultAccountsAtom } from "atoms/accounts";
+import { namadaShieldedAssetsAtom } from "atoms/balance";
+import { chainTokensAtom, osmosisSymbolAssetMapAtom } from "atoms/chain";
+import { ibcChannelsFamily } from "atoms/integrations";
 import { SwapResponse, SwapResponseError, SwapResponseOk } from "atoms/swaps";
 import { createOsmosisSwapTxAtom } from "atoms/transfer/atoms";
 import BigNumber from "bignumber.js";
+import { useTransactionFee } from "hooks/useTransactionFee";
 import invariant from "invariant";
 import { useAtomValue } from "jotai";
 import { broadcastTransaction, signTx } from "lib/query";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { AddressWithAssetAndAmount } from "types";
 import { getSdkInstance } from "utils/sdk";
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-async function _fetchPrices(coinGeckoIds: string[]): Promise<any> {
-  const url = `https://api.coingecko.com/api/v3/simple/price?ids=${coinGeckoIds.join()}&vs_currencies=usd`;
-  return await fetch(url).then((res) => res.json());
-}
 
 const SUPPORTED_TOKENS_SYMBOLS = ["OSMO", "ATOM", "TIA"] as const;
 const SLIPPAGE = 0.005;
+const SWAP_CONTRACT_ADDRESS =
+  "osmo1lrlqeq38ephw8mz0c3uzfdpt4fh3fr0s2atur5n33md90m4wx3mqmz7fq6";
 
 export const OsmosisSwap: React.FC = () => {
+  // TODO: need to figure out those maps
   const osmosisSymbolAssetsMap = useAtomValue(osmosisSymbolAssetMapAtom);
-  const osmosisBaseAssetsMap = useAtomValue(osmosisBaseAssetMapAtom);
-  const supportedAssets = SUPPORTED_TOKENS_SYMBOLS.map(
-    (s) => osmosisSymbolAssetsMap[s]
-  );
+  const chainTokens = useAtomValue(chainTokensAtom);
+
   const { mutateAsync: performOsmosisSwap } = useAtomValue(
     createOsmosisSwapTxAtom
   );
-  const [shielded, _setShielded] = useState<boolean>(true);
   const { data: availableAssets, isLoading: _isLoadingAssets } = useAtomValue(
-    shielded ? namadaShieldedAssetsAtom : namadaTransparentAssetsAtom
+    namadaShieldedAssetsAtom
   );
 
-  // const www = Object.values(availableAssets || {})?.[0]?.asset.base || null;
-  const [from, setFrom] = useState<string>("");
-  const [to, setTo] = useState<string>("");
+  const supportedAssets: [Asset, AddressWithAssetAndAmount | undefined][] =
+    SUPPORTED_TOKENS_SYMBOLS.map(
+      // TODO: we should not use availableAssets here
+      (s) => [
+        osmosisSymbolAssetsMap[s],
+        Object.values(availableAssets || {}).find((a) => a.asset.symbol === s),
+      ]
+    );
+
+  const [from, setFrom] = useState<AddressWithAssetAndAmount | undefined>();
+  const [to, setTo] = useState<
+    [Asset, AddressWithAssetAndAmount | undefined] | undefined
+  >();
   const [amount, setAmount] = useState<string>("");
+  const [recipient, setRecipient] = useState<string>(
+    "znam17k7jw0wmvzdzmfm46m8600t9cah5mjl6se75cu9jvwxywk75k3kmxehmxk7wha62l35puzl6srd"
+  );
+  const [localRecoveryAddr, setLocalRecoveryAddress] = useState<string>(
+    "osmo18st0wqx84av8y6xdlss9d6m2nepyqwj6n3q7js"
+  );
   const [quote, setQuote] = useState<
     (SwapResponseOk & { minAmount: string }) | null
   >(null);
+
+  // TODO:  we should get this from the namada assets
+  const toTrace = (
+    chainTokens?.data?.find(
+      (t) => t.address === to?.[1]?.originalAddress
+    ) as IbcToken
+  )?.trace;
+
+  const { data: ibcChannels } = useAtomValue(ibcChannelsFamily("osmosis"));
+
+  const feeProps = useTransactionFee(["IbcTransfer"], true);
 
   useEffect(() => {
     const call = async (): Promise<void> => {
       const quote = await fetch(
         "https://sqs.osmosis.zone/router/quote?" +
           new URLSearchParams({
-            tokenIn: `${amount}${from}`,
-            tokenOutDenom: to,
+            tokenIn: `${amount}${from!.asset.base}`,
+            tokenOutDenom: to![0].base,
             humanDenoms: "false",
           }).toString()
       );
@@ -73,139 +94,116 @@ export const OsmosisSwap: React.FC = () => {
     if (from && to && amount) {
       call();
     }
-  }, [from, to, amount]);
+  }, [from?.originalAddress, to?.[1]?.originalAddress, amount]);
 
-  const account = useAtomValue(defaultAccountAtom);
+  const defaultAccounts = useAtomValue(allDefaultAccountsAtom);
+  const shieldedAccount = defaultAccounts.data?.find(
+    (account) => account.type === AccountType.ShieldedKeys
+  );
+  const transparentAccount = defaultAccounts.data?.find(
+    (account) => account.type !== AccountType.ShieldedKeys
+  );
 
-  useEffect(() => {
-    const handleOsmosisSwap = async (): Promise<void> => {
-      invariant(account.data, "No transparent account is found");
+  const handleOsmosisSwap = useCallback(async () => {
+    invariant(transparentAccount, "No transparent account is found");
+    invariant(shieldedAccount, "No shielded account is found");
+    invariant(from, "No from asset");
+    invariant(ibcChannels, "No ibc channels");
+    invariant(quote, "No quote");
+    invariant(localRecoveryAddr, "No local recovery address");
+    invariant(recipient, "No recipient");
 
-      const transfer = {
-        amountInBaseDenom: BigNumber(100),
-        // osmosis channel
-        channelId: "channel-13",
-        portId: "transfer",
-        token: "tnam1pkwymnfpkjprr59u9tpard62ser5dvm7ggfs7gxh",
-        source: "tnam1qz4u7j7dkxj5wv9xuwy2qemaeeqd450ysgl7pq0r",
-        receiver:
-          "osmo1lrlqeq38ephw8mz0c3uzfdpt4fh3fr0s2atur5n33md90m4wx3mqmz7fq6",
-      };
-      const params = {
-        transfer,
-        outputDenom: "transfer/channel-14/uatom",
-        recipient: "tnam1qz4u7j7dkxj5wv9xuwy2qemaeeqd450ysgl7pq0r",
-        overflow: "tnam1qz4u7j7dkxj5wv9xuwy2qemaeeqd450ysgl7pq0r",
-        slippage: { slippagePercentage: "20", windowSeconds: BigInt(60) },
-        localRecoveryAddr: "osmo18st0wqx84av8y6xdlss9d6m2nepyqwj6n3q7js",
-        osmosisRestRpc: "https://osmosis-rest.publicnode.com",
-      };
+    let bparams: BparamsMsgValue[] | undefined;
+    if (transparentAccount.type === AccountType.Ledger) {
+      const sdk = await getSdkInstance();
+      const ledger = await sdk.initLedger();
+      bparams = await ledger.getBparams();
+      ledger.closeTransport();
+    }
 
-      try {
-        const encodedTxData = await performOsmosisSwap({
-          signer: {
-            publicKey: account.data.publicKey!,
-            address: account.data.address!,
-          },
-          account: account.data,
-          params: [params],
-          gasConfig: {
-            gasLimit: BigNumber(75000),
-            gasPriceInMinDenom: BigNumber(0.000001),
-            gasToken: "tnam1qy440ynh9fwrx8aewjvvmu38zxqgukgc259fzp6h",
-          },
-        });
-
-        const signedTxs = await signTx(encodedTxData, account.data.address!);
-        await broadcastTransaction(encodedTxData, signedTxs);
-      } catch (error) {
-        console.error("Error performing Osmosis swap:", error);
-      }
+    const transfer = {
+      amountInBaseDenom: BigNumber(amount),
+      // osmosis channel
+      channelId: "channel-13",
+      portId: "transfer",
+      token: from.originalAddress,
+      source: shieldedAccount.pseudoExtendedKey!,
+      gasSpendingKey: shieldedAccount.pseudoExtendedKey!,
+      receiver: SWAP_CONTRACT_ADDRESS,
+      bparams,
+      // TODO: replace with disposable signer
+      refundTarget: transparentAccount.address,
+    };
+    const params = {
+      transfer,
+      outputDenom: toTrace,
+      recipient,
+      // TODO: this should also be disposable address most likely
+      overflow: transparentAccount.address,
+      slippage: {
+        0: BigNumber(quote.minAmount)
+          .integerValue(BigNumber.ROUND_DOWN)
+          .toString(),
+      },
+      localRecoveryAddr,
+      // TODO: not sure if hardcoding is ok, maybe we should connect keplr wallet
+      osmosisRestRpc: "https://osmosis-rest.publicnode.com",
     };
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    (window as any).osmosisSwap = handleOsmosisSwap;
+    try {
+      const encodedTxData = await performOsmosisSwap({
+        signer: {
+          // TODO: use disposable signer
+          publicKey: transparentAccount.publicKey!,
+          address: transparentAccount.address!,
+        },
+        account: transparentAccount,
+        params: [params],
+        gasConfig: feeProps.gasConfig,
+      });
 
-    const handleOsmosisShieldedSwap = async (): Promise<void> => {
-      invariant(account.data, "No transparent account is found");
-      const a = account.data;
-
-      let bparams: BparamsMsgValue[] | undefined;
-      if (a.type === AccountType.Ledger) {
-        const sdk = await getSdkInstance();
-        const ledger = await sdk.initLedger();
-        bparams = await ledger.getBparams();
-        ledger.closeTransport();
-      }
-
-      const transfer = {
-        amountInBaseDenom: BigNumber(100),
-        // osmosis channel
-        channelId: "channel-13",
-        portId: "transfer",
-        token: "tnam1pkxwqwhjkulpd2jmc5hmetj6hkfpf0cdfuzfx70c",
-        source:
-          "03d5935721000000803fe3d7a9c42c483e5c3840c13eb7dadec2e420f850a769342a2786b58c86215d1fd1ca29f99d94bf033533a30b3461a2dacc4d0f968a080a2a335a085525d18b29f5e59e281a297c35d62299ff82a1525ae327862aca92d01faceebe375af12530bf9eff49e6f90c2eb554db591b1fc30694c716635f0bd2050682d6eeb6a2c5438dd7725495fb866d76db12de4e44ad9be424af57d12c8c19a6dc8664825d8701000000000000000000000000000000000000000000000000000000000000000001f3d7b291d734e35aefd38601bc947778d3adefc9ee2defd8d745fe124e850d0b",
-        gasSpendingKey:
-          "03d5935721000000803fe3d7a9c42c483e5c3840c13eb7dadec2e420f850a769342a2786b58c86215d1fd1ca29f99d94bf033533a30b3461a2dacc4d0f968a080a2a335a085525d18b29f5e59e281a297c35d62299ff82a1525ae327862aca92d01faceebe375af12530bf9eff49e6f90c2eb554db591b1fc30694c716635f0bd2050682d6eeb6a2c5438dd7725495fb866d76db12de4e44ad9be424af57d12c8c19a6dc8664825d8701000000000000000000000000000000000000000000000000000000000000000001f3d7b291d734e35aefd38601bc947778d3adefc9ee2defd8d745fe124e850d0b",
-        receiver:
-          "osmo1lrlqeq38ephw8mz0c3uzfdpt4fh3fr0s2atur5n33md90m4wx3mqmz7fq6",
-        bparams,
-        refundTarget: "tnam1qqshvryx9pngpk7mmzpzkjkm6klelgusuvmkc0uz",
-      };
-      const params = {
-        transfer,
-        outputDenom: "transfer/channel-13/uosmo",
-        recipient:
-          "znam17k7jw0wmvzdzmfm46m8600t9cah5mjl6se75cu9jvwxywk75k3kmxehmxk7wha62l35puzl6srd",
-        overflow: "tnam1qqshvryx9pngpk7mmzpzkjkm6klelgusuvmkc0uz",
-        slippage: { 0: "1799" },
-        localRecoveryAddr: "osmo18st0wqx84av8y6xdlss9d6m2nepyqwj6n3q7js",
-        osmosisRestRpc: "https://osmosis-rest.publicnode.com",
-      };
-
-      try {
-        const encodedTxData = await performOsmosisSwap({
-          signer: {
-            publicKey: account.data.publicKey!,
-            address: account.data.address!,
-          },
-          account: account.data,
-          params: [params],
-          gasConfig: {
-            gasLimit: BigNumber(75000),
-            gasPriceInMinDenom: BigNumber(0.000001),
-            gasToken: "tnam1qy440ynh9fwrx8aewjvvmu38zxqgukgc259fzp6h",
-          },
-        });
-
-        const signedTxs = await signTx(encodedTxData, account.data.address!);
-        await broadcastTransaction(encodedTxData, signedTxs);
-      } catch (error) {
-        console.error("Error performing Osmosis swap:", error);
-      }
-    };
-
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    (window as any).osmosisShieldedSwap = handleOsmosisShieldedSwap;
-  }, [account.data]);
+      // TODO: use disposable signer
+      const signedTxs = await signTx(
+        encodedTxData,
+        transparentAccount.address!
+      );
+      await broadcastTransaction(encodedTxData, signedTxs);
+      alert("Transaction sent 🚀");
+    } catch (error) {
+      console.error("Error performing Osmosis swap:", error);
+      alert("Transaction errror 🪦");
+    }
+  }, [transparentAccount, shieldedAccount, toTrace, quote]);
 
   return (
     <div className="text-white">
       <div>From:</div>
-      <select className="text-black" onChange={(e) => setFrom(e.target.value)}>
-        <option value=""></option>
-        {Object.values(availableAssets || {}).map((al) => (
-          <option key={al.asset.base} value={al.asset.base}>
-            {al.asset.symbol}
-          </option>
-        ))}
-      </select>
+      <Stack direction="horizontal">
+        <select
+          className="text-black"
+          onChange={(e) => setFrom(availableAssets?.[e.target.value])}
+        >
+          <option value=""></option>
+          {Object.values(availableAssets || {}).map((al) => (
+            <option key={al.asset.base} value={al.originalAddress}>
+              {al.asset.symbol}
+            </option>
+          ))}
+        </select>
+        <div>{from?.amount?.toString()}</div>
+      </Stack>
       <div>To:</div>
-      <select className="text-black" onChange={(e) => setTo(e.target.value)}>
+      <select
+        className="text-black"
+        onChange={(e) =>
+          // TODO: this sucks because avaialbleAssets only shows tokens that we can use,
+          // we need to change registry to get all supported tokens
+          setTo(supportedAssets[Number(e.target.value)])
+        }
+      >
         <option value=""></option>
-        {supportedAssets.map((asset) => (
-          <option key={asset.base} value={asset.base}>
+        {supportedAssets.map(([asset], i) => (
+          <option key={asset.base} value={i}>
             {asset.symbol}
           </option>
         ))}
@@ -216,29 +214,54 @@ export const OsmosisSwap: React.FC = () => {
         type="text"
         onChange={(e) => setAmount(e.target.value)}
       />
+      <div>Recipient(znam address):</div>
+      <input
+        className="text-black"
+        type="text"
+        onChange={(e) => setRecipient(e.target.value)}
+        value={recipient}
+      />
+      <div>
+        Local recovery address(osmosis address to send tokens to in case
+        something goes wrong on osmisis)
+      </div>
+      <input
+        className="text-black"
+        type="text"
+        onChange={(e) => setLocalRecoveryAddress(e.target.value)}
+        value={localRecoveryAddr}
+      />
+      <br />
+      <button
+        className="bg-yellow text-black p-4 m-3"
+        onClick={handleOsmosisSwap}
+      >
+        SWAP🎏
+      </button>
+
       <p>---</p>
       <div> Receive: </div>
       {quote && (
         <div>
           <div>
             Amount in: {quote.amount_in.amount}
-            {osmosisBaseAssetsMap[from].denom_units[0].aliases?.[0] || from}
+            {from?.asset.denom_units[0].aliases?.[0]}
           </div>
           <div>
             Amount out: {quote.amount_out}
-            {osmosisBaseAssetsMap[to].denom_units[0].aliases?.[0] || to}
+            {to?.[1]?.asset.denom_units[0].aliases?.[0]}
           </div>
           <div>
             Min amount out: {quote.minAmount}
-            {osmosisBaseAssetsMap[to].denom_units[0].aliases?.[0] || to}
+            {to?.[1]?.asset.denom_units[0].aliases?.[0]}
           </div>
           <div>Slippage: {SLIPPAGE * 100}%</div>
           <div>Routes: </div>
           <div>Effective fee: {BigNumber(quote.effective_fee).toString()}</div>
           <div>
-            Price: 1 {osmosisBaseAssetsMap[from].symbol} ≈{" "}
+            Price: 1 {from?.asset.symbol} ≈{" "}
             {BigNumber(quote.amount_out).div(BigNumber(amount)).toString()}{" "}
-            {osmosisBaseAssetsMap[to].symbol}
+            {to?.[1]?.asset.symbol}
           </div>
           <div>
             Price impact: {BigNumber(quote.price_impact).dp(3).toString()}
@@ -250,7 +273,7 @@ export const OsmosisSwap: React.FC = () => {
                 <ul className="list-disc list-inside pl-4">
                   {r.pools.map((p, i) => (
                     <li key={i}>
-                      {p.id}: {osmosisBaseAssetsMap[p.token_out_denom].symbol}
+                      {p.id}: {p.token_out_denom}
                       (Fee: {BigNumber(p.taker_fee).toString()})
                     </li>
                   ))}
