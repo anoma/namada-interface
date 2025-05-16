17f9ccecb1950d0bcf222ee956d25087deaaf38e
Neocyber <sean@arrowburn.co.nz>
2025-05-16 14:39:38 +1200

    fix: Restore transaction details page (#2022)

--- apps/namadillo/src/App/AppRoutes.tsx
+++ apps/namadillo/src/App/AppRoutes.tsx
@@ -45,6 +45,7 @@ import { StakingRewards } from "./Staking/StakingRewards";
 import { StakingWithdrawModal } from "./Staking/StakingWithdrawModal";
 import { Unstake } from "./Staking/Unstake";
 import { SwitchAccountPanel } from "./SwitchAccount/SwitchAccountPanel";
+import { TransactionDetails } from "./Transactions/TransactionDetails";
 import { TransactionHistory } from "./Transactions/TransactionHistory";
 import { TransferLayout } from "./Transfer/TransferLayout";
 
@@ -125,6 +126,10 @@ export const MainRoutes = (): JSX.Element => {
           {(features.namTransfersEnabled || features.ibcTransfersEnabled) && (
             <Route>
               <Route path={routes.history} element={<TransactionHistory />} />
+              <Route
+                path={routes.transaction}
+                element={<TransactionDetails />}
+              />
             </Route>
           )}
 
