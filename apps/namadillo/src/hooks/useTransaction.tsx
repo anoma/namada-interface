e49235f1bbff762af010ad9df7da069bf15f9a72
Mateusz Jasiuk <mateusz.jasiuk@gmail.com>
2025-05-16 13:46:04 +0200

    fix: claim and stake gas estimation

--- apps/namadillo/src/hooks/useTransaction.tsx
+++ apps/namadillo/src/hooks/useTransaction.tsx
@@ -43,7 +43,7 @@ export type UseTransactionProps<T> = {
   params: T[];
   createTxAtom: AtomType<T>;
   useDisposableSigner?: boolean;
-  eventType: TransactionEventsClasses;
+  eventType: TransactionEventsClasses | TransactionEventsClasses[];
   parsePendingTxNotification?: (tx: TransactionPair<T>) => PartialNotification;
   parseErrorTxNotification?: () => PartialNotification;
 } & UseTransactionPropsEvents<T>;
@@ -80,12 +80,22 @@ export const useTransaction = <T,>({
   const dispatchNotification = useSetAtom(dispatchToastNotificationAtom);
   const { mutateAsync: performBuildTx } = useAtomValue(createTxAtom);
 
-  // We don't want to display zeroed value when params are not set yet.
-  const txKinds = new Array(Math.max(1, params.length)).fill(eventType);
+  // Claim & Stake is the only array of tx kinds. The rest are single tx kinds.
+  const arr = new Array(Math.max(1, params.length));
+  const kinds =
+    Array.isArray(eventType) ?
+      // **IMPORTANT**
+      // If eventType is an array, we set kinds by multiplying each kind by params length
+      // i.e. (["ClaimRewards", "Bond"] AND params.length == 2) => ["ClaimRewards", "Bond", "ClaimRewards", "Bond"]
+      arr.fill(eventType).flat()
+    : arr.fill(eventType); // Don't display zeroed value when params are not set yet.
+
   const feeProps = useTransactionFee(
-    txKinds,
-    ["ShieldedTransfer", "UnshieldingTransfer"].includes(eventType)
+    kinds,
+    kinds.some((k) => ["ShieldedTransfer", "UnshieldingTransfer"].includes(k))
   );
+  const broadcastEventType =
+    !Array.isArray(eventType) ? eventType : "ClaimRewards";
 
   const dispatchPendingTxNotification = (
     tx: TransactionPair<T>,
@@ -192,7 +202,7 @@ export const useTransaction = <T,>({
             transactionPair.encodedTxData,
             transactionPair.signedTxs,
             transactionPair.encodedTxData.meta?.props,
-            eventType
+            broadcastEventType
           );
           onBroadcasted?.(transactionPair);
         } catch (error) {
