8747c258ace55ad4e0b5edee0c594c3280fce055
Neocyber <sean@arrowburn.co.nz>
2025-05-28 07:28:42 +1200

    fix: NAM transparent transfer shows generating masp in the button text (#2044)

--- apps/namadillo/src/App/NamadaTransfer/NamadaTransfer.tsx
+++ apps/namadillo/src/App/NamadaTransfer/NamadaTransfer.tsx
@@ -85,10 +85,14 @@ export const NamadaTransfer: React.FC = () => {
     token: selectedAsset?.originalAddress ?? "",
     displayAmount: displayAmount ?? new BigNumber(0),
     onBeforeBuildTx: () => {
-      setCurrentStatus("Generating MASP Parameters...");
-      setCurrentStatusExplanation(
-        "Generating MASP parameters can take a few seconds. Please wait..."
-      );
+      if (isSourceShielded) {
+        setCurrentStatus("Generating MASP Parameters...");
+        setCurrentStatusExplanation(
+          "Generating MASP parameters can take a few seconds. Please wait..."
+        );
+      } else {
+        setCurrentStatus("Preparing transaction...");
+      }
     },
     onBeforeSign: () => {
       setCurrentStatus("Waiting for signature...");
