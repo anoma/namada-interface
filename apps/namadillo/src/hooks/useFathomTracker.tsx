39f162d1a91664e5561b6bece331eafca8f7e645
Pedro Rezende <xpedrorezende@gmail.com>
2025-06-05 13:53:10 +0200

    feat: creating fathom tracker hook

--- apps/namadillo/src/hooks/useFathomTracker.tsx
+++ apps/namadillo/src/hooks/useFathomTracker.tsx
@@ -1,13 +1,41 @@
 import { defaultServerConfigAtom } from "atoms/settings";
-import * as Fathom from "fathom-client";
+import { load, trackEvent, trackPageview } from "fathom-client";
 import { useAtomValue } from "jotai";
 import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
-export const useFathomTracker = (): void => {
+type FathomTrackerOutput = {
+  trackEvent: (eventName: string) => void;
+};
+
+export const useFathomTracker = (): FathomTrackerOutput => {
+  const { pathname, hash, search } = useLocation();
   const tomlConfig = useAtomValue(defaultServerConfigAtom);
+  const fathomSiteId = tomlConfig.data?.fathom_site_id;
+
   useEffect(() => {
     if (tomlConfig.data?.fathom_site_id) {
-      Fathom.load(tomlConfig.data.fathom_site_id, { honorDNT: true });
+      load(tomlConfig.data.fathom_site_id, {
+        honorDNT: true,
+        auto: false,
+      });
     }
-  }, [tomlConfig.data?.fathom_site_id]);
+  }, [fathomSiteId]);
+
+  useEffect(() => {
+    if (tomlConfig.data?.fathom_site_id) {
+      trackPageview({
+        url: location.pathname + search + hash,
+        referrer: document.referrer,
+      });
+    }
+  }, [fathomSiteId, pathname, search, hash]);
+
+  return {
+    trackEvent: (eventName: string) => {
+      if (tomlConfig.data?.fathom_site_id) {
+        trackEvent(eventName);
+      }
+    },
+  };
 };
