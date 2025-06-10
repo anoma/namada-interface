f27ec30f55d4d0ff693be83395054ba628b04606
neocybereth <sean@arrowburn.co.nz>
2025-06-10 21:32:45 +1200

    fix: cleanup log

--- apps/namadillo/src/App/Transfer/TransferModule.tsx
+++ apps/namadillo/src/App/Transfer/TransferModule.tsx
@@ -467,7 +467,6 @@ export const TransferModule = ({
 
   useEffect(() => {
     if (!selectedAsset?.asset && firstAvailableAsset) {
-      console.log(source, "source");
       source.onChangeSelectedAsset?.(firstAvailableAsset?.originalAddress);
     }
   }, [firstAvailableAsset]);
