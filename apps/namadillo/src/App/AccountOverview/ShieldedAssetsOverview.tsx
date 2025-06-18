63e4d30a6b67571e3be6f641148db7a3b574ad82
Harrison Mendonça <harrisonmendonca@gmail.com>
2025-06-18 17:33:29 -0300

    fix: hide masp rewards box until phase 4

--- apps/namadillo/src/App/AccountOverview/ShieldedAssetsOverview.tsx
+++ apps/namadillo/src/App/AccountOverview/ShieldedAssetsOverview.tsx
@@ -2,13 +2,16 @@ import { ActionButton, Panel } from "@namada/components";
 import { MaspSyncCover } from "App/Common/MaspSyncCover";
 import { ShieldedAssetTable } from "App/Masp/ShieldedAssetTable";
 import { routes } from "App/routes";
+import { applicationFeaturesAtom } from "atoms/settings";
 import clsx from "clsx";
 import { useAmountsInFiat } from "hooks/useAmountsInFiat";
 import { useRequiresNewShieldedSync } from "hooks/useRequiresNewShieldedSync";
+import { useAtomValue } from "jotai";
 import { EstimateShieldingRewardsCard } from "./EstimateShieldingRewardsCard";
 import { TotalBalanceCard } from "./TotalBalanceCard";
 
 export const ShieldedAssetsOverview = (): JSX.Element => {
+  const { shieldingRewardsEnabled } = useAtomValue(applicationFeaturesAtom);
   const { shieldedAmountInFiat } = useAmountsInFiat();
   const textContainerClassList = `flex h-full gap-1 items-center justify-center`;
   const requiresNewShieldedSync = useRequiresNewShieldedSync();
@@ -34,7 +37,7 @@ export const ShieldedAssetsOverview = (): JSX.Element => {
             </>
           }
         />
-        <EstimateShieldingRewardsCard />
+        {shieldingRewardsEnabled && <EstimateShieldingRewardsCard />}
       </div>
       <div className="mt-10">
         <ShieldedAssetTable />
