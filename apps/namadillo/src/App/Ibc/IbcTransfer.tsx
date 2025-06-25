4bdcdd64d3f17c80ae655a88d638bbc5d91b0af9
neocybereth <sean@arrowburn.co.nz>
2025-06-25 13:12:58 +1200

    fix: remove blocker to loading asstes

--- apps/namadillo/src/App/Ibc/IbcTransfer.tsx
+++ apps/namadillo/src/App/Ibc/IbcTransfer.tsx
@@ -64,8 +64,9 @@ export const IbcTransfer = (): JSX.Element => {
   );
 
   const { trackEvent } = useFathomTracker();
-  const { data: enabledAssets, isLoading: isLoadingEnabledAssets } =
-    useAtomValue(enabledIbcAssetsDenomFamily(ibcChannels?.namadaChannel));
+  const { data: enabledAssets } = useAtomValue(
+    enabledIbcAssetsDenomFamily(ibcChannels?.namadaChannel)
+  );
   const [shielded, setShielded] = useState<boolean>(true);
   const [selectedAssetAddress, setSelectedAssetAddress] = useUrlState(
     params.asset
@@ -198,7 +199,7 @@ export const IbcTransfer = (): JSX.Element => {
       </div>
       <TransferModule
         source={{
-          isLoadingAssets: isLoadingBalances || isLoadingEnabledAssets,
+          isLoadingAssets: isLoadingBalances,
           availableAssets,
           selectedAssetAddress,
           availableAmount,
