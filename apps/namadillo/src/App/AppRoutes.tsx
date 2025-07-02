4e403ee4b6fc7885021af85718bff167db0c6b3c
neocybereth <sean@arrowburn.co.nz>
2025-07-02 19:39:37 +1200

    fix: remove token example

--- apps/namadillo/src/App/AppRoutes.tsx
+++ apps/namadillo/src/App/AppRoutes.tsx
@@ -43,7 +43,6 @@ import { Unstake } from "./Staking/Unstake";
 import { SwitchAccountPanel } from "./SwitchAccount/SwitchAccountPanel";
 import { TransactionDetails } from "./Transactions/TransactionDetails";
 import { TransactionHistory } from "./Transactions/TransactionHistory";
-import { SelectTokenExample } from "./Transfer/SelectTokenExample";
 import { TransferLayout } from "./Transfer/TransferLayout";
 
 export const MainRoutes = (): JSX.Element => {
@@ -104,8 +103,6 @@ export const MainRoutes = (): JSX.Element => {
               <Route path={routes.ibcWithdraw} element={<IbcWithdraw />} />
             </Route>
           )} */}
-
-          <Route path={"/example"} element={<SelectTokenExample />} />
 
           {features.ibcTransfersEnabled && (
             <Route element={<IbcLayout />}>
