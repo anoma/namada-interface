7d1437dc731438a8621337574a23076e840b3270
neocybereth <sean@arrowburn.co.nz>
2025-07-03 15:16:16 +1200

    fix: reinstate

--- apps/namadillo/src/App/AppRoutes.tsx
+++ apps/namadillo/src/App/AppRoutes.tsx
@@ -21,6 +21,9 @@ import { SubmitVote } from "./Governance/SubmitVote";
 import { ViewJson } from "./Governance/ViewJson";
 import { IbcLayout } from "./Ibc/IbcLayout";
 import { IbcShieldAll } from "./Ibc/IbcShieldAll";
+import { IbcTransfer } from "./Ibc/IbcTransfer";
+import { IbcTransfersLayout } from "./Ibc/IbcTransfersLayout";
+import { IbcWithdraw } from "./Ibc/IbcWithdraw";
 import { MaspLayout } from "./Masp/MaspLayout";
 import { MaspOverview } from "./Masp/MaspOverview";
 import { MaspShield } from "./Masp/MaspShield";
@@ -99,12 +102,12 @@ export const MainRoutes = (): JSX.Element => {
           )}
 
           {/* Ibc Transfers */}
-          {/* {features.ibcTransfersEnabled && (
+          {features.ibcTransfersEnabled && (
             <Route element={<IbcTransfersLayout />}>
               <Route path={routes.ibc} element={<IbcTransfer />} />
               <Route path={routes.ibcWithdraw} element={<IbcWithdraw />} />
             </Route>
-          )} */}
+          )}
 
           {features.ibcTransfersEnabled && (
             <Route element={<IbcLayout />}>
